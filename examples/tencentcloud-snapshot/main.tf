1f412980ceed0e798fe7fd10abe643dd53440334
ZhiQiang Fan <zhiqiangfan@tencent.com>
2018-03-09 03:31:36 -0800

    fix cbs snapshot resource name in examples & docs

--- examples/tencentcloud-snapshot/main.tf
+++ examples/tencentcloud-snapshot/main.tf
@@ -1,4 +1,4 @@
-resource "tencentcloud_snapshot" "my-snapshot" {
+resource "tencentcloud_cbs_snapshot" "my-snapshot" {
   storage_id    = "${var.storage_id}"
   snapshot_name = "${var.snapshot_name}"
 }
