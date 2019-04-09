89f668560cddd1196f5b5ea893ec3487552ce1e5
himer <xishizhaohua@qq.com>
2019-04-09 14:28:57 +0800

    resource for tencentcloud_mysql_backup_policy  (263s cost.)

---
+++ tencentcloud/resource_tc_mysql_backup_policy_test.go
@@ -0,0 +1,99 @@
+package tencentcloud
+
+import (
+	"context"
+	"fmt"
+
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/errors"
+)
+
+func TestAccTencentCloudMysqlBackupPolicy(t *testing.T) {
+	InitLogConfig(true)
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccTencentCloudMysqlBackupPolicyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccMysqlBackupPolicy(MysqlInstanceCommonTestCase),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccTencentCloudMysqlBackupPolicyExists("tencentcloud_mysql_backup_policy.mysql_backup_policy"),
+					resource.TestCheckResourceAttrSet("tencentcloud_mysql_backup_policy.mysql_backup_policy", "mysql_id"),
+					resource.TestCheckResourceAttr("tencentcloud_mysql_backup_policy.mysql_backup_policy", "retention_period", "56"),
+					resource.TestCheckResourceAttr("tencentcloud_mysql_backup_policy.mysql_backup_policy", "backup_model", "physical"),
+					resource.TestCheckResourceAttr("tencentcloud_mysql_backup_policy.mysql_backup_policy", "backup_time", "10:00-14:00"),
+					resource.TestCheckResourceAttrSet("tencentcloud_mysql_backup_policy.mysql_backup_policy", "binlog_period"),
+				),
+			},
+		},
+	})
+}
+
+func testAccTencentCloudMysqlBackupPolicyExists(r string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+
+		logId := GetLogId(nil)
+		ctx := context.WithValue(context.TODO(), "logId", logId)
+
+		rs, ok := s.RootModule().Resources[r]
+		if !ok {
+			return fmt.Errorf("resource %s is not found", r)
+		}
+
+		mysqlService := MysqlService{client: testAccProvider.Meta().(*TencentCloudClient).apiV3Conn}
+
+		_, err := mysqlService.DescribeBackupConfigByMysqlId(ctx, rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func testAccTencentCloudMysqlBackupPolicyDestroy(s *terraform.State) error {
+	logId := GetLogId(nil)
+	ctx := context.WithValue(context.TODO(), "logId", logId)
+
+	mysqlService := MysqlService{client: testAccProvider.Meta().(*TencentCloudClient).apiV3Conn}
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "tencentcloud_mysql_backup_policy" {
+			continue
+		}
+		desResponse, err := mysqlService.DescribeBackupConfigByMysqlId(ctx, rs.Primary.ID)
+
+		if err != nil {
+			sdkErr, ok := err.(*errors.TencentCloudSDKError)
+			if ok && sdkErr.Code == MysqlInstanceIdNotFound {
+				continue
+			}
+			return err
+		}
+		if *desResponse.Response.BackupExpireDays != 7 {
+			return fmt.Errorf("mysql  backup  policy  BackupExpireDays  is not default")
+		}
+		if *desResponse.Response.BackupMethod != "logical" {
+			return fmt.Errorf("mysql  backup  policy  BackupMethod  is not default")
+		}
+		if *desResponse.Response.StartTimeMax != 6 || *desResponse.Response.StartTimeMin != 2 {
+			return fmt.Errorf("mysql  backup  policy  StartTimeMax or StartTimeMin  is not default")
+		}
+	}
+
+	return nil
+}
+
+func testAccMysqlBackupPolicy(commonTestCase string) string {
+	return fmt.Sprintf(`
+%s
+resource "tencentcloud_mysql_backup_policy" "mysql_backup_policy" {
+	mysql_id = "${tencentcloud_mysql_instance.default.id}"
+	retention_period = 56
+	backup_model = "physical"
+	backup_time = "10:00-14:00"
+}`, commonTestCase)
+}
