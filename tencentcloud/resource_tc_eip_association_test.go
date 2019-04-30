711e76a3a42827ea57f8ea7fc32c3bbc5ef4ac18
ZhiQiang Fan <aji.zqfan@gmail.com>
2019-04-30 14:59:33 +0800

    update test code
    fix ci failures

--- tencentcloud/resource_tc_eip_association_test.go
+++ tencentcloud/resource_tc_eip_association_test.go
@@ -131,13 +131,12 @@ data "tencentcloud_instance_types" "my_favorate_instance_types" {
   memory_size    = 2
 }
 
-data "tencentcloud_availability_zones" "my_favorate_zones" {}
-
 resource "tencentcloud_instance" "my_instance" {
   instance_name     = "terraform_automation_test_kuruk"
-  availability_zone = "${data.tencentcloud_availability_zones.my_favorate_zones.zones.0.name}"
+  availability_zone = "ap-guangzhou-3"
   image_id          = "${data.tencentcloud_image.my_favorate_image.image_id}"
   instance_type     = "${data.tencentcloud_instance_types.my_favorate_instance_types.instance_types.0.instance_type}"
+  system_disk_type  = "CLOUD_SSD"
 }
 
 resource "tencentcloud_eip" "my_eip" {
