c811b4bf69de87072630fa6fa1166f8c2d3ecd5f
Li Kexian <doyenli@tencent.com>
2019-05-05 10:44:14 +0800

    add terraform docs generator

---
+++ gendoc/template.go
@@ -0,0 +1,33 @@
+package main
+
+const (
+	docTPL = `---
+layout: "{{.cloud_mark}}"
+page_title: "{{.cloud_title}}: {{.name}}"
+sidebar_current: "docs-{{.cloud_mark}}-{{.dtype}}-{{.resource}}"
+description: |-
+  {{.description_short}}
+---
+
+# {{.name}}
+
+{{.description}}
+
+## Example Usage
+
+{{.example}}
+
+## Argument Reference
+
+The following arguments are supported:
+
+{{.arguments}}
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+{{.attributes}}
+
+`
+)
