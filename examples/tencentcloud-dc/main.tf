e2cffc85d3e8b2eead89a30369b5d90ad3f222cb
himer <xishizhaohua@qq.com>
2019-07-11 16:32:37 +0800

    add ""

--- examples/tencentcloud-dc/main.tf
+++ examples/tencentcloud-dc/main.tf
@@ -1,4 +1,4 @@
-resource tencentcloud_dcx  bgp_main
+resource "tencentcloud_dcx"  "bgp_main"
  {
     bandwidth = 900
     dc_id = "${var.dc_id}"
@@ -10,7 +10,7 @@ resource tencentcloud_dcx  bgp_main
     vpc_id = "${var.vpc_id}"
 }
 
-resource tencentcloud_dcx  static_main
+resource "tencentcloud_dcx"  "static_main"
  {
     bandwidth = 900
     dc_id = "${var.dc_id}"
@@ -25,18 +25,18 @@ resource tencentcloud_dcx  static_main
 	customer_address = "100.93.46.2/30"
 }
 
-data tencentcloud_dcx_instances name_select{
+data "tencentcloud_dcx_instances" "name_select"{
     name = "main"
 }
 
-data tencentcloud_dcx_instances  id {
+data "tencentcloud_dcx_instances"  "id" {
     dcx_id = "${tencentcloud_dcx.static_main.id}"
 }
 
-data tencentcloud_dc_instances  name {
+data "tencentcloud_dc_instances"  "name" {
     name ="x"
 }
-data tencentcloud_dc_instances  id {
+data "tencentcloud_dc_instances"  "id" {
     dc_id="${var.dc_id}"
 }
 
