d7ed1b9aedde7bfecbfd982af117b38709ac6a40
Sherlock Holo <sherlockya@gmail.com>
2019-07-19 11:06:54 +0800

    Optimize count calculate
    Signed-off-by: Sherlock Holo <sherlockya@gmail.com>

--- tencentcloud/data_source_tc_security_groups.go
+++ tencentcloud/data_source_tc_security_groups.go
@@ -148,10 +148,7 @@ func dataSourceTencentCloudSecurityGroupsRead(d *schema.ResourceData, m interfac
 
 		var count int
 		if associate != nil {
-			count += int(*associate.CVM)
-			count += int(*associate.ENI)
-			count += int(*associate.CDB)
-			count += int(*associate.CLB)
+			count += int(*associate.CVM + *associate.ENI + *associate.CDB + *associate.CLB)
 		}
 
 		pjId, err := strconv.Atoi(*sg.ProjectId)
