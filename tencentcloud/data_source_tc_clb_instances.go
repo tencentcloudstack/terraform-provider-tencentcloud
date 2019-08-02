d8066c699f71814aeed245740415cf908dccbb57
Li Kexian <doyenli@tencent.com>
2019-08-02 19:45:22 +0800

    fixed name in doc

--- tencentcloud/data_source_tc_clb_instances.go
+++ tencentcloud/data_source_tc_clb_instances.go
@@ -4,7 +4,7 @@ Use this data source to query detailed information of CLB
 Example Usage
 
 ```hcl
-data "tencentcloud_clb" "clblab" {
+data "tencentcloud_clb_instances" "foo" {
     clb_id             = "lb-k2zjp9lv"
     network_type       = "OPEN"
     clb_name           = "myclb"
--- tencentcloud/data_source_tc_clb_listeners.go
+++ tencentcloud/data_source_tc_clb_listeners.go
@@ -4,7 +4,7 @@ Use this data source to query detailed information of CLB listener
 Example Usage
 
 ```hcl
-data "tencentcloud_clb_listener" "clblistener" {
+data "tencentcloud_clb_listeners" "foo" {
   clb_id      = "lb-k2zjp9lv"
   listener_id = "lbl-mwr6vbtv"
   protocol    = "TCP"
