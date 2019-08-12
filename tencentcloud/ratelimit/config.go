3bbe4f35fbb4494a96caf2c625bbf5fe6fc79d9c
himer <xishizhaohua@qq.com>
2019-08-12 10:19:22 +0800

    cgi ratelimit.

--- tencentcloud/ratelimit/config.go
+++ tencentcloud/ratelimit/config.go
@@ -14,7 +14,6 @@ func init() {
 	limitConfig["resource_tc_instance.update"] = 10
 	limitConfig["resource_tc_instance.delete"] = 10
 
-
 	//new(filename . action)
 	limitConfig["service_tencentcloud_mysql"] = 50
 	limitConfig["service_tencentcloud_mysql.CreateDBInstanceHour"] = 20
@@ -22,7 +21,14 @@ func init() {
 	limitConfig["service_tencentcloud_mysql.CreateBackup"] = 5
 	limitConfig["service_tencentcloud_mysql.ModifyInstanceParam"] = 20
 
+	//filename
 	limitConfig["service_tencentcloud_cos"] = 10
-
-
+	limitConfig["service_tencentcloud_vpc"] = 10
+	limitConfig["service_tencentcloud_redis"] = 10
+	limitConfig["service_tencentcloud_mongodb"] = 10
+	limitConfig["service_tencentcloud_dcg"] = 10
+	limitConfig["service_tencentcloud_dc"] = 5
+	limitConfig["service_tencentcloud_ccn"] = 20
+	limitConfig["service_tencentcloud_cbs"] = 20
+	limitConfig["service_tencentcloud_as"] = 10
 }
