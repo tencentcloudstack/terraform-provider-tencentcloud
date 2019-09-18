02461667e8e3e3a98d7726a6de75d90b05e65e9b
Sherlock Holo <sherlockya@gmail.com>
2019-09-18 10:59:53 +0800

    docs: update doc
    Signed-off-by: Sherlock Holo <sherlockya@gmail.com>

--- examples/tencentcloud-redis/main.tf
+++ examples/tencentcloud-redis/main.tf
@@ -5,10 +5,24 @@ resource "tencentcloud_redis_instance" "redis_instance_test" {
   mem_size          = 8192
   name              = "terrform_test"
   port              = 6379
+
+  tags = {
+    "test" = "test"
+  }
 }
 
 resource "tencentcloud_redis_backup_config" "redis_backup_config" {
   redis_id      = "${tencentcloud_redis_instance.redis_instance_test.id}"
   backup_time   = "01:00-02:00"
   backup_period = ["Saturday", "Sunday"]
+}
+
+data "tencentcloud_redis_instances" "redis" {
+  zone       = "${var.availability_zone}"
+  search_key = "${tencentcloud_redis_instance.redis_instance_test.id}"
+}
+
+data "tencentcloud_redis_instances" "redis-tags" {
+  zone = "${var.availability_zone}"
+  tags = "${tencentcloud_redis_instance.redis_instance_test.tags}"
 }
