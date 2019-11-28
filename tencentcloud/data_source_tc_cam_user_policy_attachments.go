24ba3e2ec235ac5c407efc63e75a0cf8109a2f1f
gailwang <gailwang@tencent.com>
2019-11-28 20:29:05 +0800

    fix para filter issue

--- tencentcloud/data_source_tc_cam_user_policy_attachments.go
+++ tencentcloud/data_source_tc_cam_user_policy_attachments.go
@@ -109,7 +109,11 @@ func dataSourceTencentCloudCamUserPolicyAttachmentsRead(d *schema.ResourceData, 
 	userId := d.Get("user_id").(string)
 	params["user_id"] = userId
 	if v, ok := d.GetOk("policy_id"); ok {
-		params["policy_id"] = uint64(v.(int))
+		policyId, err := strconv.Atoi(v.(string))
+		if err != nil {
+			return err
+		}
+		params["policy_id"] = uint64(policyId)
 	}
 	if v, ok := d.GetOk("policy_type"); ok {
 		params["policy_type"] = v.(string)
