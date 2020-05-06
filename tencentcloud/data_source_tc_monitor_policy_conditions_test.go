28ea7dcd7adea783f55b4d601ed0c99ecbd0e225
himer <xishizhaohua@qq.com>
2020-05-06 16:43:22 +0800

    test for data source.

---
+++ tencentcloud/data_source_tc_monitor_policy_conditions_test.go
@@ -0,0 +1,31 @@
+package tencentcloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccMonitorPolicyConditions(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceMonitorPolicyConditions(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckTencentCloudDataSourceID("data.tencentcloud_monitor_policy_conditions.monitor_policy_conditions"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_monitor_policy_conditions.monitor_policy_conditions",
+						"list.#"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceMonitorPolicyConditions() string {
+	return `
+data "tencentcloud_monitor_policy_conditions" "monitor_policy_conditions" {
+  name               = "Cloud Virtual Machine"
+}`
+}
