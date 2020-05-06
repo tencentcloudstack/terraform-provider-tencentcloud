c12da7ea34c6520475bf83c5b2f1ed8eabdfc589
himer <xishizhaohua@qq.com>
2020-05-06 16:29:21 +0800

    data tencentcloud_monitor_product_event.

--- tencentcloud/internal/helper/transform.go
+++ tencentcloud/internal/helper/transform.go
@@ -57,6 +57,14 @@ func InterfacesStrings(configured []interface{}) []string {
 	return vs
 }
 
+func InterfacesStringsPoint(configured []interface{}) []*string {
+	vs := make([]*string, 0, len(configured))
+	for _, v := range configured {
+		vs = append(vs, String(v.(string)))
+	}
+	return vs
+}
+
 // Flatten to an array of raw strings and returns a []interface{}
 func StringsInterfaces(list []*string) []interface{} {
 	vs := make([]interface{}, 0, len(list))
