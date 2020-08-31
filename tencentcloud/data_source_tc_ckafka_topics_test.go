7021f90f399870fefd7ae5c58a2944652de6515a
cyberHermanwang <473102868@qq.com>
2020-08-31 11:19:53 +0800

    add ckafka topic examples file

--- tencentcloud/data_source_tc_ckafka_topics_test.go
+++ tencentcloud/data_source_tc_ckafka_topics_test.go
@@ -18,7 +18,7 @@ func TestAccTencentCloudCkafkaTopicDataSource(t *testing.T) {
 					testAccCheckKafkaTopicInstanceExists("tencentcloud_ckafka_topic.kafka_topic"),
 					resource.TestCheckResourceAttr("data.tencentcloud_ckafka_topics.kafka_topics", "instance_id", "ckafka-f9ife4zz"),
 					resource.TestCheckResourceAttrSet("data.tencentcloud_ckafka_topics.kafka_topics", "instance_list.#"),
-					resource.TestCheckResourceAttr("data.tencentcloud_ckafka_topics.kafka_topics", "instance_list.0.topic_name", "ckafkaTopic-tf-test06"),
+					resource.TestCheckResourceAttr("data.tencentcloud_ckafka_topics.kafka_topics", "instance_list.0.topic_name", "ckafkaTopic-tf-test"),
 					resource.TestCheckResourceAttr("data.tencentcloud_ckafka_topics.kafka_topics", "instance_list.0.partition_num", "1"),
 					resource.TestCheckResourceAttr("data.tencentcloud_ckafka_topics.kafka_topics", "instance_list.0.replica_num", "2"),
 					resource.TestCheckResourceAttrSet("data.tencentcloud_ckafka_topics.kafka_topics", "instance_list.0.create_time"),
@@ -29,10 +29,9 @@ func TestAccTencentCloudCkafkaTopicDataSource(t *testing.T) {
 }
 
 const testAccTencentCloudCkafkaTopicDataSourceConfig = `
-
 resource "tencentcloud_ckafka_topic" "kafka_topic" {
    instance_id                   = "ckafka-f9ife4zz"
-   topic_name                = "ckafkaTopic-tf-test06"
+   topic_name                = "ckafkaTopic-tf-test"
    replica_num                   = 2
    partition_num              = 1
 }
