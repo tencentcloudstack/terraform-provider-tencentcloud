e677165592f6b659be555e650173c0566d0b6dac
“guojunchu” <junchuguo@tencnet.com>
2020-10-16 10:45:23 +0800

    update doc

--- examples/tencentcloud-clb/main.tf
+++ examples/tencentcloud-clb/main.tf
@@ -153,12 +153,12 @@ resource "tencentcloud_clb_listener_rule" "rule_basic" {
   target_type         = "TARGETGROUP"
 }
 
-resource "tencentcloud_clb_targetgroup" "test"{
+resource "tencentcloud_clb_target_group" "test"{
     target_group_name = "test-target-keep-1"
 }
 
-resource "tencentcloud_clb_tg_instance_attachment" "test"{
-  target_group_id = tencentcloud_clb_targetgroup.test.id
+resource "tencentcloud_clb_target_group_instance_attachment" "test"{
+  target_group_id = tencentcloud_clb_target_group.test.id
   bind_ip         = "172.16.48.18"
   port            = 222
   weight          = 3
@@ -168,11 +168,11 @@ resource "tencentcloud_clb_target_group_attachment" "group" {
     clb_id          = tencentcloud_clb_instance.clb_basic.id
     listener_id     = tencentcloud_clb_listener.listener_basic.id
 	rule_id         = tencentcloud_clb_listener_rule.rule_basic.id
-    targrt_group_id = tencentcloud_clb_targetgroup.test.id
+    targrt_group_id = tencentcloud_clb_target_group.test.id
 }
 
 data "tencentcloud_clb_target_groups" "target_group_info_id" {
-  target_group_id = tencentcloud_clb_targetgroup.test.id
+  target_group_id = tencentcloud_clb_target_group.test.id
 }
 
 data "tencentcloud_clb_instances" "instances" {
