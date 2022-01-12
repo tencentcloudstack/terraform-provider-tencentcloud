c2225af2bb9d0d4f0e30c3daadb76e05e98ea5f7
KGSN <ghosthcp516@hotmail.com>
2022-01-12 10:58:14 +0800

    fix: diff-test script

--- scripts/diff-test.sh
+++ scripts/diff-test.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
+# You can run `make diff-test` to execute
+
+echo "==> Checking test cases which module modified"
+
 cd ./test-diff
 
 pattern=$(go run main.go | tr -d '\r')
 
-if [[$pattern != ""]]
-  then
-    cd ../tencentcloud
+echo "=========== diffs ==========="
+git diff --name-only HEAD $(git describe --tags --abbrev=0)
+echo "======= end of diffs ========"
 
-    echo go test -v -run $pattern
+if [[ $pattern != "" ]];then
+  cd ../tencentcloud
 
-    go test -v -run $pattern
-  else
-    echo diff: $(git diff --name-only HEAD $(git describe --tags --abbrev=0))
-    echo "no test case match"
+  echo go test -v -run $pattern
+
+  go test -v -run $pattern
+else
+  echo "No test case match"
 fi
 
