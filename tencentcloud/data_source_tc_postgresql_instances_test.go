743d944b33f4a90c6a34739a61ad5d01ab553ae4
KGSN <ghosthcp516@hotmail.com>
2022-03-01 10:17:12 +0800

    fix: datasource/pg instance query id

--- tencentcloud/data_source_tc_postgresql_instances_test.go
+++ tencentcloud/data_source_tc_postgresql_instances_test.go
@@ -23,7 +23,7 @@ func TestAccTencentCloudDataPostgresqlInstances(t *testing.T) {
 					resource.TestCheckResourceAttrSet(testDataPostgresqlInstancesName, "instance_list.0.id"),
 					resource.TestCheckResourceAttrSet(testDataPostgresqlInstancesName, "instance_list.0.create_time"),
 					resource.TestCheckResourceAttr(testDataPostgresqlInstancesName, "instance_list.0.charge_type", "POSTPAID_BY_HOUR"),
-					resource.TestCheckResourceAttr(testDataPostgresqlInstancesName, "instance_list.0.engine_version", "9.3.5"),
+					resource.TestCheckResourceAttr(testDataPostgresqlInstancesName, "instance_list.0.engine_version", "10.4"),
 					resource.TestCheckResourceAttr(testDataPostgresqlInstancesName, "instance_list.0.project_id", "0"),
 					resource.TestCheckResourceAttr(testDataPostgresqlInstancesName, "instance_list.0.memory", "2"),
 					resource.TestCheckResourceAttr(testDataPostgresqlInstancesName, "instance_list.0.storage", "10"),
@@ -39,27 +39,48 @@ func TestAccTencentCloudDataPostgresqlInstances(t *testing.T) {
 }
 
 const testAccTencentCloudDataPostgresqlInstanceBasic = `
-variable "availability_zone"{
-default = "ap-guangzhou-2"
+data "tencentcloud_availability_zones_by_product" "pg" {
+  product = "postgres"
 }
 
-resource "tencentcloud_postgresql_instance" "test" {
-name = "tf_postsql_instance"
-availability_zone = var.availability_zone
-charge_type = "POSTPAID_BY_HOUR"
-engine_version		= "9.3.5"
-root_password                 = "1qaA2k1wgvfa3ZZZ"
-charset = "UTF8"
-project_id = 0
-memory = 2
-storage = 10
+resource "tencentcloud_vpc" "vpc" {
+  cidr_block = "10.0.0.0/24"
+  name       = "test-pg-vpc"
+}
 
-	tags = {
-		tf = "test"
-	}
+resource "tencentcloud_subnet" "subnet" {
+  availability_zone = local.az
+  cidr_block        = "10.0.0.0/24"
+  name              = "sub1"
+  vpc_id            = tencentcloud_vpc.vpc.id
+}
+
+locals {
+  az = data.tencentcloud_availability_zones_by_product.pg.zones.0.name
+  vpc_id = tencentcloud_vpc.vpc.id
+  subnet_id = tencentcloud_subnet.subnet.id
+}
+
+
+resource "tencentcloud_postgresql_instance" "test" {
+  name = "tf_postsql_instance"
+  availability_zone = local.az
+  charge_type = "POSTPAID_BY_HOUR"
+  engine_version		= "10.4"
+  root_password                 = "1qaA2k1wgvfa!_3ZZZ"
+  charset = "UTF8"
+  project_id = 0
+  memory = 2
+  storage = 10
+  vpc_id                       = local.vpc_id
+  subnet_id                    = local.subnet_id
+
+  tags = {
+    tf = "test"
+  }
 }
 
 data "tencentcloud_postgresql_instances" "id_test"{
-	id = tencentcloud_postgresql_instance.test.id
+  id = tencentcloud_postgresql_instance.test.id
 }
 `
