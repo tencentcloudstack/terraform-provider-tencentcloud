a9012f47ba8a11b5373a4b733a473ed6897fb0b5
mikatong <mikatong@tencent.com>
2022-03-07 20:08:01 +0800

    fix: pay_mode can not use 0

--- tencentcloud/resource_tc_emr_cluster_test.go
+++ tencentcloud/resource_tc_emr_cluster_test.go
@@ -22,10 +22,9 @@ func TestAccTencentCloudEmrClusterResource(t *testing.T) {
 					testAccCheckEmrExists(testTkeClusterResourceKey),
 					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "product_id", "4"),
 					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "display_strategy", "clusterList"),
-					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "vpc_settings.vpc_id", "vpc-fuwly8x5"),
-					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "vpc_settings.subnet_id", "subnet-d830wfso"),
-					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "softwares.0", "hadoop-2.8.4"),
-					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "softwares.1", "zookeeper-3.4.9"),
+					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "vpc_settings.vpc_id", "vpc-i9zty99n"),
+					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "vpc_settings.subnet_id", "subnet-opwnh0sw"),
+					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "softwares.0", "zookeeper-3.6.1"),
 					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "support_ha", "0"),
 					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "instance_name", "emr-test"),
 					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "resource_spec.master_resource_spec.mem_size", "8192"),
@@ -43,9 +42,9 @@ func TestAccTencentCloudEmrClusterResource(t *testing.T) {
 					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "resource_spec.master_count", "1"),
 					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "resource_spec.core_count", "2"),
 					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "login_settings.password", "tencent@cloud123"),
-					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "time_span", "1"),
-					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "time_unit", "m"),
-					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "pay_mode", "1"),
+					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "time_span", "3600"),
+					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "time_unit", "s"),
+					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "pay_mode", "0"),
 					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "placement.zone", "ap-guangzhou-3"),
 					resource.TestCheckResourceAttr(testTkeClusterResourceKey, "placement.project_id", "0"),
 					resource.TestCheckResourceAttrSet(testTkeClusterResourceKey, "instance_id"),
@@ -104,38 +103,48 @@ func testAccCheckEmrExists(n string) resource.TestCheckFunc {
 	}
 }
 
-const testEmrBasic = defaultVpcVariable + `
+const testEmrBasic = `
 resource "tencentcloud_emr_cluster" "emrrrr" {
 	product_id=4
 	display_strategy="clusterList"
-	vpc_settings={vpc_id: var.vpc_id, subnet_id: var.subnet_id }
-	softwares=["zookeeper-3.5.5"]
-    support_ha=0
-    instance_name="emr-test"
-	resource_spec {
-	    master_resource_spec {
-	        mem_size=8192
-	        cpu=4
-	        disk_size=100
-	        disk_type="CLOUD_PREMIUM"
-	        spec="CVM.S2"
-	        storage_type=5
-	    }
-	    core_resource_spec {
-	        mem_size=8192
-	        cpu=4
-	        disk_size=100
-	        disk_type="CLOUD_PREMIUM"
-	        spec="CVM.S2"
-	        storage_type=5
-	    }
-	    master_count=1
-	    core_count=2
+	vpc_settings={
+	  vpc_id="vpc-i9zty99n"
+	  subnet_id:"subnet-opwnh0sw"
 	}
-	login_settings={password:"tencent@cloud123"}
-	time_span=1
-	time_unit="m"
-    pay_mode=1
-    placement={zone:"ap-guangzhou-3", project_id:0}
-}
+	softwares=[
+	  "zookeeper-3.6.1",
+  ]
+	support_ha=0
+	instance_name="emr-test-demo"
+	resource_spec {
+	  master_resource_spec {
+		mem_size=8192
+		cpu=4
+		disk_size=100
+		disk_type="CLOUD_PREMIUM"
+		spec="CVM.S2"
+		storage_type=5
+	  }
+	  core_resource_spec {
+		mem_size=8192
+		cpu=4
+		disk_size=100
+		disk_type="CLOUD_PREMIUM"
+		spec="CVM.S2"
+		storage_type=5
+	  }
+	  master_count=1
+	  core_count=2
+	}
+	login_settings={
+	  password="tencent@cloud123"
+	}
+	time_span=3600
+	time_unit="s"
+	pay_mode=0
+	placement={
+	  zone="ap-guangzhou-3"
+	  project_id=0
+	}
+  }
 `
