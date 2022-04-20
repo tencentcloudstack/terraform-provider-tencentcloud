8bf337ff77e90a8fbe1313b2d909118eaa4ebbb2
hellertang <wsthl19920901@163.com>
2022-04-20 15:01:44 +0800

    support multi account test (#978)
    * support multi account test
    
    * fix: env ref mismatch
    
    Co-authored-by: Kagashino <ghosthcp516@hotmail.com>

--- tencentcloud/provider_test.go
+++ tencentcloud/provider_test.go
@@ -14,6 +14,21 @@ import (
 var testAccProviders map[string]terraform.ResourceProvider
 var testAccProvider *schema.Provider
 
+const (
+	ACCOUNT_TYPE_INTERNATION        = "INTERNATION"
+	ACCOUNT_TYPE_PREPAY             = "PREPAY"
+	ACCOUNT_TYPE_COMMON             = "COMMON"
+	ACCOUNT_TYPE_PRIVATE            = "PRIVATE"
+	INTERNATION_PROVIDER_SECRET_ID  = "TENCENTCLOUD_SECRET_ID_INTERNATION"
+	INTERNATION_PROVIDER_SECRET_KEY = "TENCENTCLOUD_SECRET_KEY_INTERNATION"
+	PREPAY_PROVIDER_SECRET_ID       = "TENCENTCLOUD_SECRET_ID_PREPAY"
+	PREPAY_PROVIDER_SECRET_KEY      = "TENCENTCLOUD_SECRET_KEY_PREPAY"
+	PRIVATE_PROVIDER_SECRET_ID      = "TENCENTCLOUD_SECRET_ID_PRIVATE"
+	PRIVATE_PROVIDER_SECRET_KEY     = "TENCENTCLOUD_SECRET_KEY_PRIVATE"
+	COMMON_PROVIDER_SECRET_ID       = "TENCENTCLOUD_SECRET_ID_COMMON"
+	COMMON_PROVIDER_SECRET_KEY      = "TENCENTCLOUD_SECRET_KEY_COMMON"
+)
+
 func init() {
 	testAccProvider = Provider().(*schema.Provider)
 	testAccProviders = map[string]terraform.ResourceProvider{
@@ -41,6 +56,14 @@ func testAccPreCheck(t *testing.T) {
 	if v := os.Getenv(PROVIDER_REGION); v == "" {
 		log.Printf("[INFO] Testing: Using %s as test region", defaultRegion)
 		os.Setenv(PROVIDER_REGION, defaultRegion)
+	}
+	if v := os.Getenv(COMMON_PROVIDER_SECRET_ID); v == "" {
+		secretId := os.Getenv(COMMON_PROVIDER_SECRET_ID)
+		os.Setenv(PROVIDER_SECRET_ID, secretId)
+	}
+	if v := os.Getenv(COMMON_PROVIDER_SECRET_KEY); v == "" {
+		secretKey := os.Getenv(COMMON_PROVIDER_SECRET_KEY)
+		os.Setenv(PROVIDER_SECRET_KEY, secretKey)
 	}
 }
 
