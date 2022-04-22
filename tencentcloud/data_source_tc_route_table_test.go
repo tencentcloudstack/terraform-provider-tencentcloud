67be44eb80b3a1366da7c49efc1b432c0f02e267
hellertang <hellertang@tencent.com>
2022-04-22 17:12:24 +0800

    add subnet/routetable sweeper

--- tencentcloud/data_source_tc_route_table_test.go
+++ tencentcloud/data_source_tc_route_table_test.go
@@ -1,10 +1,65 @@
 package tencentcloud
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
+
+func init() {
+	resource.AddTestSweepers("tencentcloud_route_table", &resource.Sweeper{
+		Name: "tencentcloud_route_table",
+		F:    testSweepRouteTable,
+	})
+}
+
+func testSweepRouteTable(region string) error {
+	logId := getLogId(contextNil)
+	ctx := context.WithValue(context.TODO(), logIdKey, logId)
+
+	sharedClient, err := sharedClientForRegion(region)
+	if err != nil {
+		return fmt.Errorf("getting tencentcloud client error: %s", err.Error())
+	}
+	client := sharedClient.(*TencentCloudClient)
+
+	vpcService := VpcService{
+		client: client.apiV3Conn,
+	}
+
+	instances, err := vpcService.DescribeRouteTables(ctx, "", "", "", nil, nil, "")
+	if err != nil {
+		return fmt.Errorf("get instance list error: %s", err.Error())
+	}
+
+	for _, v := range instances {
+
+		instanceId := v.routeTableId
+		instanceName := v.name
+		now := time.Now()
+		createTime := stringTotime(v.createTime)
+		interval := now.Sub(createTime).Minutes()
+
+		if strings.HasPrefix(instanceName, keepResource) || strings.HasPrefix(instanceName, defaultResource) {
+			continue
+		}
+
+		if needProtect == 1 && int64(interval) < 30 {
+			continue
+		}
+
+		if err = vpcService.DeleteRouteTable(ctx, instanceId); err != nil {
+			log.Printf("[ERROR] sweep instance %s error: %s", instanceId, err.Error())
+		}
+	}
+
+	return nil
+}
 
 func TestAccDataSourceTencentCloudRouteTable_basic(t *testing.T) {
 	t.Parallel()
