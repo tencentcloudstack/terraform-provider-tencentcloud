7a2882382db70256a3b9acfeb392d193556dba5a
hellertang <hellertang@tencent.com>
2022-05-10 21:03:40 +0800

    modify instance types

--- tencentcloud/basic_test.go
+++ tencentcloud/basic_test.go
@@ -202,14 +202,13 @@ data "tencentcloud_images" "default" {
 data "tencentcloud_instance_types" "default" {
   filter {
     name   = "instance-family"
-    values = ["S6"]
+    values = ["S5"]
   }
   filter {
     name   = "zone"
     values = [var.availability_cvm_zone]
   }
-  cpu_core_count = 2
-  memory_size    = 2
+  exclude_sold_out = true
 }
 `
 const defaultAzVariable = `
