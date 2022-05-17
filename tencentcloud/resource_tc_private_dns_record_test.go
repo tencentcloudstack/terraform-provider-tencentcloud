48e1c8c5fbbf4f72ac16d21e33cf8a9b23392933
hellertang <hellertang@tencent.com>
2022-05-17 16:25:29 +0800

    add private dns testcases (#1048)

---
+++ tencentcloud/resource_tc_private_dns_record_test.go
@@ -0,0 +1,54 @@
+package tencentcloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccTencentCloudPrivateDnsRecord_basic(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPrivateDnsRecord_basic,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("tencentcloud_private_dns_record.record", "weight", "1"),
+				),
+			},
+			{
+				ResourceName:      "tencentcloud_private_dns_record.record",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+const testAccPrivateDnsRecord_basic = defaultInstanceVariable + `
+resource "tencentcloud_private_dns_zone" "zone" {
+  dns_forward_status = "DISABLED"
+  domain             = "domain.com"
+  remark             = "test_record"
+
+  tag_set {
+    tag_key   = "created_by"
+    tag_value = "terraform"
+  }
+
+}
+
+resource "tencentcloud_private_dns_record" "record" {
+  mx           = 0
+  record_type  = "A"
+  record_value = "192.168.1.2"
+  sub_domain   = "www"
+  ttl          = 300
+  weight       = 1
+  zone_id      = tencentcloud_private_dns_zone.zone.id
+}
+
+`
