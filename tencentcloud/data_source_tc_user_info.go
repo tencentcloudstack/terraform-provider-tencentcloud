39293f88afb646feac41518aad8ce959c10cdc07
mikatong <mikatong@tencent.com>
2022-06-13 17:34:52 +0800

    fix: tencentcloud_user_info result add user name

--- tencentcloud/data_source_tc_user_info.go
+++ tencentcloud/data_source_tc_user_info.go
@@ -18,6 +18,7 @@ import (
 	"strconv"
 
 	cam "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/cam/v20190116"
+	"github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/internal/helper"
 	"github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/ratelimit"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
@@ -47,6 +48,11 @@ func datasourceTencentCloudUserInfo() *schema.Resource {
 				Type:        schema.TypeString,
 				Computed:    true,
 				Description: "Current account OwnerUIN.",
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Current account Name. NOTE: only support subaccount.",
 			},
 			"result_output_file": {
 				Type:        schema.TypeString,
@@ -100,7 +106,17 @@ func datasourceTencentCloudUserInfoRead(d *schema.ResourceData, meta interface{}
 	_ = d.Set("app_id", appId)
 	_ = d.Set("uin", uin)
 	_ = d.Set("owner_uin", ownerUin)
-
+	accountInfoRequest := cam.NewDescribeSubAccountsRequest()
+	accountInfoRequest.FilterSubAccountUin = []*uint64{helper.Uint64(helper.StrToUInt64(uin))}
+	accountInfoResponse, err := client.UseCamClient().DescribeSubAccounts(accountInfoRequest)
+	if err != nil {
+		return err
+	}
+	subAccounts := accountInfoResponse.Response.SubAccounts
+	if len(subAccounts) > 0 {
+		name := *subAccounts[0].Name
+		_ = d.Set("name", name)
+	}
 	output, ok := d.GetOk("result_output_file")
 	if ok && output.(string) != "" {
 		if e := writeToFile(output.(string), map[string]interface{}{
