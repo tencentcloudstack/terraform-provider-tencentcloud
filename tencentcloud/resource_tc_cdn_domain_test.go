2540d08551ac400b8d09c020fe2eb9ff11f8d47f
KGSN <ghosthcp516@hotmail.com>
2022-07-19 09:55:00 +0800

    feat: cdn support more params (#1169)

--- tencentcloud/resource_tc_cdn_domain_test.go
+++ tencentcloud/resource_tc_cdn_domain_test.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"testing"
 
+	"github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/internal/helper"
+
 	sdkErrors "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/errors"
 
 	dnspod "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/dnspod/v20210323"
@@ -78,7 +80,7 @@ func TestAccTencentCloudCdnDomainResource(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCdnDomainBasic,
-				Check: resource.ComposeTestCheckFunc(
+				Check: resource.ComposeAggregateTestCheckFunc(
 					testAccCheckCdnDomainExists("tencentcloud_cdn_domain.foo"),
 					resource.TestCheckResourceAttrSet("tencentcloud_cdn_domain.foo", "domain"),
 					resource.TestCheckResourceAttrSet("tencentcloud_cdn_domain.foo", "service_type"),
@@ -87,6 +89,13 @@ func TestAccTencentCloudCdnDomainResource(t *testing.T) {
 					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "origin.0.origin_list.#", "1"),
 					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "authentication.0.switch", "on"),
 					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "authentication.0.type_a.#", "1"),
+					// extends
+					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "status_code_cache.0.switch", "on"),
+					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "status_code_cache.0.cache_rules.#", "2"),
+					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "status_code_cache.0.cache_rules.0.status_code", "403"),
+					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "status_code_cache.0.cache_rules.0.cache_time", "5"),
+					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "status_code_cache.0.cache_rules.1.status_code", "404"),
+					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "status_code_cache.0.cache_rules.1.cache_time", "10"),
 				),
 			},
 			{
@@ -95,13 +104,103 @@ func TestAccTencentCloudCdnDomainResource(t *testing.T) {
 					testAccCheckCdnDomainExists("tencentcloud_cdn_domain.foo"),
 					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "authentication.0.switch", "on"),
 					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "authentication.0.type_c.#", "1"),
+					// extends
+					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "ip_freq_limit.0.switch", "on"),
+					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "status_code_cache.0.switch", "on"),
+					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "status_code_cache.0.cache_rules.#", "2"),
+					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "status_code_cache.0.cache_rules.0.status_code", "403"),
+					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "status_code_cache.0.cache_rules.0.cache_time", "10"),
+					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "status_code_cache.0.cache_rules.1.status_code", "404"),
+					resource.TestCheckResourceAttr("tencentcloud_cdn_domain.foo", "status_code_cache.0.cache_rules.1.cache_time", "10"),
 				),
 			},
 			{
-				ResourceName:            "tencentcloud_cdn_domain.foo",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"https_config"},
+				ResourceName:      "tencentcloud_cdn_domain.foo",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"https_config",
+					"ip_filter",
+					"ip_freq_limit",
+					"status_code_cache",
+					"compression",
+					"band_width_alert",
+					"error_page",
+					"response_header",
+					"downstream_capping",
+					"origin_pull_optimization",
+					"referer",
+					"max_age",
+					"aws_private_access",
+					"oss_private_access",
+					"hw_private_access",
+					"qn_private_access",
+				},
+			},
+		},
+	})
+}
+
+func TestAccTencentCloudCdnDomainDryRun(t *testing.T) {
+	t.Parallel()
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckCommon(t, ACCOUNT_TYPE_PREPAY)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCdnDomainDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCndDryRunDomainResource(1),
+				Check: func(state *terraform.State) error {
+					for _, rs := range state.RootModule().Resources {
+						if rs.Type != "tencentcloud_cdn_domain" {
+							continue
+						}
+
+						request := testAccCdnDryRunAddDomainRequest()
+						dryRunResult, ok := rs.Primary.Attributes["dry_run_create_result"]
+						if requestJson := request.ToJsonString(); ok && dryRunResult != requestJson {
+							log.Printf("[ERROR]: unexpected argument, top is expected, bottom is actual: \n%s\n%s", requestJson, dryRunResult)
+							return fmt.Errorf("resource %s.dry_run is no match request json", rs.Type)
+						}
+					}
+
+					return nil
+				},
+			},
+			{
+				ExpectNonEmptyPlan: true,
+				Config:             testAccCndDryRunDomainResource(2),
+				Check: func(state *terraform.State) error {
+					for _, rs := range state.RootModule().Resources {
+						if rs.Type != "tencentcloud_cdn_domain" {
+							continue
+						}
+
+						request := cdn.NewUpdateDomainConfigRequest()
+						request.IpFilter = &cdn.IpFilter{
+							Switch: helper.String("off"),
+						}
+						request.ErrorPage = &cdn.ErrorPage{
+							Switch: helper.String("on"),
+							PageRules: []*cdn.ErrorPageRule{
+								{
+									helper.IntInt64(403),
+									helper.IntInt64(302),
+									helper.String("https://www.test.com/error3.html"),
+								},
+							},
+						}
+						dryRunResult, ok := rs.Primary.Attributes["dry_run_update_result"]
+						if requestJson := request.ToJsonString(); ok && dryRunResult != requestJson {
+							log.Printf("[ERROR]: unexpected argument, top is expected, bottom is actual: \n%s\n%s", requestJson, dryRunResult)
+							return fmt.Errorf("resource %s.dry_run_update is no match request json", rs.Type)
+						}
+					}
+
+					return nil
+				},
 			},
 		},
 	})
@@ -427,6 +526,17 @@ resource "tencentcloud_cdn_domain" "foo" {
       backup_secret_key = "ujU4vsH3jbLzAg2DTUoTLj"
     }
   }
+  status_code_cache {
+    switch = "on"
+    cache_rules {
+      status_code = "403"
+      cache_time = 5
+    }
+    cache_rules {
+      status_code = "404"
+      cache_time = 10
+    }
+  }
 }
 `
 
@@ -453,8 +563,445 @@ resource "tencentcloud_cdn_domain" "foo" {
       backup_secret_key = "BQ6yVr"
     }
   }
+  ip_freq_limit {
+    switch = "on"
+    qps = 50
+  }
+  status_code_cache {
+    switch = "on"
+    cache_rules {
+      status_code = "403"
+      cache_time = 10
+    }
+    cache_rules {
+      status_code = "404"
+      cache_time = 10
+    }
+  }
 }
 `
+
+func testAccCndDryRunDomainResource(stage int) string {
+	ipFilter := `
+switch = "on"
+filter_type = "blacklist"
+filters = ["10.0.3.0/24"]
+filter_rules {
+  filter_type = "whitelist"
+  filters = ["10.0.2.0/24"]
+  rule_type = "all"
+  rule_paths = ["*"]
+}
+filter_rules {
+  filter_type = "blacklist"
+  filters = ["10.0.1.0/24"]
+  rule_type = "file"
+  rule_paths = ["txt"]
+}
+return_code = 404
+	`
+	errorPage := `
+switch = "on"
+page_rules {
+  status_code = 403
+  redirect_code = 302
+  redirect_url = "https://www.test.com/error.html"
+}
+page_rules {
+  status_code = 404
+  redirect_code = 301
+  redirect_url = "https://www.test2.com/error2.html"
+}
+	`
+
+	if stage == 2 {
+		ipFilter = `switch = "off"`
+		errorPage = `
+switch = "on"
+page_rules {
+  status_code = 403
+  redirect_code = 302
+  redirect_url = "https://www.test.com/error3.html"
+}
+`
+	}
+	return fmt.Sprintf(`
+%s
+resource "tencentcloud_cdn_domain" "dry_run" {
+
+  explicit_using_dry_run = true
+
+  domain = "www.example1.com"
+  service_type = "web"
+  area = "overseas"
+  origin {
+    origin_type          = "cos"
+    origin_list          = ["test-1231231231.mycloud.com"]
+    server_name			 = "test-1231231231.mycloud.com"
+    origin_pull_protocol = "follow"
+  }
+  ip_filter { %s }
+  ip_freq_limit {
+    switch = "on"
+    qps = 50
+  }
+  status_code_cache {
+    switch = "on"
+    cache_rules {
+      status_code = "403"
+      cache_time = 5
+    }
+    cache_rules {
+      status_code = "404"
+      cache_time = 10
+    }
+  }
+  compression {
+    switch = "on"
+    compression_rules {
+      compress = true
+      min_length = 200
+      max_length = 2000
+      algorithms = ["gzip"]
+      file_extensions = ["png", "js"]
+      rule_type = "directory"
+      rule_paths = ["/assets"]
+    }
+    compression_rules {
+      compress = true
+      min_length = 300
+      max_length = 4000
+      algorithms = ["gzip"]
+      file_extensions = [".css", "js"]
+      rule_type = "directory"
+      rule_paths = ["/assets2"]
+    }
+  }
+  band_width_alert {
+    switch = "on"
+    bps_threshold = 1024*1024*30
+    counter_measure = "RETURN_404"
+    alert_switch = "on"
+    alert_percentage = 90
+    metric = "bandwidth"
+  }
+  error_page { %s }
+  response_header {
+    switch = "on"
+    header_rules {
+      header_mode = "set"
+      header_name = "x-foo-header"
+      header_value = "v1"
+      rule_type = "directory"
+      rule_paths = ["/xxx/test/"]
+    }
+    header_rules {
+      header_mode = "add"
+      header_name = "x-bar-header"
+      header_value = "v2"
+      rule_type = "file"
+      rule_paths = ["jpg"]
+    }
+  }
+  downstream_capping {
+    switch = "on"
+    capping_rules {
+      rule_type = "all"
+      rule_paths = ["*"]
+      kbps_threshold = 300
+    }
+    capping_rules {
+      rule_type = "path"
+      rule_paths = ["/data/d.html"]
+      kbps_threshold = 400
+    }
+  }
+  origin_pull_optimization {
+    switch = "on"
+    optimization_type = "CNToOV"
+  }
+  referer {
+    switch = "on"
+    referer_rules {
+      rule_type = "all"
+      rule_paths = ["*"]
+      referer_type = "blacklist"
+      referers = ["www.example123.com"]
+      allow_empty = true
+    }
+  }
+  max_age {
+    switch = "on"
+    max_age_rules {
+      max_age_type = "all"
+      max_age_contents = ["*"]
+      max_age_time = 3600
+      follow_origin = "on"
+    }
+    max_age_rules {
+      max_age_type = "path"
+      max_age_contents = ["/a/b.html"]
+      max_age_time = 7200
+      follow_origin = "off"
+    }
+  }
+
+  specific_config_mainland = <<-EOF
+{
+  "Seo": { "Switch": "on" }
+}
+EOF
+  specific_config_overseas = <<-EOF
+{
+  "FollowRedirect": { "Switch": "on" }
+}
+EOF
+
+  origin_pull_timeout {
+    connect_timeout = 10
+    receive_timeout = 10
+  }
+
+  response_header_cache_switch = "on"
+  seo_switch = "on"
+  video_seek_switch = "on"
+  offline_cache_switch = "on"
+  quic_switch = "on"
+}
+`, testAccDomainCosForCDN, ipFilter, errorPage)
+}
+
+func testAccCdnDryRunAddDomainRequest() *cdn.AddCdnDomainRequest {
+	on := helper.String("on")
+	off := helper.String("off")
+	request := &cdn.AddCdnDomainRequest{
+		Domain:      helper.String("www.example1.com"),
+		ServiceType: helper.String("web"),
+		Area:        helper.String("overseas"),
+		ProjectId:   helper.IntInt64(0),
+		Origin: &cdn.Origin{
+			OriginType: helper.String("cos"),
+			Origins: []*string{
+				helper.String("test-1231231231.mycloud.com"),
+			},
+			ServerName:         helper.String("test-1231231231.mycloud.com"),
+			OriginPullProtocol: helper.String("follow"),
+			CosPrivateAccess:   off,
+		},
+		IpFilter: &cdn.IpFilter{
+			Switch:     on,
+			ReturnCode: helper.IntInt64(404),
+			FilterType: helper.String("blacklist"),
+			Filters: []*string{
+				helper.String("10.0.3.0/24"),
+			},
+			FilterRules: []*cdn.IpFilterPathRule{
+				{
+					FilterType: helper.String("whitelist"),
+					Filters: []*string{
+						helper.String("10.0.2.0/24"),
+					},
+					RuleType: helper.String("all"),
+					RulePaths: []*string{
+						helper.String("*"),
+					},
+				},
+				{
+					FilterType: helper.String("blacklist"),
+					Filters: []*string{
+						helper.String("10.0.1.0/24"),
+					},
+					RuleType: helper.String("file"),
+					RulePaths: []*string{
+						helper.String("txt"),
+					},
+				},
+			},
+		},
+		IpFreqLimit: &cdn.IpFreqLimit{
+			Switch: on,
+			Qps:    helper.IntInt64(50),
+		},
+		StatusCodeCache: &cdn.StatusCodeCache{
+			Switch: on,
+			CacheRules: []*cdn.StatusCodeCacheRule{
+				{
+					StatusCode: helper.String("403"),
+					CacheTime:  helper.IntInt64(5),
+				},
+				{
+					StatusCode: helper.String("404"),
+					CacheTime:  helper.IntInt64(10),
+				},
+			},
+		},
+		Compression: &cdn.Compression{
+			Switch: on,
+			CompressionRules: []*cdn.CompressionRule{
+				{
+					Compress:  helper.Bool(true),
+					MinLength: helper.IntInt64(200),
+					MaxLength: helper.IntInt64(2000),
+					Algorithms: []*string{
+						helper.String("gzip"),
+					},
+					FileExtensions: []*string{
+						helper.String("png"),
+						helper.String("js"),
+					},
+					RuleType: helper.String("directory"),
+					RulePaths: []*string{
+						helper.String("/assets"),
+					},
+				},
+				{
+					Compress:  helper.Bool(true),
+					MinLength: helper.IntInt64(300),
+					MaxLength: helper.IntInt64(4000),
+					Algorithms: []*string{
+						helper.String("gzip"),
+					},
+					FileExtensions: []*string{
+						helper.String(".css"),
+						helper.String("js"),
+					},
+					RuleType: helper.String("directory"),
+					RulePaths: []*string{
+						helper.String("/assets2"),
+					},
+				},
+			},
+		},
+		BandwidthAlert: &cdn.BandwidthAlert{
+			Switch:          on,
+			BpsThreshold:    helper.IntInt64(1024 * 1024 * 30),
+			CounterMeasure:  helper.String("RETURN_404"),
+			AlertSwitch:     on,
+			AlertPercentage: helper.IntInt64(90),
+			Metric:          helper.String("bandwidth"),
+		},
+		ErrorPage: &cdn.ErrorPage{
+			Switch: on,
+			PageRules: []*cdn.ErrorPageRule{
+				{
+					StatusCode:   helper.IntInt64(403),
+					RedirectCode: helper.IntInt64(302),
+					RedirectUrl:  helper.String("https://www.test.com/error.html"),
+				},
+				{
+					StatusCode:   helper.IntInt64(404),
+					RedirectCode: helper.IntInt64(301),
+					RedirectUrl:  helper.String("https://www.test2.com/error2.html"),
+				},
+			},
+		},
+		ResponseHeader: &cdn.ResponseHeader{
+			Switch: on,
+			HeaderRules: []*cdn.HttpHeaderPathRule{
+				{
+					HeaderMode:  helper.String("set"),
+					HeaderName:  helper.String("x-foo-header"),
+					HeaderValue: helper.String("v1"),
+					RuleType:    helper.String("directory"),
+					RulePaths: []*string{
+						helper.String("/xxx/test/"),
+					},
+				},
+				{
+					HeaderMode:  helper.String("add"),
+					HeaderName:  helper.String("x-bar-header"),
+					HeaderValue: helper.String("v2"),
+					RuleType:    helper.String("file"),
+					RulePaths: []*string{
+						helper.String("jpg"),
+					},
+				},
+			},
+		},
+		DownstreamCapping: &cdn.DownstreamCapping{
+			Switch: on,
+			CappingRules: []*cdn.CappingRule{
+				{
+					RuleType: helper.String("all"),
+					RulePaths: []*string{
+						helper.String("*"),
+					},
+					KBpsThreshold: helper.IntInt64(300),
+				},
+				{
+					RuleType: helper.String("path"),
+					RulePaths: []*string{
+						helper.String("/data/d.html"),
+					},
+					KBpsThreshold: helper.IntInt64(400),
+				},
+			},
+		},
+		OriginPullOptimization: &cdn.OriginPullOptimization{
+			Switch:           on,
+			OptimizationType: helper.String("CNToOV"),
+		},
+		Referer: &cdn.Referer{
+			Switch: on,
+			RefererRules: []*cdn.RefererRule{
+				{
+					RuleType: helper.String("all"),
+					RulePaths: []*string{
+						helper.String("*"),
+					},
+					RefererType: helper.String("blacklist"),
+					Referers: []*string{
+						helper.String("www.example123.com"),
+					},
+					AllowEmpty: helper.Bool(true),
+				},
+			},
+		},
+		MaxAge: &cdn.MaxAge{
+			Switch: on,
+			MaxAgeRules: []*cdn.MaxAgeRule{
+				{
+					MaxAgeType: helper.String("all"),
+					MaxAgeContents: []*string{
+						helper.String("*"),
+					},
+					MaxAgeTime:   helper.IntInt64(3600),
+					FollowOrigin: on,
+				},
+				{
+					MaxAgeType: helper.String("path"),
+					MaxAgeContents: []*string{
+						helper.String("/a/b.html"),
+					},
+					MaxAgeTime:   helper.IntInt64(7200),
+					FollowOrigin: off,
+				},
+			},
+		},
+		SpecificConfig: &cdn.SpecificConfig{
+			Overseas: &cdn.OverseaConfig{
+				FollowRedirect: &cdn.FollowRedirect{Switch: on},
+			},
+			Mainland: &cdn.MainlandConfig{
+				Seo: &cdn.Seo{Switch: on},
+			},
+		},
+		OriginPullTimeout: &cdn.OriginPullTimeout{
+			ConnectTimeout: helper.IntUint64(10),
+			ReceiveTimeout: helper.IntUint64(10),
+		},
+		ResponseHeaderCache: &cdn.ResponseHeaderCache{Switch: on},
+		Seo:                 &cdn.Seo{Switch: on},
+		VideoSeek:           &cdn.VideoSeek{Switch: on},
+		OfflineCache:        &cdn.OfflineCache{Switch: on},
+		Quic:                &cdn.Quic{Switch: on},
+		Ipv6Access:          &cdn.Ipv6Access{Switch: off},
+		RangeOriginPull:     &cdn.RangeOriginPull{Switch: on}, // by argument default
+		FollowRedirect:      &cdn.FollowRedirect{Switch: off}, // by argument default
+		CacheKey:            &cdn.CacheKey{FullUrlCache: on},  // by argument default
+	}
+	return request
+}
 
 const testAccSSLForCDN = `
 data "tencentcloud_ssl_certificates" "foo" {
