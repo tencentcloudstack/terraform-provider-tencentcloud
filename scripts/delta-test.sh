3727bca140cda680d54cfa4f67d517e2b5bbced3
tongyiming <1290863722@qq.com>
2022-07-25 10:45:43 +0800

    fix: mr action (#1180)
    Co-authored-by: mikatong <mikatong@tencent.com>

--- scripts/delta-test.sh
+++ scripts/delta-test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #service files
-update_services=`git diff --name-status origin/master| awk '{print $2}' | grep "^tencentcloud/" | xargs egrep "type .+Service struct" | awk '{print $2}'`
+update_service_functions=`git diff --name-status origin/master| awk '{print $2}' | grep "^tencentcloud/service*" | xargs git diff | grep "@@" | grep "func" | awk -F ")" '{print $2}' | awk -F "(" '{print $1}' | tr -d ' '`
 need_test_files=""
-for update_service in $update_services; do
-    tmp_files=`grep -r --with-filename $update_service ./tencentcloud | awk -F ":" '{print $1}' | grep -v "service_tencent*" | awk -F "/" '{print $3}' | sort | uniq | egrep "^resource_tc_|^data_source_tc" | awk -F "." '{print $1}' | awk '/_test$/{print "tencentcloud/"$0".go"} !/_test$/{print "tencentcloud/"$0"_test.go"}'`
+for update_service_function in $update_service_functions; do
+    tmp_files=`grep -r --with-filename $update_service_function ./tencentcloud | awk -F ":" '{print $1}' | grep -v "service_tencent*" | awk -F "/" '{print $3}' | sort | uniq | egrep "^resource_tc_|^data_source_tc" | awk -F "." '{print $1}' | awk '/_test$/{print "tencentcloud/"$0".go"} !/_test$/{print "tencentcloud/"$0"_test.go"}'`
     need_test_files="$need_test_files $tmp_files"
 done
 
@@ -17,10 +17,9 @@ delta_test_files="$delta_test_files $need_test_files $update_sources"
 delta_test_files=`echo $delta_test_files | xargs -n1 | sort | uniq`
 for delta_test_file in ${delta_test_files}; do
     test_casts=`egrep "func TestAcc.+\(" ${delta_test_file} | awk -F "(" '{print $1}' | awk '{print $2}' | grep -v "NeedFix"`
-    echo "[$delta_test_file]: $test_casts"
+    echo "[$delta_test_file] \n$test_casts"
     for test_cast in ${test_casts}; do
         go_test_cmd="go test -v -run ${test_cast} -timeout=0 ./tencentcloud/"
-        echo ${go_test_cmd}
         $go_test_cmd
         if [ $? -ne 0 ]; then
             printf "[GO TEST FILED] ${go_test_cmd}"
