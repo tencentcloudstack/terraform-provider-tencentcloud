fa7e1bd9492237dfe2e16000841cf17b31a1de1b
hellertang <hellertang@tencent.com>
2022-07-25 21:24:01 +0800

    add need fix

--- tencentcloud/data_source_tc_vpn_gateway_routes_test.go
+++ tencentcloud/data_source_tc_vpn_gateway_routes_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-func TestAccTencentCloudVpnGatewayRoutesDataSource(t *testing.T) {
+func TestAccTencentCloudNeedFixVpnGatewayRoutesDataSource(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
