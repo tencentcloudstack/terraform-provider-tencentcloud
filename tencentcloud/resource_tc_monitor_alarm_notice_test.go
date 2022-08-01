49569e18b7f15ab7e7fbdbae8d03584f0bade514
LH1029 <1099270795@qq.com>
2022-08-01 16:27:13 +0800

    feat: add resource alarm-notices

---
+++ tencentcloud/resource_tc_monitor_alarm_notice_test.go
@@ -0,0 +1,33 @@
+package tencentcloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccTencentCloudMonitorAlarmNoticeResource(t *testing.T) {
+	t.Parallel()
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccMonitorAlarmNotice,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("tencentcloud_monitor_alarm_notice.example", "name", "test_alarm_notice_123"),
+				),
+			},
+		},
+	})
+}
+
+const testAccMonitorAlarmNotice string = `
+resource "tencentcloud_monitor_alarm_notice" "example" {
+  module                = "monitor"
+  name                  = "test_alarm_notice_123"
+  notice_type           = "ALL"
+  notice_language       = "zh-CN"
+
+}
+`
