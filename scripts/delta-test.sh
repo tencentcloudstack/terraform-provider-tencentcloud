c9e9f57e1c465a78b49ee0a07259a1e65ee38ec8
tongyiming <1290863722@qq.com>
2022-08-18 17:43:06 +0800

    Update delta-test.sh

--- scripts/delta-test.sh
+++ scripts/delta-test.sh
@@ -4,19 +4,24 @@ range_sha=${BASE_SHA}
 echo $(git diff --name-status ${range_sha} | awk '{print $2}')
 #service files
 update_service_functions=`git diff --name-status ${range_sha} | awk '{print $2}' | grep "^tencentcloud/service*" | xargs git diff ${range_sha} | grep "@@" | grep "func" | awk -F ")" '{print $2}' | awk -F "(" '{print $1}' | tr -d ' '`
+echo "update_service_functions: $update_service_functions"
 need_test_files=""
 for update_service_function in $update_service_functions; do
     tmp_files=`grep -r --with-filename $update_service_function ./tencentcloud | awk -F ":" '{print $1}' | grep -v "service_tencent*" | awk -F "/" '{print $3}' | sort | uniq | egrep "^resource_tc_|^data_source_tc" | awk -F "." '{print $1}' | awk '/_test$/{print "tencentcloud/"$0".go"} !/_test$/{print "tencentcloud/"$0"_test.go"}'`
     need_test_files="$need_test_files $tmp_files"
 done
+echo "need_test_files: $need_test_files"
 
 # resource&&data_source files
 update_sources=`git diff --name-status ${range_sha}| awk '{print $2}' | egrep "^tencentcloud/resource_tc|^tencentcloud/data_source" | egrep -v "_test.go" | awk -F "." '{print $1"_test.go"}'`
+echo "update_sources: $update_sources"
 # test files
 delta_test_files=`git diff --name-status ${range_sha} | egrep "_test\.go$" | awk '{print $2}'`
+echo "delta_test_files: $delta_test_files"
 # all test files
 delta_test_files="$delta_test_files $need_test_files $update_sources"
 delta_test_files=`echo $delta_test_files | xargs -n1 | sort | uniq`
+echo "all delta_test_files: $delta_test_files"
 for delta_test_file in ${delta_test_files}; do
     test_casts=`egrep "func TestAcc.+\(" ${delta_test_file} | awk -F "(" '{print $1}' | awk '{print $2}' | grep -v "NeedFix"`
     echo "[$delta_test_file] \n$test_casts"
