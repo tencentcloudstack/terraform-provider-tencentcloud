a5633b2917f3e431bd9f13b81361d67140b9666f
LH1029 <83651954+LH1029@users.noreply.github.com>
2022-08-19 14:36:56 +0800

    Delete outputs.tf

--- generated/tencentcloud/vpc/ap-guangzhou/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "tencentcloud_vpc_tfer--ci-temp-test-updated_vpc-8beinccp_id" {
-  value = "${tencentcloud_vpc.tfer--ci-temp-test-updated_vpc-8beinccp.id}"
-}
-
-output "tencentcloud_vpc_tfer--test-import-1_vpc-m3k357gn_id" {
-  value = "${tencentcloud_vpc.tfer--test-import-1_vpc-m3k357gn.id}"
-}
