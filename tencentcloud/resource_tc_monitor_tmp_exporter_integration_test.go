084ef840a38fc1718d2a0a5eb4421e709f6c2fc8
arunma <arunma@tencent.com>
2022-08-25 15:55:08 +0800

    fix: update exporter integration test

--- tencentcloud/resource_tc_monitor_tmp_exporter_integration_test.go
+++ tencentcloud/resource_tc_monitor_tmp_exporter_integration_test.go
@@ -22,7 +22,7 @@ func TestAccTencentCloudMonitorExporterIntegration_basic(t *testing.T) {
 					testAccCheckExporterIntegrationExists("tencentcloud_monitor_tmp_exporter_integration.basic"),
 					resource.TestCheckResourceAttr("tencentcloud_monitor_tmp_exporter_integration.basic", "kind", "cvm-http-sd-exporter"),
 					resource.TestCheckResourceAttr("tencentcloud_monitor_tmp_exporter_integration.basic", "kube_type", "1"),
-					resource.TestCheckResourceAttr("tencentcloud_monitor_tmp_exporter_integration.basic", "cluster_id", "cls-ely08ic4"),
+					resource.TestCheckResourceAttr("tencentcloud_monitor_tmp_exporter_integration.basic", "cluster_id", "cls-87o4klby"),
 				),
 			},
 			{
@@ -94,9 +94,6 @@ const testExporterIntegrationVar = `
 variable "prometheus_id" {
   default = "` + defaultPrometheusId + `"
 }
-variable "default_cluster" {
-  default = "` + defaultTkeClusterId + `"
-}
 variable "cluster_id" {
   default = "` + tkeClusterIdAgent + `"
 }
@@ -107,7 +104,7 @@ resource "tencentcloud_monitor_tmp_exporter_integration" "basic" {
   kind 			= "cvm-http-sd-exporter"
   content		= "{\"kind\":\"cvm-http-sd-exporter\",\"spec\":{\"job\":\"job_name: example-job-name\\nmetrics_path: /metrics\\ncvm_sd_configs:\\n- region: ap-guangzhou\\n  ports:\\n  - 9100\\n  filters:         \\n  - name: tag:示例标签键\\n    values: \\n    - 示例标签值\\nrelabel_configs: \\n- source_labels: [__meta_cvm_instance_state]\\n  regex: RUNNING\\n  action: keep\\n- regex: __meta_cvm_tag_(.*)\\n  replacement: $1\\n  action: labelmap\\n- source_labels: [__meta_cvm_region]\\n  target_label: region\\n  action: replace\"}}"
   kube_type		= 1
-  cluster_id	= var.default_cluster
+  cluster_id	= var.cluster_id
 }`
 
 const testExporterIntegration_update = testExporterIntegrationVar + `
