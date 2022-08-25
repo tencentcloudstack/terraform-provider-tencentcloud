cc881b1d6036f5987d325ea2136da1da57737e97
hellertang <hellertang@tencent.com>
2022-08-25 12:24:15 +0800

    update doc

--- tencentcloud/provider.go
+++ tencentcloud/provider.go
@@ -651,7 +651,6 @@ Lighthouse
   Resource
 	tencentcloud_lighthouse_instance
 
-<<<<<<< HEAD
 TEM
   Resource
 	tencentcloud_tem_environment
@@ -661,8 +660,7 @@ TEM
 	tencentcloud_tem_log_config
 	tencentcloud_tem_scale_rule
 	tencentcloud_tem_gateway
-=======
->>>>>>> f10ed3ebb98bbd66bb8c210527181fc83a893e66
+
 Teo
   Resource
 	tencentcloud_teo_zone
