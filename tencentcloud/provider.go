99df7ee9fff35f04421d9d58038ddbb44d1f4f4a
arunma <arunma@tencent.com>
2022-10-13 10:49:57 +0800

    feat: support tcm

--- tencentcloud/provider.go
+++ tencentcloud/provider.go
@@ -693,6 +693,11 @@ TencentCloud EdgeOne(TEO)
 	tencentcloud_teo_ddos_policy
 	tencentcloud_teo_security_policy
 	tencentcloud_teo_custom_error_page
+
+TencentCloud ServiceMesh(TCM)
+  Resource
+	tencentcloud_tcm_mesh
+
 */
 package tencentcloud
 
@@ -1246,6 +1251,7 @@ func Provider() terraform.ResourceProvider {
 			"tencentcloud_teo_custom_error_page":                    resourceTencentCloudTeoCustomErrorPage(),
 			// "tencentcloud_teo_host_certificate":                     resourceTencentCloudTeoHostCertificate(),
 			// "tencentcloud_teo_default_certificate":                  resourceTencentCloudTeoDefaultCertificate(),
+			"tencentcloud_tcm_mesh": resourceTencentCloudTcmMesh(),
 		},
 
 		ConfigureFunc: providerConfigure,
