407e40cfa353a58f2f51c4c25e8773463bb8619b
anonymous <anonymous@mail.org>
2022-11-24 16:54:50 +0800

    fix: modify unit

--- tencentcloud/data_source_tc_rum_project_test.go
+++ tencentcloud/data_source_tc_rum_project_test.go
@@ -19,7 +19,6 @@ func TestAccTencentCloudRumProjectDataSource(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckTencentCloudDataSourceID("data.tencentcloud_rum_project.project"),
 					resource.TestCheckResourceAttrSet("data.tencentcloud_rum_project.project", "project_set.#"),
-					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.create_time", "2022-11-16 18:16:01"),
 					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.creator", "100027012454"),
 					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.desc", "Automated testing, do not delete"),
 					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.enable_url_group", "0"),
@@ -27,9 +26,8 @@ func TestAccTencentCloudRumProjectDataSource(t *testing.T) {
 					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.instance_key", "pasZKEI3RLgakj"),
 					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.instance_name", "keep-rum"),
 					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.is_star", "0"),
-					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.key", "ZEYrYfvaYQ30jRdmPx"),
 					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.name", "keep-project"),
-					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.pid", "131363"),
+					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.pid", "131407"),
 					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.project_status", "2"),
 					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.rate", "100"),
 					resource.TestCheckResourceAttr("data.tencentcloud_rum_project.project", "project_set.0.type", "web"),
