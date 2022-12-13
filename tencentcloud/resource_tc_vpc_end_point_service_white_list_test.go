a94151ab81b70da9642ea1343c734067574f9e2b
hellertang <hellertang@tencent.com>
2022-12-13 12:10:15 +0800

    update test name

--- tencentcloud/resource_tc_vpc_end_point_service_white_list_test.go
+++ tencentcloud/resource_tc_vpc_end_point_service_white_list_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-func TestTencentCloudVpcEndPointServiceWhiteListResource_basic(t *testing.T) {
+func TestAccTencentCloudVpcEndPointServiceWhiteListResource_basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
