7e5f96edca28f70cd63bf09f1540d8ae5684c809
hellertang <hellertang@tencent.com>
2022-12-13 11:56:20 +0800

    add endpoint

--- tencentcloud/resource_tc_cvm_hpc_cluster_test.go
+++ tencentcloud/resource_tc_cvm_hpc_cluster_test.go
@@ -1,8 +1,9 @@
 package tencentcloud
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
 func TestAccTencentCloudNeedFixCvmHpcClusterResource_basic(t *testing.T) {
