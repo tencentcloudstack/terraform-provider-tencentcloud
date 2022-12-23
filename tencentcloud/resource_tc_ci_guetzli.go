73c6f569750338b10455ecb1e06be5378e87f07c
ritchiecai <ritchiecai@tencent.com>
2022-12-23 21:13:19 +0800

    chore: update doc string

--- tencentcloud/resource_tc_ci_guetzli.go
+++ tencentcloud/resource_tc_ci_guetzli.go
@@ -11,6 +11,15 @@ resource "tencentcloud_ci_guetzli" "foo" {
 }
 
 ```
+
+Import
+
+Resource guetzli can be imported using the id, e.g.
+
+```
+$ terraform import tencentcloud_ci_guetzli.example examplebucket-1250000000
+```
+
 */
 package tencentcloud
 
