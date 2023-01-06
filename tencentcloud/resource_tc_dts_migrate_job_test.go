7519ed79f32a28ea3ba1070f875a63e9b1faec6b
nickyinluo <nickyinluo@tencent.com>
2023-01-06 15:44:46 +0800

    modify migrate job

--- tencentcloud/resource_tc_dts_migrate_job_test.go
+++ tencentcloud/resource_tc_dts_migrate_job_test.go
@@ -85,7 +85,52 @@ func testAccCheckDtsMigrateJobExists(re string) resource.TestCheckFunc {
 	}
 }
 
-const testAccDtsMigrateJob_basic = `
+const testAccDtsCynosdb_mysql = `
+resource "tencentcloud_cynosdb_cluster" "foo" {
+	available_zone               = var.availability_zone
+	vpc_id                       = var.my_vpc
+	subnet_id                    = var.my_subnet
+	db_type                      = "MYSQL"
+	db_version                   = "5.7"
+	storage_limit                = 1000
+	cluster_name                 = "tf-cynosdb"
+	password                     = "cynos@123"
+	instance_maintain_duration   = 3600
+	instance_maintain_start_time = 10800
+	instance_maintain_weekdays   = [
+	  "Fri",
+	  "Mon",
+	  "Sat",
+	  "Sun",
+	  "Thu",
+	  "Wed",
+	  "Tue",
+	]
+  
+	instance_cpu_core    = 1
+	instance_memory_size = 2
+	param_items {
+	  name = "character_set_server"
+	  current_value = "utf8"
+	}
+	param_items {
+	  name = "time_zone"
+	  current_value = "+09:00"
+	}
+  
+	force_delete = true
+  
+	rw_group_sg = [
+	  local.sg_id
+	]
+	ro_group_sg = [
+	  local.sg_id
+	]
+	prarm_template_id = var.my_param_template
+  }
+`
+
+const testAccDtsMigrateJob_basic = testAccDtsCynosdb_mysql + `
 
 resource "tencentcloud_dts_migrate_service" "service" {
 	src_database_type = "mysql"
