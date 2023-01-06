ca1bc9cdbb20b14fc415aa7279decbffcea56993
anonymous <anonymous@mail.org>
2023-01-06 11:03:39 +0800

    fix: modify protocol description

--- tencentcloud/resource_tc_tem_application_service.go
+++ tencentcloud/resource_tc_tem_application_service.go
@@ -99,7 +99,7 @@ func resourceTencentCloudTemApplicationService() *schema.Resource {
 									"protocol": {
 										Type:        schema.TypeString,
 										Optional:    true,
-										Description: "udp or TCP.",
+										Description: "UDP or TCP.",
 									},
 								},
 							},
