8f7d2a97d948ebba9464e96327e670f2bc5c9308
Sora Morimoto <sora@morimoto.io>
2023-01-27 03:38:28 +0900

    Simplify the gofmtcheck.sh script
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- scripts/gofmtcheck.sh
+++ scripts/gofmtcheck.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Check gofmt
-echo "==> Checking that code complies with gofmt requirements..."
-gofmt_files=$(gofmt -l `find tencentcloud -name '*.go' | grep -v vendor`)
-if [[ -n ${gofmt_files} ]]; then
-    echo 'gofmt needs running on the following files:'
-    echo "${gofmt_files}"
-    echo "You can use the command: \`make fmt\` to reformat code."
-    exit 1
+# Check goimports
+echo "==> Checking that code complies with fmt requirements..."
+goimports_files=$(goimports -l tencentcloud)
+if [[ -n ${goimports_files} ]]; then
+  echo 'fmt needs running on the following files:'
+  echo "${goimports_files}"
+  echo "You can use the command: \`make fmt\` to reformat code."
+  exit 1
 fi
 
 exit 0
