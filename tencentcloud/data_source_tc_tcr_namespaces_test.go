707b66a779c4165720dbbb33f5522ebdb3f86eeb
nickyinluo <nickyinluo@tencent.com>
2023-03-01 15:04:45 +0800

    adjust tcr ns e2e case

--- tencentcloud/data_source_tc_tcr_namespaces_test.go
+++ tencentcloud/data_source_tc_tcr_namespaces_test.go
@@ -19,7 +19,7 @@ func TestAccTencentCloudTCRNamespacesData(t *testing.T) {
 				Config: testAccTencentCloudDataTCRNamespacesBasic,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet(testDataTCRNamespacesNameAll, "namespace_list.0.name"),
-					resource.TestCheckResourceAttr(testDataTCRNamespacesNameAll, "namespace_list.0.is_public", "false"),
+					resource.TestCheckResourceAttrSet(testDataTCRNamespacesNameAll, "namespace_list.0.is_public"), // we only need to care whether the value is set or not, rather than the exact value itself, and this value of public cannot be confirmed when the e2e case parallel running
 				),
 			},
 		},
