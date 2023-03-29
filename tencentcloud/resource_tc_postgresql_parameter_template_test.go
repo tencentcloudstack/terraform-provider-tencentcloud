69fdedcc24e79ec48eec0b0fe4189b8e74676d6a
nickyinluo <nickyinluo@tencent.com>
2023-03-29 18:40:40 +0800

    adjust e2e case for changing single attribute

--- tencentcloud/resource_tc_postgresql_parameter_template_test.go
+++ tencentcloud/resource_tc_postgresql_parameter_template_test.go
@@ -64,21 +64,53 @@ func TestAccTencentCloudPostgresqlParameterTemplateResource_basic(t *testing.T) 
 					resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "template_name", "tf_test_pg_temp"),
 					resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "db_major_version", "13"),
 					resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "db_engine", "postgresql"),
+					resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "template_description", "For_tf_test"),
 					resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.#", "2"),
-					// resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.0.name", "lc_time"),
-					// resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.0.expected_value", "POSIX"),
+					// resource.TestCheckTypeSetElemAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.*",
+					// 	map[string]string{
+					// 		"name":           "lc_time",
+					// 		"expected_value": "POSIX",
+					// 	}),
+					// resource.TestCheckTypeSetElemAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.*",
+					// 	map[string]string{
+					// 		"name":           "timezone",
+					// 		"expected_value": "PRC",
+					// 	}),
 				),
 			},
 			{
-				Config: testAccPostgresqlParameterTemplate_update,
+				Config: testAccPostgresqlParameterTemplate_update_desc,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckPostgresqlParameterTemplateExists("tencentcloud_postgresql_parameter_template.parameter_template"),
 					resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.#", "2"),
-					// resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.0.name", "timezone"),
-					// resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.0.expected_value", "UTC"),
-					// resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.1.name", "lock_timeout"),
-					// resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.1.expected_value", "123"),
+					resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "template_description", "For_tf_test_desc_changed"),
+				),
+			},
+			{
+				Config: testAccPostgresqlParameterTemplate_update_name,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPostgresqlParameterTemplateExists("tencentcloud_postgresql_parameter_template.parameter_template"),
+					resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.#", "2"),
+					resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "template_name", "tf_test_pg_temp_name_changed"),
+				),
+			},
+			{
+				Config: testAccPostgresqlParameterTemplate_update_multiple_attr,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPostgresqlParameterTemplateExists("tencentcloud_postgresql_parameter_template.parameter_template"),
+					resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.#", "2"),
+					// resource.TestCheckTypeSetElemAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.*",
+					// 	map[string]string{
+					// 		"name":           "timezone",
+					// 		"expected_value": "UTC",
+					// 	}),
+					// resource.TestCheckTypeSetElemAttr("tencentcloud_postgresql_parameter_template.parameter_template", "modify_param_entry_set.*",
+					// 	map[string]string{
+					// 		"name":           "lock_timeout",
+					// 		"expected_value": "123",
+					// 	}),
 					resource.TestCheckResourceAttr("tencentcloud_postgresql_parameter_template.parameter_template", "delete_param_set.#", "1"),
+					// resource.TestCheckTypeSetElemAttr("tencentcloud_postgresql_parameter_template.parameter_template", "delete_param_set.*", "lc_time"),
 				),
 			},
 			{
@@ -153,13 +185,53 @@ resource "tencentcloud_postgresql_parameter_template" "parameter_template" {
 
 `
 
-const testAccPostgresqlParameterTemplate_update = `
+const testAccPostgresqlParameterTemplate_update_desc = `
 
 resource "tencentcloud_postgresql_parameter_template" "parameter_template" {
-  template_name = "tf_test_pg_temp_updated"
+  template_name = "tf_test_pg_temp"
   db_major_version = "13"
   db_engine = "postgresql"
-  template_description = "For_tf_test_updated"
+  template_description = "For_tf_test_desc_changed"
+
+  modify_param_entry_set {
+	name = "lc_time"
+	expected_value = "POSIX"
+  }
+  modify_param_entry_set {
+	name = "timezone"
+	expected_value = "PRC"
+  }
+}
+
+`
+
+const testAccPostgresqlParameterTemplate_update_name = `
+
+resource "tencentcloud_postgresql_parameter_template" "parameter_template" {
+  template_name = "tf_test_pg_temp_name_changed"
+  db_major_version = "13"
+  db_engine = "postgresql"
+  template_description = "For_tf_test_desc_changed"
+
+  modify_param_entry_set {
+	name = "lc_time"
+	expected_value = "POSIX"
+  }
+  modify_param_entry_set {
+	name = "timezone"
+	expected_value = "PRC"
+  }
+}
+
+`
+
+const testAccPostgresqlParameterTemplate_update_multiple_attr = `
+
+resource "tencentcloud_postgresql_parameter_template" "parameter_template" {
+  template_name = "tf_test_pg_temp_name_multi_changed"
+  db_major_version = "13"
+  db_engine = "postgresql"
+  template_description = "For_tf_test_desc_multi_changed"
 
   modify_param_entry_set {
 	name = "timezone"
