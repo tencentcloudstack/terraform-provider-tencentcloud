2aff6d8dd8896f8821389b70f0f5af9390be5070
mikatong <mikatong@tencent.com>
2023-05-08 14:13:59 +0800

    mongodb ckafka and gaap compatible with terraform v2

--- tencentcloud/data_source_tc_mongodb_instances.go
+++ tencentcloud/data_source_tc_mongodb_instances.go
@@ -286,7 +286,7 @@ instancesLoop:
 			"charge_type":    MONGODB_CHARGE_TYPE[*mongo.PayMode],
 		}
 		if MONGODB_CHARGE_TYPE[*mongo.PayMode] == MONGODB_CHARGE_TYPE_PREPAID {
-			_ = d.Set("auto_renew_flag", *mongo.AutoRenewFlag)
+			instance["auto_renew_flag"] = *mongo.AutoRenewFlag
 		}
 		instanceList = append(instanceList, instance)
 		ids = append(ids, *mongo.InstanceId)
