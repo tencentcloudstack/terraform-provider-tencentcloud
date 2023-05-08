735b4fbda36ca31a5f32cdafc0be158a0f81d330
anonymous <anonymous@mail.org>
2023-05-08 15:00:54 +0800

    fix: modify tse

--- tencentcloud/service_tencentcloud_tse.go
+++ tencentcloud/service_tencentcloud_tse.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	tse "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/tse/v20201207"
 	"github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/connectivity"
 	"github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/internal/helper"
