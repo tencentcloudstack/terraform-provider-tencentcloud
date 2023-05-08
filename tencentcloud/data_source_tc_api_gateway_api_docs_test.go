33072ee67a6f8f69d5475a54d45d368676eba669
SevenEarth <391613297@qq.com>
2023-05-08 15:24:29 +0800

    fix/apigw-custom_domain-bug

--- tencentcloud/data_source_tc_api_gateway_api_docs_test.go
+++ tencentcloud/data_source_tc_api_gateway_api_docs_test.go
@@ -12,9 +12,9 @@ var testAPIGatewayAPIDocsResourceName = "data.tencentcloud_api_gateway_api_docs"
 func TestAccTencentAPIGatewayAPIDocsDataSource_basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		//CheckDestroy: testAccCheckAPIGatewayAPIDocDestroy,
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAPIGatewayAPIDocDestroy,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccTestAccTencentAPIGatewayAPIDocs,
@@ -29,9 +29,9 @@ func TestAccTencentAPIGatewayAPIDocsDataSource_basic(t *testing.T) {
 const testAccTestAccTencentAPIGatewayAPIDocs = `
 resource "tencentcloud_api_gateway_api_doc" "test" {
   api_doc_name = "doc_test1"
-  service_id   = "service-hwd9bmdk"
+  service_id   = "service-7lybgojo"
   environment  = "release"
-  api_ids      = ["api-p019gpxq"]
+  api_ids      = ["api-2bntitvw"]
 }
 
 data "tencentcloud_api_gateway_api_docs" "test" {
