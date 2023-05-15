28bb020e2017b6548d872f8a7f6175e88bf7492b
nickyinluo <nickyinluo@tencent.com>
2023-05-15 09:38:42 +0800

    feat:update tke addon config for tcr

--- tencentcloud/resource_tc_kubernetes_addon_attachment.go
+++ tencentcloud/resource_tc_kubernetes_addon_attachment.go
@@ -19,27 +19,30 @@ resource "tencentcloud_kubernetes_addon_attachment" "addon_cbs" {
 resource "tencentcloud_kubernetes_addon_attachment" "addon_tcr" {
   cluster_id = "cls-xxxxxxxx"
   name = "tcr"
-  # version = "1.0.0"
+  version = "1.0.0" #specify the version you want to add
   values = [
     # imagePullSecretsCrs is an array which can configure image pull
-    "global.imagePullSecretsCrs[0].name=unique-sample-vpc",
-    "global.imagePullSecretsCrs[0].namespaces=tcr-assistant-system",
-    "global.imagePullSecretsCrs[0].serviceAccounts=*",
-    "global.imagePullSecretsCrs[0].type=docker",
-    "global.imagePullSecretsCrs[0].dockerUsername=100012345678",
-    "global.imagePullSecretsCrs[0].dockerPassword=a.b.tcr-token",
-    "global.imagePullSecretsCrs[0].dockerServer=xxxx.tencentcloudcr.com",
-    "global.imagePullSecretsCrs[1].name=sample-public",
-    "global.imagePullSecretsCrs[1].namespaces=*",
+    "global.imagePullSecretsCrs[0].name=${local.tcr_id}-vpc",                              #specify a unique name, invalid format as: `${tcrId}-vpc`
+    "global.imagePullSecretsCrs[0].namespaces=${local.ns_name}",                           #input the specified namespaces of the cluster, or input `*` for all.
+    "global.imagePullSecretsCrs[0].serviceAccounts=*",                                     #input the specified service account of the cluster, or input `*` for all.
+    "global.imagePullSecretsCrs[0].type=docker",                                           #only support docker now
+    "global.imagePullSecretsCrs[0].dockerUsername=${local.user_name}",                     #input the access username, or you can create it from data source `tencentcloud_tcr_tokens`
+    "global.imagePullSecretsCrs[0].dockerPassword=${local.token}",                         #input the access token, or you can create it from data source `tencentcloud_tcr_tokens`
+    "global.imagePullSecretsCrs[0].dockerServer=${local.tcr_name}-vpc.tencentcloudcr.com", #invalid format as: `${tcr_name}-vpc.tencentcloudcr.com`
+    "global.imagePullSecretsCrs[1].name=${local.tcr_id}-public",                           #specify a unique name, invalid format as: `${tcr_id}-public`
+    "global.imagePullSecretsCrs[1].namespaces=${local.ns_name}",
     "global.imagePullSecretsCrs[1].serviceAccounts=*",
     "global.imagePullSecretsCrs[1].type=docker",
-    "global.imagePullSecretsCrs[1].dockerUsername=100012345678",
-    "global.imagePullSecretsCrs[1].dockerPassword=a.b.tcr-token",
-    "global.imagePullSecretsCrs[1].dockerServer=sample",
-    # Specify global hosts
-	"global.hosts[0].domain=sample-vpc.tencentcloudcr.com",
-	"global.hosts[0].ip=10.16.0.49",
-	"global.hosts[0].disabled=false",
+    "global.imagePullSecretsCrs[1].dockerUsername=${local.user_name}",                 #refer to previous description
+    "global.imagePullSecretsCrs[1].dockerPassword=${local.token}",                     #refer to previous description
+    "global.imagePullSecretsCrs[1].dockerServer=${local.tcr_name}-tencentcloudcr.com", #invalid format as: `${tcr_name}.tencentcloudcr.com`
+    # Specify global hosts(optional), the numbers of hosts must be matched with the numbers of imagePullSecretsCrs
+    # "global.hosts[0].domain=${local.tcr_name}-vpc.tencentcloudcr.com", #Corresponds to the dockerServer in the imagePullSecretsCrs above
+    # "global.hosts[0].ip=${tencentcloud_tcr_instance.mytcr.internal_end_point}",              #input InternalEndpoint of tcr instance, you can get it from data source `tencentcloud_tcr_instances`
+    # "global.hosts[0].disabled=false",                                                        #disabled this host config or not
+    # "global.hosts[1].domain=${local.tcr_name}-tencentcloudcr.com",
+    # "global.hosts[1].ip=${tencentcloud_tcr_instance.mytcr.internal_end_point}",
+    # "global.hosts[1].disabled=false",
   ]
 }
 ```
@@ -235,7 +238,6 @@ func resourceTencentCloudTkeAddonAttachmentRead(d *schema.ResourceData, meta int
 		err               error
 		response          string
 		addonResponseData = &AddonResponseData{}
-		status            = make(map[string]*string)
 	)
 
 	_, has, err = service.PollingAddonsPhase(ctx, clusterId, addonName, addonResponseData)
@@ -255,6 +257,7 @@ func resourceTencentCloudTkeAddonAttachmentRead(d *schema.ResourceData, meta int
 	_ = d.Set("response_body", response)
 
 	spec := addonResponseData.Spec
+	status := addonResponseData.Status
 
 	if spec != nil {
 		_ = d.Set("cluster_id", clusterId)
