b0010db81ac20d6a2686d4e79c80dc20fe9d204a
xawei <csxawei@foxmail.com>
2023-05-15 10:39:55 +0800

    misc: update test func name

--- tencentcloud/resource_tc_kubernetes_cluster_endpoint_test.go
+++ tencentcloud/resource_tc_kubernetes_cluster_endpoint_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccTencentCloudKubernetesClusterEndpoint(t *testing.T) {
+func TestAccTencentCloudKubernetesClusterEndpointResource(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
