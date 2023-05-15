4519bca69ac1e5908f7ff43e010ef6155853b821
nickyinluo <nickyinluo@tencent.com>
2023-05-15 12:59:27 +0800

    add changelog and update doc

--- tencentcloud/resource_tc_kubernetes_addon_attachment.go
+++ tencentcloud/resource_tc_kubernetes_addon_attachment.go
@@ -5,6 +5,8 @@ Provide a resource to configure kubernetes cluster app addons.
 
 Example Usage
 
+Install cbs addon by passing values
+
 ```hcl
 
 resource "tencentcloud_kubernetes_addon_attachment" "addon_cbs" {
@@ -15,11 +17,15 @@ resource "tencentcloud_kubernetes_addon_attachment" "addon_cbs" {
     "rootdir=/var/lib/kubelet"
   ]
 }
+```
 
+Install tcr addon by passing values
+
+```hcl
 resource "tencentcloud_kubernetes_addon_attachment" "addon_tcr" {
-  cluster_id = "cls-xxxxxxxx"
-  name = "tcr"
-  version = "1.0.0" #specify the version you want to add
+  cluster_id = "cls-gomql7rs"
+  name       = "tcr"
+  version    = "1.0.0"
   values = [
     # imagePullSecretsCrs is an array which can configure image pull
     "global.imagePullSecretsCrs[0].name=${local.tcr_id}-vpc",                              #specify a unique name, invalid format as: `${tcrId}-vpc`
@@ -36,14 +42,56 @@ resource "tencentcloud_kubernetes_addon_attachment" "addon_tcr" {
     "global.imagePullSecretsCrs[1].dockerUsername=${local.user_name}",                 #refer to previous description
     "global.imagePullSecretsCrs[1].dockerPassword=${local.token}",                     #refer to previous description
     "global.imagePullSecretsCrs[1].dockerServer=${local.tcr_name}-tencentcloudcr.com", #invalid format as: `${tcr_name}.tencentcloudcr.com`
+    "global.cluster.region=gz",
+    "global.cluster.longregion=ap-guangzhou",
     # Specify global hosts(optional), the numbers of hosts must be matched with the numbers of imagePullSecretsCrs
-    # "global.hosts[0].domain=${local.tcr_name}-vpc.tencentcloudcr.com", #Corresponds to the dockerServer in the imagePullSecretsCrs above
-    # "global.hosts[0].ip=${tencentcloud_tcr_instance.mytcr.internal_end_point}",              #input InternalEndpoint of tcr instance, you can get it from data source `tencentcloud_tcr_instances`
-    # "global.hosts[0].disabled=false",                                                        #disabled this host config or not
-    # "global.hosts[1].domain=${local.tcr_name}-tencentcloudcr.com",
-    # "global.hosts[1].ip=${tencentcloud_tcr_instance.mytcr.internal_end_point}",
-    # "global.hosts[1].disabled=false",
+    "global.hosts[0].domain=${local.tcr_name}-vpc.tencentcloudcr.com",                 #Corresponds to the dockerServer in the imagePullSecretsCrs above
+    "global.hosts[0].ip=${local.end_point}",                                           #input InternalEndpoint of tcr instance, you can get it from data source `tencentcloud_tcr_instances`
+    "global.hosts[0].disabled=false",                                                  #disabled this host config or not
+    "global.hosts[1].domain=${local.tcr_name}-tencentcloudcr.com",
+    "global.hosts[1].ip=${local.end_point}",
+    "global.hosts[1].disabled=false",
   ]
+}
+
+locals {
+  tcr_id   = tencentcloud_tcr_instance.mytcr.id
+  tcr_name = tencentcloud_tcr_instance.mytcr.name
+  ns_name   = tencentcloud_tcr_namespace.my_ns.name
+  user_name = tencentcloud_tcr_token.my_token.user_name
+  token     = tencentcloud_tcr_token.my_token.token
+  end_point = data.tencentcloud_tcr_instances.my_ins.instance_list.0.internal_end_point
+}
+
+resource "tencentcloud_tcr_token" "my_token" {
+  instance_id = local.tcr_id
+  description = "tcr token"
+}
+
+data "tencentcloud_tcr_instances" "my_ins" {
+  instance_id = local.tcr_id
+}
+
+resource "tencentcloud_tcr_instance" "mytcr" {
+  name          = "tf-test-tcr-addon"
+  instance_type = "basic"
+  delete_bucket = true
+
+  tags = {
+    test = "test"
+  }
+}
+
+resource "tencentcloud_tcr_namespace" "my_ns" {
+  instance_id    = local.tcr_id
+  name           = "tf_test_tcr_ns_addon"
+  is_public      = true
+  is_auto_scan   = true
+  is_prevent_vul = true
+  severity       = "medium"
+  cve_whitelist_items {
+    cve_id = "cve-xxxxx"
+  }
 }
 ```
 
@@ -257,7 +305,7 @@ func resourceTencentCloudTkeAddonAttachmentRead(d *schema.ResourceData, meta int
 	_ = d.Set("response_body", response)
 
 	spec := addonResponseData.Spec
-	status := addonResponseData.Status
+	statuses := addonResponseData.Status
 
 	if spec != nil {
 		_ = d.Set("cluster_id", clusterId)
@@ -271,8 +319,9 @@ func resourceTencentCloudTkeAddonAttachmentRead(d *schema.ResourceData, meta int
 		}
 	}
 
-	if status != nil {
-		err := d.Set("status", status)
+	if statuses != nil || len(statuses) == 0 {
+		strMap := helper.CovertInterfaceMapToStrPtr(statuses)
+		err := d.Set("status", strMap)
 		if err != nil {
 			return err
 		}
