e3ac9c03d118d78435dcf8686c1205770e6969a3
nickyinluo <nickyinluo@tencent.com>
2023-05-23 16:27:18 +0800

    adjust example

--- tencentcloud/resource_tc_postgresql_backup_download_restriction_config.go
+++ tencentcloud/resource_tc_postgresql_backup_download_restriction_config.go
@@ -3,6 +3,21 @@ Provides a resource to create a postgresql backup_download_restriction_config
 
 Example Usage
 
+Unlimit the restriction of the backup file download.
+```hcl
+resource "tencentcloud_postgresql_backup_download_restriction_config" "backup_download_restriction_config" {
+  restriction_type = "NONE"
+}
+```
+
+Set the download only to allow the intranet downloads.
+```hcl
+resource "tencentcloud_postgresql_backup_download_restriction_config" "backup_download_restriction_config" {
+  restriction_type = "INTRANET"
+}
+```
+
+Restrict the backup file download by customizing.
 ```hcl
 resource "tencentcloud_vpc" "pg_vpc" {
 	name       = var.instance_name
@@ -10,11 +25,11 @@ resource "tencentcloud_vpc" "pg_vpc" {
 }
 
 resource "tencentcloud_postgresql_backup_download_restriction_config" "backup_download_restriction_config" {
-  restriction_type = "INTRANET"
+  restriction_type = "CUSTOMIZE"
   vpc_restriction_effect = "DENY"
-  vpc_id_set = [tencentcloud_vpc.pg_vpc.id]
+  vpc_id_set = [tencentcloud_vpc.pg_vpc2.id]
   ip_restriction_effect = "DENY"
-  ip_set = ["0.0.0.0"]
+  ip_set = ["192.168.0.0"]
 }
 ```
 
@@ -156,7 +171,13 @@ func resourceTencentCloudPostgresqlBackupDownloadRestrictionConfigUpdate(d *sche
 	request := postgresql.NewModifyBackupDownloadRestrictionRequest()
 
 	resType := d.Id()
-	request.RestrictionType = helper.String(resType)
+
+	if d.HasChange("restriction_type") {
+		if v, ok := d.GetOk("restriction_type"); ok {
+			resType = v.(string)
+		}
+	}
+	request.RestrictionType = &resType
 
 	if d.HasChange("vpc_restriction_effect") {
 		if v, ok := d.GetOk("vpc_restriction_effect"); ok {
--- tencentcloud/resource_tc_postgresql_backup_download_restriction_config_test.go
+++ tencentcloud/resource_tc_postgresql_backup_download_restriction_config_test.go
@@ -9,7 +9,6 @@ import (
 const TestAccPostgresqlBackupDownloadRestrictionObject = "tencentcloud_postgresql_backup_download_restriction_config.backup_download_restriction_config"
 
 func TestAccTencentCloudPostgresqlBackupDownloadRestrictionConfigResource_basic(t *testing.T) {
-	t.Parallel()
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
@@ -17,25 +16,10 @@ func TestAccTencentCloudPostgresqlBackupDownloadRestrictionConfigResource_basic(
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccPostgresqlBackupDownloadRestrictionConfig_none,
+				Config: testAccPostgresqlBackupDownloadRestrictionConfig_NONE,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet(TestAccPostgresqlBackupDownloadRestrictionObject, "id"),
 					resource.TestCheckResourceAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "restriction_type", "NONE"),
-					resource.TestCheckResourceAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "vpc_restriction_effect", "ALLOW"),
-					resource.TestCheckResourceAttrSet(TestAccPostgresqlBackupDownloadRestrictionObject, "vpc_id_set.#"),
-					resource.TestCheckResourceAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "ip_restriction_effect", "ALLOW"),
-					resource.TestCheckTypeSetElemAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "ip_set.*", "127.0.0.1"),
-				),
-			},
-			{
-				Config: testAccPostgresqlBackupDownloadRestrictionConfig_INTRANET,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(TestAccPostgresqlBackupDownloadRestrictionObject, "id"),
-					resource.TestCheckResourceAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "restriction_type", "INTRANET"),
-					resource.TestCheckResourceAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "vpc_restriction_effect", "DENY"),
-					resource.TestCheckResourceAttrSet(TestAccPostgresqlBackupDownloadRestrictionObject, "vpc_id_set.#"),
-					resource.TestCheckResourceAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "ip_restriction_effect", "DENY"),
-					resource.TestCheckTypeSetElemAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "ip_set.*", "0.0.0.0"),
 				),
 			},
 			{
@@ -47,34 +31,121 @@ func TestAccTencentCloudPostgresqlBackupDownloadRestrictionConfigResource_basic(
 	})
 }
 
-const testAccPostgresqlBackupDownloadRestrictionConfig_none = defaultVpcVariable + `
-resource "tencentcloud_vpc" "pg_vpc" {
+func TestAccTencentCloudPostgresqlBackupDownloadRestrictionConfigResource_CUSTOMIZE(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPostgresqlBackupDownloadRestrictionConfig_CUSTOMIZE_ALLOW,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(TestAccPostgresqlBackupDownloadRestrictionObject, "id"),
+					resource.TestCheckResourceAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "restriction_type", "CUSTOMIZE"),
+					resource.TestCheckResourceAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "vpc_restriction_effect", "ALLOW"),
+					resource.TestCheckResourceAttrSet(TestAccPostgresqlBackupDownloadRestrictionObject, "vpc_id_set.#"),
+					resource.TestCheckResourceAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "ip_restriction_effect", "ALLOW"),
+					resource.TestCheckTypeSetElemAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "ip_set.*", "192.168.1.1"),
+				),
+			},
+			{
+				Config: testAccPostgresqlBackupDownloadRestrictionConfig_CUSTOMIZE_DENY,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(TestAccPostgresqlBackupDownloadRestrictionObject, "id"),
+					resource.TestCheckResourceAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "restriction_type", "CUSTOMIZE"),
+					resource.TestCheckResourceAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "vpc_restriction_effect", "DENY"),
+					resource.TestCheckResourceAttrSet(TestAccPostgresqlBackupDownloadRestrictionObject, "vpc_id_set.#"),
+					resource.TestCheckResourceAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "ip_restriction_effect", "DENY"),
+					resource.TestCheckTypeSetElemAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "ip_set.*", "192.168.0.0"),
+				),
+			},
+			{
+				ResourceName:      TestAccPostgresqlBackupDownloadRestrictionObject,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccTencentCloudPostgresqlBackupDownloadRestrictionConfigResource_INTRANET(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPostgresqlBackupDownloadRestrictionConfig_INTRANET,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(TestAccPostgresqlBackupDownloadRestrictionObject, "id"),
+					resource.TestCheckResourceAttr(TestAccPostgresqlBackupDownloadRestrictionObject, "restriction_type", "INTRANET"),
+				),
+			},
+			{
+				ResourceName:      TestAccPostgresqlBackupDownloadRestrictionObject,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+const testAccPostgresqlBackupDownloadRestrictionConfig_NONE = `
+
+resource "tencentcloud_postgresql_backup_download_restriction_config" "backup_download_restriction_config" {
+  restriction_type = "NONE"
+}
+
+`
+
+const testAccPostgresqlBackupDownloadRestrictionConfig_CUSTOMIZE_ALLOW = defaultVpcVariable + `
+resource "tencentcloud_vpc" "pg_vpc1" {
+	name       = var.instance_name
+	cidr_block = var.vpc_cidr
+}
+
+resource "tencentcloud_vpc" "pg_vpc2" {
 	name       = var.instance_name
 	cidr_block = var.vpc_cidr
 }
 
 resource "tencentcloud_postgresql_backup_download_restriction_config" "backup_download_restriction_config" {
-  restriction_type = "NONE"
+  restriction_type = "CUSTOMIZE"
   vpc_restriction_effect = "ALLOW"
-  vpc_id_set = [tencentcloud_vpc.pg_vpc.id]
+  vpc_id_set = [tencentcloud_vpc.pg_vpc1.id]
   ip_restriction_effect = "ALLOW"
-  ip_set = [127.0.0.1]
+  ip_set = ["192.168.1.1"]
+}
+
+`
+
+const testAccPostgresqlBackupDownloadRestrictionConfig_CUSTOMIZE_DENY = defaultVpcVariable + `
+resource "tencentcloud_vpc" "pg_vpc1" {
+	name       = var.instance_name
+	cidr_block = var.vpc_cidr
+}
+
+resource "tencentcloud_vpc" "pg_vpc2" {
+	name       = var.instance_name
+	cidr_block = var.vpc_cidr
+}
+
+resource "tencentcloud_postgresql_backup_download_restriction_config" "backup_download_restriction_config" {
+  restriction_type = "CUSTOMIZE"
+  vpc_restriction_effect = "DENY"
+  vpc_id_set = [tencentcloud_vpc.pg_vpc2.id]
+  ip_restriction_effect = "DENY"
+  ip_set = ["192.168.0.0"]
 }
 
 `
 
 const testAccPostgresqlBackupDownloadRestrictionConfig_INTRANET = `
-resource "tencentcloud_vpc" "pg_vpc" {
-	name       = var.instance_name
-	cidr_block = var.vpc_cidr
-}
 
 resource "tencentcloud_postgresql_backup_download_restriction_config" "backup_download_restriction_config" {
   restriction_type = "INTRANET"
-  vpc_restriction_effect = "DENY"
-  vpc_id_set = [tencentcloud_vpc.pg_vpc.id]
-  ip_restriction_effect = "DENY"
-  ip_set = ["0.0.0.0"]
 }
 
 `
