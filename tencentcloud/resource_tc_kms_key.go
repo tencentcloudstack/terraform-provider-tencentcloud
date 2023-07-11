d34568ce8c1ad5976ddb6f051b345c2a32565f7c
nickyinluo <nickyinluo@tencent.com>
2023-07-11 20:36:32 +0800

    adjust example

--- tencentcloud/resource_tc_kms_key.go
+++ tencentcloud/resource_tc_kms_key.go
@@ -3,16 +3,44 @@ Provide a resource to create a KMS key.
 
 Example Usage
 
-```hcl
-resource "tencentcloud_kms_key" "foo" {
-	alias = "test"
-	description = "describe key test message."
-	key_rotation_enabled = true
-	is_enabled = true
+Create and enable a instance.
 
-	tags = {
-		"test-tag":"key-test"
-	}
+```hcl
+resource "tencentcloud_kms_key" "example" {
+  alias                = "tf-example-kms-key"
+  description          = "example of kms key"
+  key_rotation_enabled = false
+  is_enabled           = true
+
+  tags = {
+    "createdBy" = "terraform"
+  }
+}
+```
+
+Specify the Key Usage as an asymmetry method.
+
+```hcl
+resource "tencentcloud_kms_key" "example2" {
+  alias       = "tf-example-kms-key"
+  description = "example of kms key"
+  key_usage   = "ASYMMETRIC_DECRYPT_RSA_2048"
+  is_enabled  = false
+}
+```
+
+Disable the kms key instance.
+
+```hcl
+resource "tencentcloud_kms_key" "example3" {
+  alias                = "tf-example-kms-key"
+  description          = "example of kms key"
+  key_rotation_enabled = false
+  is_enabled           = false
+
+  tags = {
+    "test-tag" = "unit-test"
+  }
 }
 ```
 
