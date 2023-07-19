8e506d12a00a76321a5ce4b94605e6bb152b537e
SevenEarth <45937856+SevenEarth@users.noreply.github.com>
2023-07-19 17:09:25 +0800

    fix/sqlserver (#1960)
    * fix/sqlserver
    
    * fix/sqlserver

--- tencentcloud/resource_tc_sqlserver_complete_expansion_test.go
+++ tencentcloud/resource_tc_sqlserver_complete_expansion_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 // go test -i; go test -test.run TestAccTencentCloudSqlserverCompleteExpansionResource_basic -v
+// go test -v -run TestAccTencentCloudSqlserverCompleteExpansionResource_basic -timeout=0 ./tencentcloud/
 func TestAccTencentCloudSqlserverCompleteExpansionResource_basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
@@ -36,41 +37,41 @@ func TestAccTencentCloudSqlserverCompleteExpansionResource_basic(t *testing.T) {
 
 const testAccNewSqlserverInstance string = testAccSqlserverBasicInstanceNetwork + `
 resource "tencentcloud_sqlserver_instance" "test" {
-  name                          = "tf_sqlserver_instance"
-  availability_zone             = "ap-guangzhou-7"
-  charge_type                   = "POSTPAID_BY_HOUR"
-  vpc_id                        = "vpc-1yg5ua6l"
-  subnet_id                     = "subnet-h7av55g8"
-  security_groups               = ["sg-mayqdlt1"]
-  project_id                    = 0
-  memory                        = 2
-  storage                       = 10
-  maintenance_week_set          = [1,2,3]
-  maintenance_start_time        = "09:00"
-  maintenance_time_span         = 3
-  tags = {
-    "test"                      = "test"
+  name                   = "tf_sqlserver_instance"
+  availability_zone      = "ap-guangzhou-7"
+  charge_type            = "POSTPAID_BY_HOUR"
+  vpc_id                 = "vpc-1yg5ua6l"
+  subnet_id              = "subnet-h7av55g8"
+  security_groups        = ["sg-mayqdlt1"]
+  project_id             = 0
+  memory                 = 2
+  storage                = 20
+  maintenance_week_set   = [1, 2, 3]
+  maintenance_start_time = "09:00"
+  maintenance_time_span  = 3
+  tags                   = {
+    "test" = "test"
   }
 }
 `
 
 const testAccUpdateNewSqlserverInstance string = testAccSqlserverBasicInstanceNetwork + `
 resource "tencentcloud_sqlserver_instance" "test" {
-  name                          = "tf_sqlserver_instance"
-  availability_zone             = "ap-guangzhou-7"
-  charge_type                   = "POSTPAID_BY_HOUR"
-  vpc_id                        = "vpc-1yg5ua6l"
-  subnet_id                     = "subnet-h7av55g8"
-  security_groups               = ["sg-mayqdlt1"]
-  project_id                    = 0
-  memory                        = 2
-  storage                       = 20
-  maintenance_week_set          = [1,2,3]
-  maintenance_start_time        = "09:00"
-  maintenance_time_span         = 3
-  wait_switch                   = 1
-  tags = {
-    "test"                      = "test"
+  name                   = "tf_sqlserver_instance"
+  availability_zone      = "ap-guangzhou-7"
+  charge_type            = "POSTPAID_BY_HOUR"
+  vpc_id                 = "vpc-1yg5ua6l"
+  subnet_id              = "subnet-h7av55g8"
+  security_groups        = ["sg-mayqdlt1"]
+  project_id             = 0
+  memory                 = 2
+  storage                = 40
+  maintenance_week_set   = [1, 2, 3]
+  maintenance_start_time = "09:00"
+  maintenance_time_span  = 3
+  wait_switch            = 1
+  tags                   = {
+    "test" = "test"
   }
 }
 
