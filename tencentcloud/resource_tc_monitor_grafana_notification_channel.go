6241b9375656bd0a726b4d55e09c40b341e116de
Arunma <41282634+gitmkn@users.noreply.github.com>
2023-07-19 17:10:38 +0800

    fix: modify grafana (#1971)
    * fix: modify grafana
    
    * feat: add changelog
    
    * fix: add is_distroy
    
    * fix: modify test
    
    ---------
    
    Co-authored-by: anonymous <anonymous@mail.org>

--- tencentcloud/resource_tc_monitor_grafana_notification_channel.go
+++ tencentcloud/resource_tc_monitor_grafana_notification_channel.go
@@ -4,12 +4,69 @@ Provides a resource to create a monitor grafanaNotificationChannel
 Example Usage
 
 ```hcl
+variable "availability_zone" {
+  default = "ap-guangzhou-6"
+}
+
+resource "tencentcloud_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  name       = "tf_monitor_vpc"
+}
+
+resource "tencentcloud_subnet" "subnet" {
+  vpc_id            = tencentcloud_vpc.vpc.id
+  availability_zone = var.availability_zone
+  name              = "tf_monitor_subnet"
+  cidr_block        = "10.0.1.0/24"
+}
+
+resource "tencentcloud_monitor_grafana_instance" "foo" {
+  instance_name         = "test-grafana"
+  vpc_id                = tencentcloud_vpc.vpc.id
+  subnet_ids            = [tencentcloud_subnet.subnet.id]
+  grafana_init_password = "1234567890"
+  enable_internet = false
+
+  tags = {
+    "createdBy" = "test"
+  }
+}
+
+resource "tencentcloud_monitor_alarm_notice" "foo" {
+  name                  = "tf_alarm_notice"
+  notice_type           = "ALL"
+  notice_language       = "zh-CN"
+
+  user_notices    {
+      receiver_type              = "USER"
+      start_time                 = 0
+      end_time                   = 1
+      notice_way                 = ["SMS","EMAIL"]
+      user_ids                   = [10001]
+      group_ids                  = []
+      phone_order                = [10001]
+      phone_circle_times         = 2
+      phone_circle_interval      = 50
+      phone_inner_interval       = 60
+      need_phone_arrive_notice   = 1
+      phone_call_type            = "CIRCLE"
+      weekday                    =[1,2,3,4,5,6,7]
+  }
+
+  url_notices {
+      url    = "https://www.mytest.com/validate"
+      end_time =  0
+      start_time = 1
+      weekday = [1,2,3,4,5,6,7]
+  }
+}
+
 resource "tencentcloud_monitor_grafana_notification_channel" "grafanaNotificationChannel" {
-  instance_id   = "grafana-50nj6v00"
-  channel_name  = "create-channel"
+  instance_id   = tencentcloud_monitor_grafana_instance.foo.id
+  channel_name  = "tf-channel"
   org_id        = 1
-  receivers     = ["Consumer-6vkna7pevq"]
-  extra_org_ids = []
+  receivers     = [tencentcloud_monitor_alarm_notice.foo.amp_consumer_id]
+  extra_org_ids = ["1"]
 }
 
 ```
