fdbe4e0afd63da24b6aa1ccec6565fb10ca76817
nickyinluo <nickyinluo@tencent.com>
2023-07-31 20:41:57 +0800

    1.add env_az 2.add env_region

--- tencentcloud/resource_tc_kubernetes_node_pool_test.go
+++ tencentcloud/resource_tc_kubernetes_node_pool_test.go
@@ -264,9 +264,13 @@ variable "availability_zone" {
   default = "ap-guangzhou-3"
 }
 
+variable "env_az" {
+  type = string
+}
+
 data "tencentcloud_vpc_subnets" "vpc" {
     is_default        = true
-    availability_zone = var.availability_zone
+    availability_zone = var.env_az != "" ? var.env_az : var.availability_zone
 }
 
 data "tencentcloud_security_groups" "sg" {
