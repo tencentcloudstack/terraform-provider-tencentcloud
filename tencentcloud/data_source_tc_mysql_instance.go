c6edd6193fddb9f8e2e055809bef1be0a1d8ee48
Arunma <41282634+gitmkn@users.noreply.github.com>
2023-08-02 20:31:50 +0800

    Fix/mysql test (#2002)
    * fix: update mysql test
    
    * fix: update redis_instance_node
    
    * fix: update mysq readonly
    
    * fix: modify test
    
    * fix: support group_id'
    
    ---------
    
    Co-authored-by: anonymous <anonymous@mail.org>

--- tencentcloud/data_source_tc_mysql_instance.go
+++ tencentcloud/data_source_tc_mysql_instance.go
@@ -259,6 +259,28 @@ func dataSourceTencentCloudMysqlInstance() *schema.Resource {
 								Type: schema.TypeString,
 							},
 						},
+						"ro_groups": {
+							Type:        schema.TypeList,
+							Computed:    true,
+							Description: "read-only instance group.",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"group_id": {
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: "Group ID, such as `cdbrg-pz7vg37p`.",
+									},
+									"instance_ids": {
+										Type:        schema.TypeList,
+										Computed:    true,
+										Description: "ID list of read-only type associated with the current instance.",
+										Elem: &schema.Schema{
+											Type: schema.TypeString,
+										},
+									},
+								},
+							},
+						},
 						"dr_instance_ids": {
 							Type:        schema.TypeList,
 							Computed:    true,
@@ -398,12 +420,22 @@ func dataSourceTencentCloudMysqlInstanceRead(d *schema.ResourceData, meta interf
 		}
 		if len(item.RoGroups) > 0 {
 			roInstanceIds := make([]string, 0)
+			roGroupList := make([]map[string]interface{}, 0, len(item.RoGroups))
 			for _, roGroupInfo := range item.RoGroups {
+				roInstanceId := make([]string, 0)
 				for _, roInfo := range roGroupInfo.RoInstances {
-					roInstanceIds = append(roInstanceIds, *roInfo.InstanceId)
+					roInstanceId = append(roInstanceId, *roInfo.InstanceId)
 				}
+				roGroup := map[string]interface{}{
+					"group_id":     *roGroupInfo.RoGroupId,
+					"instance_ids": roInstanceId,
+				}
+
+				roInstanceIds = append(roInstanceIds, roInstanceId...)
+				roGroupList = append(roGroupList, roGroup)
 			}
 			mapping["ro_instance_ids"] = roInstanceIds
+			mapping["ro_groups"] = roGroupList
 		}
 		if len(item.DrInfo) > 0 {
 			drInstanceIds := make([]string, 0)
--- tencentcloud/resource_tc_mysql_readonly_instance_test.go
+++ tencentcloud/resource_tc_mysql_readonly_instance_test.go
@@ -12,7 +12,7 @@ import (
 
 // go test -i; go test -test.run TestAccTencentCloudMysqlReadonlyInstanceResource_basic -v
 func TestAccTencentCloudMysqlReadonlyInstanceResource_basic(t *testing.T) {
-	t.Parallel()
+	// t.Parallel()
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
