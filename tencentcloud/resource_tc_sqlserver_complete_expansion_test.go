58cbda05912d792749cfcb27e4025452d77321d5
SevenEarth <391613297@qq.com>
2023-08-04 17:11:18 +0800

    feat/sql

--- tencentcloud/resource_tc_sqlserver_complete_expansion_test.go
+++ tencentcloud/resource_tc_sqlserver_complete_expansion_test.go
@@ -35,36 +35,18 @@ func TestAccTencentCloudSqlserverCompleteExpansionResource_basic(t *testing.T) {
 	})
 }
 
-const testAccNewSqlserverInstance string = `
+const testAccNewSqlserverInstance string = defaultVpcVariable + defaultSecurityGroupData + `
 data "tencentcloud_availability_zones_by_product" "zones" {
   product = "sqlserver"
-}
-
-resource "tencentcloud_vpc" "vpc" {
-  name       = "example-vpc"
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "tencentcloud_subnet" "subnet" {
-  availability_zone = data.tencentcloud_availability_zones_by_product.zones.zones.4.name
-  name              = "example-vpc"
-  vpc_id            = tencentcloud_vpc.vpc.id
-  cidr_block        = "10.0.0.0/16"
-  is_multicast      = false
-}
-
-resource "tencentcloud_security_group" "security_group" {
-  name        = "example-sg"
-  description = "desc."
 }
 
 resource "tencentcloud_sqlserver_instance" "example" {
   name                   = "tf_example_sql"
   availability_zone      = data.tencentcloud_availability_zones_by_product.zones.zones.4.name
   charge_type            = "POSTPAID_BY_HOUR"
-  vpc_id                 = tencentcloud_vpc.vpc.id
-  subnet_id              = tencentcloud_subnet.subnet.id
-  security_groups        = [tencentcloud_security_group.security_group.id]
+  vpc_id                 = var.vpc_id
+  subnet_id              = var.subnet_id
+  security_groups        = [local.sg_id]
   project_id             = 0
   memory                 = 2
   storage                = 20
@@ -75,24 +57,6 @@ resource "tencentcloud_sqlserver_instance" "example" {
     "createBy" = "tfExample"
   }
 }
-
-resource "tencentcloud_sqlserver_instance" "test" {
-  name                   = "tf_sqlserver_instance"
-  availability_zone      = "ap-guangzhou-7"
-  charge_type            = "POSTPAID_BY_HOUR"
-  vpc_id                 = "vpc-1yg5ua6l"
-  subnet_id              = "subnet-h7av55g8"
-  security_groups        = ["sg-mayqdlt1"]
-  project_id             = 0
-  memory                 = 2
-  storage                = 20
-  maintenance_week_set   = [1, 2, 3]
-  maintenance_start_time = "09:00"
-  maintenance_time_span  = 3
-  tags                   = {
-    "test" = "test"
-  }
-}
 `
 
 const testAccUpdateNewSqlserverInstance string = `
@@ -100,31 +64,13 @@ data "tencentcloud_availability_zones_by_product" "zones" {
   product = "sqlserver"
 }
 
-resource "tencentcloud_vpc" "vpc" {
-  name       = "example-vpc"
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "tencentcloud_subnet" "subnet" {
-  availability_zone = data.tencentcloud_availability_zones_by_product.zones.zones.4.name
-  name              = "example-vpc"
-  vpc_id            = tencentcloud_vpc.vpc.id
-  cidr_block        = "10.0.0.0/16"
-  is_multicast      = false
-}
-
-resource "tencentcloud_security_group" "security_group" {
-  name        = "example-sg"
-  description = "desc."
-}
-
 resource "tencentcloud_sqlserver_instance" "example" {
   name                   = "tf_example_sql"
   availability_zone      = data.tencentcloud_availability_zones_by_product.zones.zones.4.name
   charge_type            = "POSTPAID_BY_HOUR"
-  vpc_id                 = tencentcloud_vpc.vpc.id
-  subnet_id              = tencentcloud_subnet.subnet.id
-  security_groups        = [tencentcloud_security_group.security_group.id]
+  vpc_id                 = var.vpc_id
+  subnet_id              = var.subnet_id
+  security_groups        = [local.sg_id]
   project_id             = 0
   memory                 = 4
   storage                = 40
