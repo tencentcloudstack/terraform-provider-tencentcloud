2cde7f34a6d5c2d6612e91157788b4cd3b8a86e1
SevenEarth <391613297@qq.com>
2023-08-04 14:30:33 +0800

    feat/sql

--- tencentcloud/resource_tc_sqlserver_migration.go
+++ tencentcloud/resource_tc_sqlserver_migration.go
@@ -21,65 +21,90 @@ resource "tencentcloud_subnet" "subnet" {
   is_multicast      = false
 }
 
-resource "tencentcloud_sqlserver_instance" "src_example" {
-  name              = "tf-example-src"
-  availability_zone = data.tencentcloud_availability_zones_by_product.zones.zones.4.name
-  charge_type       = "POSTPAID_BY_HOUR"
-  vpc_id            = tencentcloud_vpc.vpc.id
-  subnet_id         = tencentcloud_subnet.subnet.id
-  project_id        = 0
-  memory            = 16
-  storage           = 40
+resource "tencentcloud_security_group" "security_group" {
+  name        = "sg-example"
+  description = "desc."
 }
 
-resource "tencentcloud_sqlserver_instance" "dst_example" {
-  name              = "tf-example-dst"
-  availability_zone = data.tencentcloud_availability_zones_by_product.zones.zones.4.name
-  charge_type       = "POSTPAID_BY_HOUR"
-  vpc_id            = tencentcloud_vpc.vpc.id
-  subnet_id         = tencentcloud_subnet.subnet.id
-  project_id        = 0
-  memory            = 16
-  storage           = 40
+resource "tencentcloud_sqlserver_basic_instance" "src_example" {
+  name                   = "tf-example"
+  availability_zone      = data.tencentcloud_availability_zones_by_product.zones.zones.4.name
+  charge_type            = "POSTPAID_BY_HOUR"
+  vpc_id                 = tencentcloud_vpc.vpc.id
+  subnet_id              = tencentcloud_subnet.subnet.id
+  project_id             = 0
+  memory                 = 4
+  storage                = 100
+  cpu                    = 2
+  machine_type           = "CLOUD_PREMIUM"
+  maintenance_week_set   = [1, 2, 3]
+  maintenance_start_time = "09:00"
+  maintenance_time_span  = 3
+  security_groups        = [tencentcloud_security_group.security_group.id]
+
+  tags = {
+    "test" = "test"
+  }
+}
+
+resource "tencentcloud_sqlserver_basic_instance" "dst_example" {
+  name                   = "tf-example"
+  availability_zone      = data.tencentcloud_availability_zones_by_product.zones.zones.4.name
+  charge_type            = "POSTPAID_BY_HOUR"
+  vpc_id                 = tencentcloud_vpc.vpc.id
+  subnet_id              = tencentcloud_subnet.subnet.id
+  project_id             = 0
+  memory                 = 4
+  storage                = 100
+  cpu                    = 2
+  machine_type           = "CLOUD_PREMIUM"
+  maintenance_week_set   = [1, 2, 3]
+  maintenance_start_time = "09:00"
+  maintenance_time_span  = 3
+  security_groups        = [tencentcloud_security_group.security_group.id]
+
+  tags = {
+    "test" = "test"
+  }
 }
 
 resource "tencentcloud_sqlserver_db" "src" {
-  instance_id = tencentcloud_sqlserver_instance.src_example.id
+  instance_id = tencentcloud_sqlserver_basic_instance.src_example.id
   name        = "tf_example_db_src"
   charset     = "Chinese_PRC_BIN"
   remark      = "testACC-remark"
 }
 
 resource "tencentcloud_sqlserver_db" "dst" {
-  instance_id = tencentcloud_sqlserver_instance.dst_example.id
+  instance_id = tencentcloud_sqlserver_basic_instance.dst_example.id
   name        = "tf_example_db_dst"
   charset     = "Chinese_PRC_BIN"
   remark      = "testACC-remark"
 }
 
 resource "tencentcloud_sqlserver_account" "src" {
-  instance_id = tencentcloud_sqlserver_instance.src_example.id
+  instance_id = tencentcloud_sqlserver_basic_instance.src_example.id
   name        = "tf_example_src_account"
   password    = "Qwer@234"
   is_admin    = true
 }
 
 resource "tencentcloud_sqlserver_account" "dst" {
-  instance_id = tencentcloud_sqlserver_instance.dst_example.id
+  instance_id = tencentcloud_sqlserver_basic_instance.dst_example.id
   name        = "tf_example_dst_account"
   password    = "Qwer@234"
   is_admin    = true
 }
 
 resource "tencentcloud_sqlserver_account_db_attachment" "src" {
-  instance_id  = tencentcloud_sqlserver_instance.src_example.id
+  instance_id  = tencentcloud_sqlserver_basic_instance.src_example.id
   account_name = tencentcloud_sqlserver_account.src.name
   db_name      = tencentcloud_sqlserver_db.src.name
   privilege    = "ReadWrite"
 }
 
 resource "tencentcloud_sqlserver_account_db_attachment" "dst" {
-  instance_id  = tencentcloud_sqlserver_instance.dst_example.id
+  instance_id  = tencentcloud_sqlserver_basic_instance.dst_example.id
   account_name = tencentcloud_sqlserver_account.dst.name
   db_name      = tencentcloud_sqlserver_db.dst.name
   privilege    = "ReadWrite"
@@ -90,12 +115,12 @@ resource "tencentcloud_sqlserver_migration" "migration" {
   migrate_type = 1
   source_type  = 1
   source {
-    instance_id = tencentcloud_sqlserver_instance.src_example.id
+    instance_id = tencentcloud_sqlserver_basic_instance.src_example.id
     user_name   = tencentcloud_sqlserver_account.src.name
     password    = tencentcloud_sqlserver_account.src.password
   }
   target {
-    instance_id = tencentcloud_sqlserver_instance.dst_example.id
+    instance_id = tencentcloud_sqlserver_basic_instance.dst_example.id
     user_name   = tencentcloud_sqlserver_account.dst.name
     password    = tencentcloud_sqlserver_account.dst.password
   }
--- website/docs/r/sqlserver_config_backup_strategy.html.markdown
+++ website/docs/r/sqlserver_config_backup_strategy.html.markdown
@@ -33,19 +33,34 @@ resource "tencentcloud_subnet" "subnet" {
   is_multicast      = false
 }
 
-resource "tencentcloud_sqlserver_instance" "example" {
-  name              = "tf-example"
-  availability_zone = data.tencentcloud_availability_zones_by_product.zones.zones.4.name
-  charge_type       = "POSTPAID_BY_HOUR"
-  vpc_id            = tencentcloud_vpc.vpc.id
-  subnet_id         = tencentcloud_subnet.subnet.id
-  project_id        = 0
-  memory            = 16
-  storage           = 40
+resource "tencentcloud_security_group" "security_group" {
+  name        = "sg-example"
+  description = "desc."
+}
+
+resource "tencentcloud_sqlserver_basic_instance" "example" {
+  name                   = "tf-example"
+  availability_zone      = data.tencentcloud_availability_zones_by_product.zones.zones.4.name
+  charge_type            = "POSTPAID_BY_HOUR"
+  vpc_id                 = tencentcloud_vpc.vpc.id
+  subnet_id              = tencentcloud_subnet.subnet.id
+  project_id             = 0
+  memory                 = 4
+  storage                = 100
+  cpu                    = 2
+  machine_type           = "CLOUD_PREMIUM"
+  maintenance_week_set   = [1, 2, 3]
+  maintenance_start_time = "09:00"
+  maintenance_time_span  = 3
+  security_groups        = [tencentcloud_security_group.security_group.id]
+
+  tags = {
+    "test" = "test"
+  }
 }
 
 resource "tencentcloud_sqlserver_config_backup_strategy" "example" {
-  instance_id              = tencentcloud_sqlserver_instance.example.id
+  instance_id              = tencentcloud_sqlserver_basic_instance.example.id
   backup_type              = "daily"
   backup_time              = 0
   backup_day               = 1
@@ -63,7 +78,7 @@ resource "tencentcloud_sqlserver_config_backup_strategy" "example" {
 
 ```hcl
 resource "tencentcloud_sqlserver_config_backup_strategy" "example" {
-  instance_id              = tencentcloud_sqlserver_instance.example.id
+  instance_id              = tencentcloud_sqlserver_basic_instance.example.id
   backup_type              = "weekly"
   backup_time              = 0
   backup_model             = "master_no_pkg"
@@ -80,7 +95,7 @@ resource "tencentcloud_sqlserver_config_backup_strategy" "example" {
 
 ```hcl
 resource "tencentcloud_sqlserver_config_backup_strategy" "example" {
-  instance_id               = tencentcloud_sqlserver_instance.example.id
+  instance_id               = tencentcloud_sqlserver_basic_instance.example.id
   backup_time               = 0
   backup_model              = "master_no_pkg"
   backup_cycle              = [1, 3]
