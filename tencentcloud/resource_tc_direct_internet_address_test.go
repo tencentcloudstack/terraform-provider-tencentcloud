58ac2b3ef15c20a8b065abd3cce6a998801a82f6
WeiMengXS <nickcchen@tencent.com>
2023-08-04 18:35:54 +0800

    fix: pipeline and case

--- tencentcloud/resource_tc_direct_internet_address_test.go
+++ tencentcloud/resource_tc_direct_internet_address_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccTencentCloudDirectInternetAddressResource_basic(t *testing.T) {
+func TestAccTencentCloudNeedFixDcInternetAddressResource_basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
