211be22d5510f23d5065afdd76b86cb62f8ad15b
nickyinluo <nickyinluo@tencent.com>
2023-08-08 19:15:06 +0800

    fix tke e2e gpu issue

--- tencentcloud/resource_tc_kubernetes_scale_worker_test.go
+++ tencentcloud/resource_tc_kubernetes_scale_worker_test.go
@@ -284,8 +284,8 @@ resource tencentcloud_kubernetes_scale_worker test_scale {
   gpu_args {
     mig_enable = false
     driver = {
-      name = "NVIDIA-Linux-x86_64-470.82.01.run"
-      version = "470.82.01"
+      name = "NVIDIA-Linux-x86_64-470.182.03.run"
+      version = "470.182.03"
     }
     cuda = {
       name = "cuda_11.4.3_470.82.01_linux.run"
