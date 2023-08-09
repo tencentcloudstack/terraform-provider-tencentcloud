87e62766fa340980865e5103a60e712e115cad22
WeiMengXS <nickcchen@tencent.com>
2023-08-09 11:18:57 +0800

    fix: fix test e2e

--- tencentcloud/resource_tc_cls_data_transform_test.go
+++ tencentcloud/resource_tc_cls_data_transform_test.go
@@ -87,16 +87,51 @@ func testAccCheckClsDataTransformExists(n string) resource.TestCheckFunc {
 }
 
 const testAccClsDataTransform = `
-
+resource "tencentcloud_cls_logset" "logset_src" {
+  logset_name = "tf-example-src"
+  tags = {
+    "createdBy" = "terraform"
+  }
+}
+resource "tencentcloud_cls_topic" "topic_src" {
+  topic_name           = "tf-example_src"
+  logset_id            = tencentcloud_cls_logset.logset_src.id
+  auto_split           = false
+  max_split_partitions = 20
+  partition_count      = 1
+  period               = 10
+  storage_type         = "hot"
+  tags                 = {
+    "test" = "test",
+  }
+}
+resource "tencentcloud_cls_logset" "logset_dst" {
+  logset_name = "tf-example-dst"
+  tags = {
+    "createdBy" = "terraform"
+  }
+}
+resource "tencentcloud_cls_topic" "topic_dst" {
+  topic_name           = "tf-example-dst"
+  logset_id            = tencentcloud_cls_logset.logset_dst.id
+  auto_split           = false
+  max_split_partitions = 20
+  partition_count      = 1
+  period               = 10
+  storage_type         = "hot"
+  tags                 = {
+    "test" = "test",
+  }
+}
 resource "tencentcloud_cls_data_transform" "data_transform" {
   func_type = 1
-  src_topic_id = "88735a07-bea4-4985-8763-e9deb6da4fad"
+  src_topic_id = tencentcloud_cls_topic.topic_src.id
   name = "iac-test-src"
   etl_content = "ext_sep(\"content\", \"f1, f2, f3\", sep=\",\", quote=\"\", restrict=False, mode=\"overwrite\")fields_drop(\"content\")"
   task_type = 3
   enable_flag = 1
   dst_resources {
-    topic_id = "7e34a3a7-635e-4da8-9005-88106c1fde69"
+    topic_id = tencentcloud_cls_topic.topic_dst.id
     alias = "iac-test-dst"
 
   }
