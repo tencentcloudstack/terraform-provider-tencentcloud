67be3dc680e1bf55b75311263aaef3d5615a6e6a
nickyinluo <nickyinluo@tencent.com>
2023-08-16 20:30:18 +0800

    add example for tke node pool with endpoint

---
+++ examples/tencentcloud-tke-nodepool/variables.tf
@@ -0,0 +1,15 @@
+variable "default_instance_type" {
+  default = "SA2.2XLARGE16"
+}
+
+variable "availability_zone_first" {
+  default = "ap-guangzhou-3"
+}
+
+variable "availability_zone_second" {
+  default = "ap-guangzhou-4"
+}
+
+variable "example_cluster_cidr" {
+  default = "10.31.0.0/16"
+}
