926476406257a8f41b01eeef3222b5ee27260330
Arunma <41282634+gitmkn@users.noreply.github.com>
2023-08-25 20:38:14 +0800

    feat: support amp_consumer_id (#2081)
    * feat: support amp_consumer_id
    
    * feat: add chagnelog
    
    * fix: modify test

--- tencentcloud/data_source_tc_monitor_alarm_notices_test.go
+++ tencentcloud/data_source_tc_monitor_alarm_notices_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
+// go test -i; go test -test.run TestAccAlarmNoticesDatasourceBasic -v
 func TestAccAlarmNoticesDatasourceBasic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
@@ -16,9 +17,16 @@ func TestAccAlarmNoticesDatasourceBasic(t *testing.T) {
 				Config: testAccDataSourceAlarmNotices(),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckTencentCloudDataSourceID("data.tencentcloud_monitor_alarm_notices.notices"),
-					//resource.TestCheckResourceAttr("data.tencentcloud_monitor_alarm_notices.notices", "alarm_notice.#", "2"),
-					resource.TestCheckResourceAttr("data.tencentcloud_monitor_alarm_notices.notices", "order", "DESC"),
-					resource.TestCheckResourceAttr("data.tencentcloud_monitor_alarm_notices.notices", "owner_uid", "1"),
+					resource.TestCheckResourceAttr("data.tencentcloud_monitor_alarm_notices.notices", "alarm_notice.#", "1"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_monitor_alarm_notices.notices", "alarm_notice.0.amp_consumer_id"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_monitor_alarm_notices.notices", "alarm_notice.0.is_preset"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_monitor_alarm_notices.notices", "alarm_notice.0.name"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_monitor_alarm_notices.notices", "alarm_notice.0.notice_language"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_monitor_alarm_notices.notices", "alarm_notice.0.notice_type"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_monitor_alarm_notices.notices", "alarm_notice.0.policy_ids.#"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_monitor_alarm_notices.notices", "alarm_notice.0.updated_at"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_monitor_alarm_notices.notices", "alarm_notice.0.updated_by"),
+					resource.TestCheckResourceAttr("data.tencentcloud_monitor_alarm_notices.notices", "alarm_notice.0.user_notices.#", "1"),
 				),
 			},
 		},
@@ -26,13 +34,9 @@ func TestAccAlarmNoticesDatasourceBasic(t *testing.T) {
 }
 
 func testAccDataSourceAlarmNotices() string {
-	return `data "tencentcloud_monitor_alarm_notices" "notices" {
-    order = "DESC"
-    owner_uid = 1
-    name = ""
-    receiver_type = ""
-    user_ids = []
-    group_ids = []
-    notice_ids = []
+	return `
+data "tencentcloud_monitor_alarm_notices" "notices" {
+  order      = "DESC"
+  notice_ids = ["notice-f2svbu3w"]
 }`
 }
