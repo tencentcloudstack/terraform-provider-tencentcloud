553dee948d2691b2a94550de7facade99b7f1a24
WeiMengXS <nickcchen@tencent.com>
2023-09-08 17:00:31 +0800

    feat: resource mfaFlag

--- tencentcloud/resource_tc_cam_mfa_flag_test.go
+++ tencentcloud/resource_tc_cam_mfa_flag_test.go
@@ -16,12 +16,18 @@ func TestAccTencentCloudCamMfaFlagResource_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCamMfaFlag,
-				Check:  resource.ComposeTestCheckFunc(resource.TestCheckResourceAttrSet("tencentcloud_cam_mfa_flag.mfa_flag", "id")),
-			},
-			{
-				ResourceName:      "tencentcloud_cam_mfa_flag.mfa_flag",
-				ImportState:       true,
-				ImportStateVerify: true,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("tencentcloud_cam_mfa_flag.mfa_flag", "id"),
+					resource.TestCheckResourceAttrSet("tencentcloud_cam_mfa_flag.mfa_flag", "login_flag.#"),
+					resource.TestCheckResourceAttr("tencentcloud_cam_mfa_flag.mfa_flag", "login_flag.0.phone", "0"),
+					resource.TestCheckResourceAttr("tencentcloud_cam_mfa_flag.mfa_flag", "login_flag.0.stoken", "1"),
+					resource.TestCheckResourceAttr("tencentcloud_cam_mfa_flag.mfa_flag", "login_flag.0.wechat", "0"),
+
+					resource.TestCheckResourceAttrSet("tencentcloud_cam_mfa_flag.mfa_flag", "action_flag.#"),
+					resource.TestCheckResourceAttr("tencentcloud_cam_mfa_flag.mfa_flag", "action_flag.0.phone", "0"),
+					resource.TestCheckResourceAttr("tencentcloud_cam_mfa_flag.mfa_flag", "action_flag.0.stoken", "1"),
+					resource.TestCheckResourceAttr("tencentcloud_cam_mfa_flag.mfa_flag", "action_flag.0.wechat", "0"),
+				),
 			},
 		},
 	})
@@ -36,7 +42,6 @@ resource "tencentcloud_cam_mfa_flag" "mfa_flag" {
 		phone = 0
 		stoken = 1
 		wechat = 0
-
   }
   action_flag {
 		phone = 0
