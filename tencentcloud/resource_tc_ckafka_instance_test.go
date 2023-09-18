657b57f9ef5c83bf05cf257e00be4f721fc75cc4
tongyiming <tongyiming@threebody.xyz>
2023-09-18 19:52:53 +0800

    support ckafka postpaid instance (#2130)
    * support ckafka prepaid instance
    
    * add changelog
    
    * add changelog
    
    * update test
    
    * update test
    
    * update
    
    * update
    
    * update test
    
    ---------
    
    Co-authored-by: mikatong <mikatong@tencent.com>

--- tencentcloud/resource_tc_ckafka_instance_test.go
+++ tencentcloud/resource_tc_ckafka_instance_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func TestAccTencentCloudCkafkaInstanceResource(t *testing.T) {
+func TestAccTencentCloudCkafkaInstanceResource_prepaid(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheckCommon(t, ACCOUNT_TYPE_PREPAY) },
@@ -20,14 +20,14 @@ func TestAccTencentCloudCkafkaInstanceResource(t *testing.T) {
 				Config: testAccKafkaInstance,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckKafkaInstanceExists("tencentcloud_ckafka_instance.kafka_instance"),
-					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "instance_name", "ckafka-instance-tf-test"),
-					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "zone_id", "100003"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "instance_name", "ckafka-instance-prepaid"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "zone_id", "100007"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "period", "1"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "msg_retention_time", "1300"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "max_message_byte", "1024"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "renew_flag", "0"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "kafka_version", "1.1.1"),
-					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "disk_size", "500"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "disk_size", "200"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "disk_type", "CLOUD_BASIC"),
 					resource.TestCheckResourceAttrSet("tencentcloud_ckafka_instance.kafka_instance", "vip"),
 					resource.TestCheckResourceAttrSet("tencentcloud_ckafka_instance.kafka_instance", "vport"),
@@ -37,14 +37,14 @@ func TestAccTencentCloudCkafkaInstanceResource(t *testing.T) {
 				Config: testAccKafkaInstanceUpdate,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckKafkaInstanceExists("tencentcloud_ckafka_instance.kafka_instance"),
-					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "instance_name", "ckafka-instance-tf-test"),
-					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "zone_id", "100003"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "instance_name", "ckafka-instance-prepaid"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "zone_id", "100007"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "period", "1"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "msg_retention_time", "1200"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "max_message_byte", "1025"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "renew_flag", "0"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "kafka_version", "1.1.1"),
-					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "disk_size", "500"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "disk_size", "200"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "disk_type", "CLOUD_BASIC"),
 				),
 			},
@@ -52,13 +52,56 @@ func TestAccTencentCloudCkafkaInstanceResource(t *testing.T) {
 				ResourceName:            "tencentcloud_ckafka_instance.kafka_instance",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"period", "max_message_byte"},
+				ImportStateVerifyIgnore: []string{"period", "max_message_byte", "charge_type"},
 			},
 		},
 	})
 }
 
-func TestAccTencentCloudCkafkaInstanceMAZResource(t *testing.T) {
+func TestAccTencentCloudCkafkaInstanceResource_postpaid(t *testing.T) {
+	t.Parallel()
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheckCommon(t, ACCOUNT_TYPE_PREPAY) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccTencentCloudKafkaInstanceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccKafkaInstancePostpaid,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckKafkaInstanceExists("tencentcloud_ckafka_instance.kafka_instance_postpaid"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance_postpaid", "instance_name", "ckafka-instance-postpaid"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance_postpaid", "zone_id", "100007"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance_postpaid", "msg_retention_time", "1300"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance_postpaid", "kafka_version", "1.1.1"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance_postpaid", "disk_size", "200"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance_postpaid", "disk_type", "CLOUD_BASIC"),
+					resource.TestCheckResourceAttrSet("tencentcloud_ckafka_instance.kafka_instance_postpaid", "vip"),
+					resource.TestCheckResourceAttrSet("tencentcloud_ckafka_instance.kafka_instance_postpaid", "vport"),
+				),
+			},
+			{
+				Config: testAccKafkaInstanceUpdatePostpaid,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckKafkaInstanceExists("tencentcloud_ckafka_instance.kafka_instance_postpaid"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance_postpaid", "instance_name", "ckafka-instance-postpaid"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance_postpaid", "zone_id", "100007"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance_postpaid", "msg_retention_time", "1200"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance_postpaid", "kafka_version", "1.1.1"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance_postpaid", "disk_size", "200"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance_postpaid", "disk_type", "CLOUD_BASIC"),
+				),
+			},
+			{
+				ResourceName:            "tencentcloud_ckafka_instance.kafka_instance_postpaid",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"period", "max_message_byte", "charge_type"},
+			},
+		},
+	})
+}
+
+func TestAccTencentCloudCkafkaInstanceResource_maz(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheckCommon(t, ACCOUNT_TYPE_PREPAY) },
@@ -70,7 +113,7 @@ func TestAccTencentCloudCkafkaInstanceMAZResource(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckKafkaInstanceExists("tencentcloud_ckafka_instance.kafka_instance"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "instance_name", "ckafka-instance-maz-tf-test"),
-					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "zone_id", "100003"),
+					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "zone_id", "100007"),
 					resource.TestCheckResourceAttr("tencentcloud_ckafka_instance.kafka_instance", "zone_ids.#", "2"),
 				),
 			},
@@ -78,13 +121,13 @@ func TestAccTencentCloudCkafkaInstanceMAZResource(t *testing.T) {
 				ResourceName:            "tencentcloud_ckafka_instance.kafka_instance",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"period", "max_message_byte"},
+				ImportStateVerifyIgnore: []string{"period", "max_message_byte", "charge_type"},
 			},
 		},
 	})
 }
 
-func TestAccTencentCloudCkafkaInstanceTypeResource(t *testing.T) {
+func TestAccTencentCloudCkafkaInstanceResource_type(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheckCommon(t, ACCOUNT_TYPE_PREPAY) },
@@ -104,7 +147,7 @@ func TestAccTencentCloudCkafkaInstanceTypeResource(t *testing.T) {
 				ResourceName:            "tencentcloud_ckafka_instance.kafka_instance",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"period", "max_message_byte"},
+				ImportStateVerifyIgnore: []string{"period", "max_message_byte", "charge_type"},
 			},
 		},
 	})
@@ -169,8 +212,8 @@ func testAccCheckKafkaInstanceExists(n string) resource.TestCheckFunc {
 
 const testAccKafkaInstance = defaultKafkaVariable + `
 resource "tencentcloud_ckafka_instance" "kafka_instance" {
-  instance_name      = "ckafka-instance-tf-test"
-  zone_id            = 100003
+  instance_name      = "ckafka-instance-prepaid"
+  zone_id            = 100007
   period             = 1
   vpc_id             = var.vpc_id
   subnet_id          = var.subnet_id
@@ -178,8 +221,10 @@ resource "tencentcloud_ckafka_instance" "kafka_instance" {
   max_message_byte   = 1024
   renew_flag         = 0
   kafka_version      = "1.1.1"
-  disk_size          = 500
+  disk_size          = 200
+  band_width         = 20
   disk_type          = "CLOUD_BASIC"
+  partition          = 400
 
 
   config {
@@ -196,8 +241,8 @@ resource "tencentcloud_ckafka_instance" "kafka_instance" {
 
 const testAccKafkaInstanceUpdate = defaultKafkaVariable + `
 resource "tencentcloud_ckafka_instance" "kafka_instance" {
-  instance_name      = "ckafka-instance-tf-test"
-  zone_id            = 100003
+  instance_name      = "ckafka-instance-prepaid"
+  zone_id            = 100007
   period             = 1
   vpc_id             =  var.vpc_id
   subnet_id          =  var.subnet_id
@@ -205,9 +250,66 @@ resource "tencentcloud_ckafka_instance" "kafka_instance" {
   max_message_byte   = 1025
   renew_flag         = 0
   kafka_version      = "1.1.1"
-  disk_size          = 500
+  disk_size          = 200
+  band_width         = 20
   disk_type          = "CLOUD_BASIC"
+  partition          = 400
 
+
+  config {
+    auto_create_topic_enable   = true
+    default_num_partitions     = 3
+    default_replication_factor = 3
+  }
+
+  dynamic_retention_config {
+    enable = 1
+  }
+
+  tag_set = {
+    createdBy = "terraform"
+  }
+}
+`
+
+const testAccKafkaInstancePostpaid = defaultKafkaVariable + `
+resource "tencentcloud_ckafka_instance" "kafka_instance_postpaid" {
+  instance_name      = "ckafka-instance-postpaid"
+  zone_id            = 100007
+  vpc_id             = var.vpc_id
+  subnet_id          = var.subnet_id
+  msg_retention_time = 1300
+  kafka_version      = "1.1.1"
+  disk_size          = 200
+  band_width         = 20
+  disk_type          = "CLOUD_BASIC"
+  partition          = 400
+  charge_type        = "POSTPAID_BY_HOUR"
+
+  config {
+    auto_create_topic_enable   = true
+    default_num_partitions     = 3
+    default_replication_factor = 3
+  }
+
+  dynamic_retention_config {
+    enable = 1
+  }
+}
+`
+
+const testAccKafkaInstanceUpdatePostpaid = defaultKafkaVariable + `
+resource "tencentcloud_ckafka_instance" "kafka_instance_postpaid" {
+  instance_name      = "ckafka-instance-postpaid"
+  zone_id            = 100007
+  vpc_id             =  var.vpc_id
+  subnet_id          =  var.subnet_id
+  msg_retention_time = 1200
+  kafka_version      = "1.1.1"
+  disk_type          = "CLOUD_BASIC"
+  disk_size          = 200
+  band_width         = 20
+  charge_type        = "POSTPAID_BY_HOUR"
 
   config {
     auto_create_topic_enable   = true
@@ -228,9 +330,9 @@ resource "tencentcloud_ckafka_instance" "kafka_instance" {
 const testAccKafkaInstanceMAZ = defaultKafkaVariable + `
 resource "tencentcloud_ckafka_instance" "kafka_instance" {
   instance_name      = "ckafka-instance-maz-tf-test"
-  zone_id            = 100003
+  zone_id            = 100007
   multi_zone_flag    = true
-  zone_ids           = [100003, 100006]
+  zone_ids           = [100007, 100006]
   period             = 1
   vpc_id             = var.vpc_id
   subnet_id          = var.subnet_id
@@ -253,22 +355,32 @@ resource "tencentcloud_ckafka_instance" "kafka_instance" {
 }
 `
 
-const testAccKafkaInstanceType = defaultKafkaVariable + `
+const testAccKafkaInstanceType = `
+resource "tencentcloud_vpc" "vpc" {
+  name       = "tmp"
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "tencentcloud_subnet" "subnet" {
+  vpc_id            = tencentcloud_vpc.vpc.id
+  name              = "subnet-example"
+  cidr_block        = "10.0.0.0/16"
+  availability_zone = "ap-guangzhou-7"
+}
+
 resource "tencentcloud_ckafka_instance" "kafka_instance" {
   instance_name      = "ckafka-instance-type-tf-test"
-  zone_id            = 100003
-  period             = 1
-  vpc_id             = var.vpc_id
-  subnet_id          = var.subnet_id
+  zone_id            = 100007
+  vpc_id             = tencentcloud_vpc.vpc.id
+  subnet_id          = tencentcloud_subnet.subnet.id
   msg_retention_time = 1300
-  renew_flag         = 0
   kafka_version      = "1.1.1"
-  disk_size          = 1000
-  disk_type          = "CLOUD_BASIC"
-
   specifications_type = "standard"
   instance_type       = 2
-
+  disk_size          = 1000
+  disk_type          = "CLOUD_BASIC"
+  band_width         = 100
+  charge_type        = "POSTPAID_BY_HOUR"
 
   config {
     auto_create_topic_enable   = true
