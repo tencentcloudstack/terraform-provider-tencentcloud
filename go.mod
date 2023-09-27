dde0fb5a16f45e41e9fcd2f0266786d85c835cec
nickyinluo <nickyinluo@tencent.com>
2023-09-28 00:56:44 +0800

    feat:support mps withdraw wm operation

--- go.mod
+++ go.mod
@@ -64,7 +64,7 @@ require (
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/mariadb v1.0.672
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/mongodb v1.0.651
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/monitor v1.0.734
-	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/mps v1.0.584
+	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/mps v1.0.758
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/organization v1.0.540
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/postgres v1.0.676
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/privatedns v1.0.751
---
+++ tencentcloud/resource_tc_mps_withdraws_watermark_operation.go
@@ -0,0 +1,391 @@
+/*
+Provides a resource to create a mps withdraws_watermark_operation
+
+Example Usage
+
+```hcl
+resource "tencentcloud_mps_withdraws_watermark_operation" "withdraws_watermark_operation" {
+  input_info {
+		type = ""
+		cos_input_info {
+			bucket = ""
+			region = ""
+			object = ""
+		}
+		url_input_info {
+			url = ""
+		}
+		s3_input_info {
+			s3_bucket = ""
+			s3_region = ""
+			s3_object = ""
+			s3_secret_id = ""
+			s3_secret_key = ""
+		}
+
+  }
+  task_notify_config {
+		cmq_model = ""
+		cmq_region = ""
+		topic_name = ""
+		queue_name = ""
+		notify_mode = ""
+		notify_type = ""
+		notify_url = ""
+		aws_sqs {
+			sqs_region = ""
+			sqs_queue_name = ""
+			s3_secret_id = ""
+			s3_secret_key = ""
+		}
+
+  }
+  session_context = ""
+}
+```
+
+Import
+
+mps withdraws_watermark_operation can be imported using the id, e.g.
+
+```
+terraform import tencentcloud_mps_withdraws_watermark_operation.withdraws_watermark_operation withdraws_watermark_operation_id
+```
+*/
+package tencentcloud
+
+import (
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	mps "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/mps/v20190612"
+	"github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/internal/helper"
+)
+
+func resourceTencentCloudMpsWithdrawsWatermarkOperation() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceTencentCloudMpsWithdrawsWatermarkOperationCreate,
+		Read:   resourceTencentCloudMpsWithdrawsWatermarkOperationRead,
+		Delete: resourceTencentCloudMpsWithdrawsWatermarkOperationDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"input_info": {
+				Required:    true,
+				ForceNew:    true,
+				Type:        schema.TypeList,
+				MaxItems:    1,
+				Description: "Input information of file for metadata getting.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"type": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "The input type. Valid values:&lt;li&gt;`COS`: A COS bucket address.&lt;/li&gt;&lt;li&gt; `URL`: A URL.&lt;/li&gt;&lt;li&gt; `AWS-S3`: An AWS S3 bucket address. Currently, this type is only supported for transcoding tasks.&lt;/li&gt;.",
+						},
+						"cos_input_info": {
+							Type:        schema.TypeList,
+							MaxItems:    1,
+							Optional:    true,
+							Description: "The information of the COS object to process. This parameter is valid and required when `Type` is `COS`.",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"bucket": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "The COS bucket of the object to process, such as `TopRankVideo-125xxx88`.",
+									},
+									"region": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "The region of the COS bucket, such as `ap-chongqing`.",
+									},
+									"object": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "The path of the object to process, such as `/movie/201907/WildAnimal.mov`.",
+									},
+								},
+							},
+						},
+						"url_input_info": {
+							Type:        schema.TypeList,
+							MaxItems:    1,
+							Optional:    true,
+							Description: "The URL of the object to process. This parameter is valid and required when `Type` is `URL`.Note: This field may return null, indicating that no valid value can be obtained.",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"url": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "URL of a video.",
+									},
+								},
+							},
+						},
+						"s3_input_info": {
+							Type:        schema.TypeList,
+							MaxItems:    1,
+							Optional:    true,
+							Description: "The information of the AWS S3 object processed. This parameter is required if `Type` is `AWS-S3`.Note: This field may return null, indicating that no valid value can be obtained.",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"s3_bucket": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "The AWS S3 bucket.",
+									},
+									"s3_region": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "The region of the AWS S3 bucket.",
+									},
+									"s3_object": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "The path of the AWS S3 object.",
+									},
+									"s3_secret_id": {
+										Type:        schema.TypeString,
+										Optional:    true,
+										Description: "The key ID required to access the AWS S3 object.",
+									},
+									"s3_secret_key": {
+										Type:        schema.TypeString,
+										Optional:    true,
+										Description: "The key required to access the AWS S3 object.",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+
+			"task_notify_config": {
+				Optional:    true,
+				ForceNew:    true,
+				Type:        schema.TypeList,
+				MaxItems:    1,
+				Description: "Event notification information of a task. If this parameter is left empty, no event notifications will be obtained.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"cmq_model": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "The CMQ or TDMQ-CMQ model. Valid values: Queue, Topic.",
+						},
+						"cmq_region": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "The CMQ or TDMQ-CMQ region, such as `sh` (Shanghai) or `bj` (Beijing).",
+						},
+						"topic_name": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "The CMQ or TDMQ-CMQ topic to receive notifications. This parameter is valid when `CmqModel` is `Topic`.",
+						},
+						"queue_name": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "The CMQ or TDMQ-CMQ queue to receive notifications. This parameter is valid when `CmqModel` is `Queue`.",
+						},
+						"notify_mode": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "Workflow notification method. Valid values: Finish, Change. If this parameter is left empty, `Finish` will be used.",
+						},
+						"notify_type": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "The notification type. Valid values: &lt;li&gt;`CMQ`: This value is no longer used. Please use `TDMQ-CMQ` instead.&lt;/li&gt; &lt;li&gt;`TDMQ-CMQ`: Message queue&lt;/li&gt; &lt;li&gt;`URL`: If `NotifyType` is set to `URL`, HTTP callbacks are sent to the URL specified by `NotifyUrl`. HTTP and JSON are used for the callbacks. The packet contains the response parameters of the `ParseNotification` API.&lt;/li&gt; &lt;li&gt;`SCF`: This notification type is not recommended. You need to configure it in the SCF console.&lt;/li&gt; &lt;li&gt;`AWS-SQS`: AWS queue. This type is only supported for AWS tasks, and the queue must be in the same region as the AWS bucket.&lt;/li&gt; &lt;font color=red&gt;Note: If you do not pass this parameter or pass in an empty string, `CMQ` will be used. To use a different notification type, specify this parameter accordingly.&lt;/font&gt;.",
+						},
+						"notify_url": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "HTTP callback URL, required if `NotifyType` is set to `URL`.",
+						},
+						"aws_sqs": {
+							Type:        schema.TypeList,
+							MaxItems:    1,
+							Optional:    true,
+							Description: "The AWS SQS queue. This parameter is required if `NotifyType` is `AWS-SQS`.Note: This field may return null, indicating that no valid values can be obtained.",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"sqs_region": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "The region of the SQS queue.",
+									},
+									"sqs_queue_name": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "The name of the SQS queue.",
+									},
+									"s3_secret_id": {
+										Type:        schema.TypeString,
+										Optional:    true,
+										Description: "The key ID required to read from/write to the SQS queue.",
+									},
+									"s3_secret_key": {
+										Type:        schema.TypeString,
+										Optional:    true,
+										Description: "The key required to read from/write to the SQS queue.",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+
+			"session_context": {
+				Optional:    true,
+				ForceNew:    true,
+				Type:        schema.TypeString,
+				Description: "The source context which is used to pass through the user request information. The task flow status change callback will return the value of this field.",
+			},
+		},
+	}
+}
+
+func resourceTencentCloudMpsWithdrawsWatermarkOperationCreate(d *schema.ResourceData, meta interface{}) error {
+	defer logElapsed("resource.tencentcloud_mps_withdraws_watermark_operation.create")()
+	defer inconsistentCheck(d, meta)()
+
+	logId := getLogId(contextNil)
+
+	var (
+		request  = mps.NewWithdrawsWatermarkRequest()
+		response = mps.NewWithdrawsWatermarkResponse()
+		taskId   string
+	)
+	if dMap, ok := helper.InterfacesHeadMap(d, "input_info"); ok {
+		mediaInputInfo := mps.MediaInputInfo{}
+		if v, ok := dMap["type"]; ok {
+			mediaInputInfo.Type = helper.String(v.(string))
+		}
+		if cosInputInfoMap, ok := helper.InterfaceToMap(dMap, "cos_input_info"); ok {
+			cosInputInfo := mps.CosInputInfo{}
+			if v, ok := cosInputInfoMap["bucket"]; ok {
+				cosInputInfo.Bucket = helper.String(v.(string))
+			}
+			if v, ok := cosInputInfoMap["region"]; ok {
+				cosInputInfo.Region = helper.String(v.(string))
+			}
+			if v, ok := cosInputInfoMap["object"]; ok {
+				cosInputInfo.Object = helper.String(v.(string))
+			}
+			mediaInputInfo.CosInputInfo = &cosInputInfo
+		}
+		if urlInputInfoMap, ok := helper.InterfaceToMap(dMap, "url_input_info"); ok {
+			urlInputInfo := mps.UrlInputInfo{}
+			if v, ok := urlInputInfoMap["url"]; ok {
+				urlInputInfo.Url = helper.String(v.(string))
+			}
+			mediaInputInfo.UrlInputInfo = &urlInputInfo
+		}
+		if s3InputInfoMap, ok := helper.InterfaceToMap(dMap, "s3_input_info"); ok {
+			s3InputInfo := mps.S3InputInfo{}
+			if v, ok := s3InputInfoMap["s3_bucket"]; ok {
+				s3InputInfo.S3Bucket = helper.String(v.(string))
+			}
+			if v, ok := s3InputInfoMap["s3_region"]; ok {
+				s3InputInfo.S3Region = helper.String(v.(string))
+			}
+			if v, ok := s3InputInfoMap["s3_object"]; ok {
+				s3InputInfo.S3Object = helper.String(v.(string))
+			}
+			if v, ok := s3InputInfoMap["s3_secret_id"]; ok {
+				s3InputInfo.S3SecretId = helper.String(v.(string))
+			}
+			if v, ok := s3InputInfoMap["s3_secret_key"]; ok {
+				s3InputInfo.S3SecretKey = helper.String(v.(string))
+			}
+			mediaInputInfo.S3InputInfo = &s3InputInfo
+		}
+		request.InputInfo = &mediaInputInfo
+	}
+
+	if dMap, ok := helper.InterfacesHeadMap(d, "task_notify_config"); ok {
+		taskNotifyConfig := mps.TaskNotifyConfig{}
+		if v, ok := dMap["cmq_model"]; ok {
+			taskNotifyConfig.CmqModel = helper.String(v.(string))
+		}
+		if v, ok := dMap["cmq_region"]; ok {
+			taskNotifyConfig.CmqRegion = helper.String(v.(string))
+		}
+		if v, ok := dMap["topic_name"]; ok {
+			taskNotifyConfig.TopicName = helper.String(v.(string))
+		}
+		if v, ok := dMap["queue_name"]; ok {
+			taskNotifyConfig.QueueName = helper.String(v.(string))
+		}
+		if v, ok := dMap["notify_mode"]; ok {
+			taskNotifyConfig.NotifyMode = helper.String(v.(string))
+		}
+		if v, ok := dMap["notify_type"]; ok {
+			taskNotifyConfig.NotifyType = helper.String(v.(string))
+		}
+		if v, ok := dMap["notify_url"]; ok {
+			taskNotifyConfig.NotifyUrl = helper.String(v.(string))
+		}
+		if awsSqsMap, ok := helper.InterfaceToMap(dMap, "aws_sqs"); ok {
+			awsSqs := mps.AwsSQS{}
+			if v, ok := awsSqsMap["sqs_region"]; ok {
+				awsSqs.SQSRegion = helper.String(v.(string))
+			}
+			if v, ok := awsSqsMap["sqs_queue_name"]; ok {
+				awsSqs.SQSQueueName = helper.String(v.(string))
+			}
+			if v, ok := awsSqsMap["s3_secret_id"]; ok {
+				awsSqs.S3SecretId = helper.String(v.(string))
+			}
+			if v, ok := awsSqsMap["s3_secret_key"]; ok {
+				awsSqs.S3SecretKey = helper.String(v.(string))
+			}
+			taskNotifyConfig.AwsSQS = &awsSqs
+		}
+		request.TaskNotifyConfig = &taskNotifyConfig
+	}
+
+	if v, ok := d.GetOk("session_context"); ok {
+		request.SessionContext = helper.String(v.(string))
+	}
+
+	err := resource.Retry(writeRetryTimeout, func() *resource.RetryError {
+		result, e := meta.(*TencentCloudClient).apiV3Conn.UseMpsClient().WithdrawsWatermark(request)
+		if e != nil {
+			return retryError(e)
+		} else {
+			log.Printf("[DEBUG]%s api[%s] success, request body [%s], response body [%s]\n", logId, request.GetAction(), request.ToJsonString(), result.ToJsonString())
+		}
+		response = result
+		return nil
+	})
+	if err != nil {
+		log.Printf("[CRITAL]%s operate mps withdrawsWatermarkOperation failed, reason:%+v", logId, err)
+		return err
+	}
+
+	taskId = *response.Response.TaskId
+	d.SetId(taskId)
+
+	return resourceTencentCloudMpsWithdrawsWatermarkOperationRead(d, meta)
+}
+
+func resourceTencentCloudMpsWithdrawsWatermarkOperationRead(d *schema.ResourceData, meta interface{}) error {
+	defer logElapsed("resource.tencentcloud_mps_withdraws_watermark_operation.read")()
+	defer inconsistentCheck(d, meta)()
+
+	return nil
+}
+
+func resourceTencentCloudMpsWithdrawsWatermarkOperationDelete(d *schema.ResourceData, meta interface{}) error {
+	defer logElapsed("resource.tencentcloud_mps_withdraws_watermark_operation.delete")()
+	defer inconsistentCheck(d, meta)()
+
+	return nil
+}
