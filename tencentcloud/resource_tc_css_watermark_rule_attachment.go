ee1aaf34e41f2057bdb740ae8d252e3ea386d311
nickyinluo <nickyinluo@tencent.com>
2023-09-27 13:02:33 +0800

    add e2e

--- tencentcloud/resource_tc_css_watermark_rule_attachment.go
+++ tencentcloud/resource_tc_css_watermark_rule_attachment.go
@@ -3,23 +3,35 @@ Provides a resource to create a css watermark_rule
 
 Example Usage
 
-Binding domain with a css watermark rule
+Binding watermark rule with a css stream
 
 ```hcl
-resource "tencentcloud_css_watermark" "watermark" {
-  picture_url = "picture_url"
-  watermark_name = "watermark_name"
-  x_position = 0
-  y_position = 0
-  width = 0
-  height = 0
+resource "tencentcloud_css_pull_stream_task" "example" {
+  stream_name = "tf_example_stream_name"
+  source_type = "PullLivePushLive"
+  source_urls = ["rtmp://xxx.com/live/stream"]
+  domain_name = "test.domain.com"
+  app_name    = "live"
+  start_time  = "2023-09-27T10:28:21Z"
+  end_time    = "2023-09-27T17:28:21Z"
+  operator    = "tf_admin"
+  comment     = "This is a e2e test case."
 }
 
-resource "tencentcloud_css_watermark_rule_attachment" "watermark_rule" {
-  domain_name = "example"
-  app_name    = "example"
-  stream_name = "example"
-  template_id = tencentcloud_css_watermark.watermark.id
+resource "tencentcloud_css_watermark" "example" {
+  picture_url    = "picture_url"
+  watermark_name = "watermark_name"
+  x_position     = 0
+  y_position     = 0
+  width          = 0
+  height         = 0
+}
+
+resource "tencentcloud_css_watermark_rule_attachment" "watermark_rule_attachment" {
+  domain_name = tencentcloud_css_pull_stream_task.example.domain_name
+  app_name    = tencentcloud_css_pull_stream_task.example.app_name
+  stream_name = tencentcloud_css_pull_stream_task.example.stream_name
+  template_id = tencentcloud_css_watermark.example.id
 }
 ```
 
@@ -84,7 +96,7 @@ func resourceTencentCloudCssWatermarkRuleAttachment() *schema.Resource {
 
 			"create_time": {
 				Computed:    true,
-				Type:        schema.TypeInt,
+				Type:        schema.TypeString,
 				Description: "create time.",
 			},
 
--- website/docs/r/css_watermark_rule_attachment.html.markdown
+++ website/docs/r/css_watermark_rule_attachment.html.markdown
@@ -13,10 +13,22 @@ Provides a resource to create a css watermark_rule
 
 ## Example Usage
 
-### Binding domain with a css watermark rule
+### Binding watermark rule with a css stream
 
 ```hcl
-resource "tencentcloud_css_watermark" "watermark" {
+resource "tencentcloud_css_pull_stream_task" "example" {
+  stream_name = "tf_example_stream_name"
+  source_type = "PullLivePushLive"
+  source_urls = ["rtmp://xxx.com/live/stream"]
+  domain_name = "test.domain.com"
+  app_name    = "live"
+  start_time  = "2023-09-27T10:28:21Z"
+  end_time    = "2023-09-27T17:28:21Z"
+  operator    = "tf_admin"
+  comment     = "This is a e2e test case."
+}
+
+resource "tencentcloud_css_watermark" "example" {
   picture_url    = "picture_url"
   watermark_name = "watermark_name"
   x_position     = 0
@@ -25,11 +37,11 @@ resource "tencentcloud_css_watermark" "watermark" {
   height         = 0
 }
 
-resource "tencentcloud_css_watermark_rule_attachment" "watermark_rule" {
-  domain_name = "example"
-  app_name    = "example"
-  stream_name = "example"
-  template_id = tencentcloud_css_watermark.watermark.id
+resource "tencentcloud_css_watermark_rule_attachment" "watermark_rule_attachment" {
+  domain_name = tencentcloud_css_pull_stream_task.example.domain_name
+  app_name    = tencentcloud_css_pull_stream_task.example.app_name
+  stream_name = tencentcloud_css_pull_stream_task.example.stream_name
+  template_id = tencentcloud_css_watermark.example.id
 }
 ```
 
