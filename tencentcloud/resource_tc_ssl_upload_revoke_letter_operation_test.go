cee55a1d053354ddf623bd9d09283f3a4246daa7
WeiMengXS <nickcchen@tencent.com>
2023-10-08 16:46:32 +0800

    feat: changelog

---
+++ tencentcloud/resource_tc_ssl_upload_revoke_letter_operation_test.go
@@ -0,0 +1,40 @@
+package tencentcloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccTencentCloudSslUploadRevokeLetterResource_basic(t *testing.T) {
+	t.Parallel()
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckCommon(t, ACCOUNT_TYPE_SSL)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSslUploadRevokeLetter,
+				Check: resource.ComposeTestCheckFunc(resource.TestCheckResourceAttrSet("tencentcloud_ssl_upload_revoke_letter.upload_revoke_letter", "id"),
+					resource.TestCheckResourceAttr("tencentcloud_ssl_upload_revoke_letter.upload_revoke_letter", "certificate_id", "8xRYdDlc"),
+					resource.TestCheckResourceAttrSet("tencentcloud_ssl_upload_revoke_letter.upload_revoke_letter", "revoke_letter"),
+				),
+			},
+			{
+				ResourceName:      "tencentcloud_ssl_upload_revoke_letter.upload_revoke_letter",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+const testAccSslUploadRevokeLetter = `
+
+resource "tencentcloud_ssl_upload_revoke_letter" "upload_revoke_letter" {
+  certificate_id = "8xRYdDlc"
+  revoke_letter = filebase64("./c.pdf")
+}
+
+`
