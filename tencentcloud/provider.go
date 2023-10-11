2d628ade65bbda2bfc38b85111e05c30dc65a09e
WeiMengXS <nickcchen@tencent.com>
2023-10-11 19:20:51 +0800

    fix: fmt

--- tencentcloud/provider.go
+++ tencentcloud/provider.go
@@ -2494,7 +2494,7 @@ func Provider() *schema.Provider {
 			"tencentcloud_ses_statistics_report":                       dataSourceTencentCloudSesStatisticsReport(),
 			"tencentcloud_ses_send_email_status":                       dataSourceTencentCloudSesSendEmailStatus(),
 			"tencentcloud_organization_org_financial_by_member":      dataSourceTencentCloudOrganizationOrgFinancialByMember(),
-			"tencentcloud_organization_org_auth_node":                dataSourceTencentCloudOrganizationOrgAuthNode(),
+			"tencentcloud_organization_org_auth_node":                  dataSourceTencentCloudOrganizationOrgAuthNode(),
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
