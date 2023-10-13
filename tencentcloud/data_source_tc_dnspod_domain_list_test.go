f7c9f2bdd0df637859be5bb0534fae92ca800b76
andrewjiang <andrewjiang@tencent.com>
2023-10-13 23:34:57 +0800

    feat(dnspod): add datasource

--- tencentcloud/data_source_tc_dnspod_domain_list_test.go
+++ tencentcloud/data_source_tc_dnspod_domain_list_test.go
@@ -25,20 +25,20 @@ const testAccDnspodDomain_listDataSource = `
 
 data "tencentcloud_dnspod_domain_list" "domain_list" {
   type = "ALL"
-  #  group_id = 
-  keyword = "keyword_demo"
+  group_id = [1]
+  # keyword = "keyword_demo"
   sort_field = "UPDATED_ON"
   sort_type = "DESC"
-  # status = 
-  # package = 
-  remark = "remark_demo"
-  updated_at_begin = "2021-05-01 03:00:00"
-  updated_at_end = "2021-05-10 20:00:00"
-  record_count_begin = 0
-  record_count_end = 100
-  project_id = -1
-      tags = {
-    "createdBy" = "terraform"
+  status = ["PAUSE"]
+  # package =
+  remark = ""
+  # updated_at_begin = "2021-05-01 03:00:00"
+  # updated_at_end = "2021-05-10 20:00:00"
+  # record_count_begin = 0
+  # record_count_end = 100
+  # project_id = -1
+  tags = {
+   "createdBy" = "terraform"
   }
 }
 
