acae876aa96a88de885d5a1b3807e197a62bdea6
andrewjiang <andrewjiang@tencent.com>
2023-10-16 19:51:04 +0800

    docs(dnspod): Add datasource of dnspod domain analytics

---
+++ tencentcloud/data_source_tc_dnspod_domain_analytics_test.go
@@ -0,0 +1,37 @@
+package tencentcloud
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"testing"
+)
+
+func TestAccTencentCloudDnspodDomainAnalyticsDataSource_basic(t *testing.T) {
+	t.Parallel()
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDnspodDomainAnalyticsDataSource,
+				Check:  resource.ComposeTestCheckFunc(testAccCheckTencentCloudDataSourceID("data.tencentcloud_dnspod_domain_analytics.domain_analytics")),
+			},
+		},
+	})
+}
+
+const testAccDnspodDomainAnalyticsDataSource = `
+
+data "tencentcloud_dnspod_domain_analytics" "domain_analytics" {
+  domain = "dnspod.cn"
+  start_date = "2023-10-07"
+  end_date = "2023-10-12"
+  dns_format = "HOUR"
+  # domain_id = 123
+  tags = {
+    "createdBy" = "terraform"
+  }
+}
+
+`
