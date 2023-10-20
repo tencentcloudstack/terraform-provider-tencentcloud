e9df4c14238c4560b9e6a5016b79d6da2736bf7c
andrewjiang <andrewjiang@tencent.com>
2023-10-20 08:36:33 +0800

    feat(dnspod): e2e use pre uin

--- tencentcloud/data_source_tc_dnspod_domain_analytics_test.go
+++ tencentcloud/data_source_tc_dnspod_domain_analytics_test.go
@@ -9,7 +9,7 @@ import (
 func TestAccTencentCloudDnspodDomainAnalyticsDataSource_basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheckCommon(t, ACCOUNT_TYPE_PREPAY) },
+		PreCheck: func() { testAccPreCheckCommon(t, ACCOUNT_TYPE_PREPAY) },
 		// PreCheck: func() {
 		// 	testAccPreCheck(t)
 		// },
