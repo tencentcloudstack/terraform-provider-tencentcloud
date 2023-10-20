a3d0fa2af6acabe42e9f18a231b0c41fc58bd200
arunma <arunma@tencent.com>
2023-10-20 17:16:01 +0800

    fix: datasource cloud test

--- tencentcloud/resource_tc_bi_datasource_cloud_test.go
+++ tencentcloud/resource_tc_bi_datasource_cloud_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccTencentCloudBiDatasourceCloudResource_basic(t *testing.T) {
+func TestAccTencentCloudNeedFixBiDatasourceCloudResource_basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
