61a1878868b1e07cc929152d4b4d8d37501c3f33
WeiMengXS <nickcchen@tencent.com>
2023-10-23 19:29:16 +0800

    feat: datasource

---
+++ tencentcloud/data_source_tc_dlc_describe_user_roles_test.go
@@ -0,0 +1,31 @@
+package tencentcloud
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"testing"
+)
+
+func TestAccTencentCloudDlcDescribeUserRolesDataSource_basic(t *testing.T) {
+	t.Parallel()
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDlcDescribeUserRolesDataSource,
+				Check: resource.ComposeTestCheckFunc(testAccCheckTencentCloudDataSourceID("data.tencentcloud_dlc_describe_user_roles.describe_user_roles"),
+					resource.TestCheckResourceAttr("data.tencentcloud_dlc_describe_user_roles.describe_user_roles", "fuzzy", "1")),
+			},
+		},
+	})
+}
+
+const testAccDlcDescribeUserRolesDataSource = `
+
+data "tencentcloud_dlc_describe_user_roles" "describe_user_roles" {
+  fuzzy = "1"
+  }
+
+`
