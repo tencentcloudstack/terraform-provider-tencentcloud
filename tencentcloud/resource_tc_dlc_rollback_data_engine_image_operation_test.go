213160365bdec3c7b4905accbd6e0f90d93cbe54
WeiMengXS <nickcchen@tencent.com>
2023-10-24 21:46:01 +0800

    feat: changelog

--- tencentcloud/resource_tc_dlc_rollback_data_engine_image_operation_test.go
+++ tencentcloud/resource_tc_dlc_rollback_data_engine_image_operation_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccTencentCloudDlcRollbackDataEngineImageResource_basic(t *testing.T) {
+func TestAccTencentCloudDlcRollbackDataEngineImageOperationResource_basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
