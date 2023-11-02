42b0a9503575b87259c8415e3ec2b5820e191c1a
WeiMengXS <nickcchen@tencent.com>
2023-11-02 15:29:05 +0800

    feat: update row filter

--- tencentcloud/provider.go
+++ tencentcloud/provider.go
@@ -1921,6 +1921,7 @@ Data Lake Compute(DLC)
 	tencentcloud_dlc_switch_data_engine_image_operation
 	tencentcloud_dlc_upgrade_data_engine_image_operation
 	tencentcloud_dlc_user_data_engine_config
+	tencentcloud_dlc_update_row_filter
 	tencentcloud_dlc_bind_work_groups_to_user_attachment
 
 WeData
@@ -3674,6 +3675,7 @@ func Provider() *schema.Provider {
 			"tencentcloud_dlc_upgrade_data_engine_image_operation":             resourceTencentCloudDlcUpgradeDataEngineImageOperation(),
 			"tencentcloud_dlc_bind_work_groups_to_user_attachment":             resourceTencentCloudDlcBindWorkGroupsToUserAttachment(),
 			"tencentcloud_dlc_user_data_engine_config":                         resourceTencentCloudDlcUserDataEngineConfig(),
+			"tencentcloud_dlc_update_row_filter":                               resourceTencentCloudDlcUpdateRowFilter(),
 			"tencentcloud_wedata_rule_template":                                resourceTencentCloudWedataRuleTemplate(),
 			"tencentcloud_waf_custom_rule":                                     resourceTencentCloudWafCustomRule(),
 			"tencentcloud_waf_custom_white_rule":                               resourceTencentCloudWafCustomWhiteRule(),
