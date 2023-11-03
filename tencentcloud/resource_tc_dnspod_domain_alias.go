ada4595c32522ff52ff769ed46cb76b97d50ee9c
andrewjiang <andrewjiang@tencent.com>
2023-11-03 08:31:05 +0800

    feat(dnspod): domain alias

--- tencentcloud/resource_tc_dnspod_domain_alias.go
+++ tencentcloud/resource_tc_dnspod_domain_alias.go
@@ -24,12 +24,13 @@ package tencentcloud
 import (
 	"context"
 	"fmt"
+	"log"
+	"strings"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	dnspod "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/dnspod/v20210323"
 	"github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/internal/helper"
-	"log"
-	"strings"
 )
 
 func resourceTencentCloudDnspodDomainAlias() *schema.Resource {
