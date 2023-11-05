a8c30441fd17cce8f8c36b576a074f85ac298feb
andrewjiang <andrewjiang@tencent.com>
2023-11-05 17:45:08 +0800

    feat(dnspod): dnspod operation

--- tencentcloud/resource_tc_dnspod_modify_record_group_operation_test.go
+++ tencentcloud/resource_tc_dnspod_modify_record_group_operation_test.go
@@ -16,9 +16,9 @@ func TestAccTencentCloudDnspodModifyRecordGroupOperationResource_basic(t *testin
 				Config: testAccDnspodModifyRecordGroupOperation,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("tencentcloud_dnspod_modify_record_group_operation.modify_record_group", "domain", "iac-tf.cloud"),
-					resource.TestCheckResourceAttr("tencentcloud_dnspod_modify_record_group_operation.modify_record_group", "group_id", 1),
+					resource.TestCheckResourceAttr("tencentcloud_dnspod_modify_record_group_operation.modify_record_group", "group_id", "1"),
 					resource.TestCheckResourceAttr("tencentcloud_dnspod_modify_record_group_operation.modify_record_group", "record_id", "234"),
-					resource.TestCheckResourceAttr("tencentcloud_dnspod_modify_record_group_operation.modify_record_group", "domain_id", 123),
+					resource.TestCheckResourceAttr("tencentcloud_dnspod_modify_record_group_operation.modify_record_group", "domain_id", "123"),
 				),
 			},
 		},
@@ -29,9 +29,9 @@ const testAccDnspodModifyRecordGroupOperation = `
 
 resource "tencentcloud_dnspod_modify_record_group_operation" "modify_record_group" {
   domain = "dnspod.cn"
-  group_id = 1
+  group_id = "1"
   record_id = "234|345"
-  domain_id = 123
+  domain_id = "123"
 }
 
 `
