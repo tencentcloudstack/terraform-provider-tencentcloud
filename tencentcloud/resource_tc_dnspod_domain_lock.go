a1ddc0c55c487a887e55dbbd22578e58448128fe
andrewjiang <andrewjiang@tencent.com>
2023-11-17 21:30:33 +0800

    feat(dnspod): domain lock and unlock

--- tencentcloud/resource_tc_dnspod_domain_lock.go
+++ tencentcloud/resource_tc_dnspod_domain_lock.go
@@ -94,7 +94,10 @@ func resourceTencentCloudDnspodDomainLockCreate(d *schema.ResourceData, meta int
 	}
 
 	d.SetId(strings.Join([]string{domain, lockCode}, FILED_SP))
-	d.Set("lock_code", lockCode)
+	if err = d.Set("lock_code", lockCode); err != nil {
+		log.Printf("[CRITAL]%s provider set lock_code fail, reason:%s", logId, err.Error())
+		return err
+	}
 
 	return resourceTencentCloudDnspodDomainLockRead(d, meta)
 }
