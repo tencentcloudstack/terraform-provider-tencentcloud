eef4205df74f5a01f143bc91184585727a048e8d
WeiMengXS <nickcchen@tencent.com>
2023-11-20 18:08:08 +0800

    feat: cdb testing

---
+++ tencentcloud/resource_tc_cbs_storage_testing_test.go
@@ -0,0 +1,43 @@
+package tencentcloud
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"testing"
+)
+
+func TestAccTencentCloudTestingCbsStorageResource_basic(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCbsStorageDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccTestingCbsStorage_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckStorageExists("tencentcloud_cbs_storage.storage_basic"),
+					resource.TestCheckResourceAttr("tencentcloud_cbs_storage.storage_basic", "storage_name", "tf-storage-basic"),
+					resource.TestCheckResourceAttr("tencentcloud_cbs_storage.storage_basic", "storage_type", "CLOUD_PREMIUM"),
+					resource.TestCheckResourceAttr("tencentcloud_cbs_storage.storage_basic", "storage_size", "50"),
+					resource.TestCheckResourceAttr("tencentcloud_cbs_storage.storage_basic", "availability_zone", "ap-guangzhou-3"),
+				),
+			},
+			{
+				ResourceName:            "tencentcloud_cbs_storage.storage_basic",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"force_delete"},
+			},
+		},
+	})
+}
+
+const testAccTestingCbsStorage_basic = `
+resource "tencentcloud_cbs_storage" "storage_basic" {
+	storage_type      = "CLOUD_PREMIUM"
+	storage_name      = "tf-storage-basic"
+	storage_size      = 50
+	availability_zone = "ap-guangzhou-3"
+}
+`
