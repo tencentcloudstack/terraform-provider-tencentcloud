a0884fbbbdc5679cd0484b465bee4ce25b54d156
SevenEarth <391613297@qq.com>
2023-11-22 22:29:44 +0800

    feat/waf

--- tencentcloud/resource_tc_waf_cc_session_test.go
+++ tencentcloud/resource_tc_waf_cc_session_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
+// go test -i; go test -test.run TestAccTencentCloudWafCcSessionResource_basic -v
 func TestAccTencentCloudWafCcSessionResource_basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
