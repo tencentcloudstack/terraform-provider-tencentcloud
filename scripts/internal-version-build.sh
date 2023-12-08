0ffdbaf4dd445663dfef7d96a4420d9efe075add
Wmxs <54929266+WeiMengXS@users.noreply.github.com>
2023-12-08 19:24:59 +0800

    feat: make file

--- scripts/internal-version-build.sh
+++ scripts/internal-version-build.sh
@@ -1,4 +1,4 @@
-python ./scripts/yunti-process-code.py
+python ./scripts/internal-version-process.py
 go mod vendor
 yq eval 'keys' ./scripts/yunti-code.yaml | awk '{print $2}' |grep -v 'go.mod'| xargs -I {} goimports -w {}
 yq eval 'keys' ./scripts/yunti-code.yaml | awk '{print $2}' |grep -v 'go.mod'| xargs -I {} go fmt {}
