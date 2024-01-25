dc950eedc6819bac97a66a896d023cf45df1e889
Wmxs <54929266+WeiMengXS@users.noreply.github.com>
2024-01-25 20:10:37 +0800

    fix: filter nil

--- tencentcloud/services/ssl/resource_tc_ssl_certificate_test.go
+++ tencentcloud/services/ssl/resource_tc_ssl_certificate_test.go
@@ -282,28 +282,28 @@ resource "tencentcloud_ssl_certificate" "test-ssl-certificate-tag" {
 	return fmt.Sprintf(str, testAccSslCertificateCA, tags)
 }
 
-const testAccSslCertificateCA = "-----BEGIN CERTIFICATE-----\\nMIIERzCCAq+gAwIBAgIBAjANBgkqhkiG9w0BAQsF" +
-	"ADAoMQ0wCwYDVQQDEwR0ZXN0\\nMRcwFQYDVQQKEw50ZXJyYWZvcm0gdGVzdDAeFw0xOTA4MTM" +
-	"wMzE5MzlaFw0yOTA4\\nMTAwMzE5MzlaMC4xEzARBgNVBAMTCnNlcnZlciBzc2wxFzAVBgNVBA" +
-	"oTDnRlcnJh\\nZm9ybS10ZXN0MIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEA1Ryp+" +
-	"DKK\\nSNFKZsPtwfR+jzOnQ8YFieIKYgakV688d8YgpolenbmeEPrzT87tunFD7G9f6ALG\\nND" +
-	"8rj7npj0AowxhOL/h/v1D9u0UsIaj5i2GWJrqNAhGLaxWiEB/hy5WOiwxDrGei\\ngQqJkFM52" +
-	"Ep7G1Yx7PHJmKFGwN9FhIsFi1cNZfVRopZuCe/RMPNusNVZaIi+qcEf\\nfsE1cmfmuSlG3Ap0" +
-	"RKOIyR0ajDEzqZn9/0R7VwWCF97qy8TNYk94K/1tq3zyhVzR\\nZ83xOSfrTqEfb3so3AU2jyK" +
-	"gYdwr/FZS72VCHS8IslgnqJW4izIXZqgIKmHaRZtM\\nN4jUloi6l/6lktt6Lsgh9xECecxziS" +
-	"JtPMaog88aC8HnMqJJ3kScGCL36GYG+Kaw\\n5PnDlWXBaeiDe8z/eWK9+Rr2M+rhTNxosAVGf" +
-	"DJyxAXyiX49LQ0v7f9qzwc/0JiD\\nbvsUv1cm6OgpoEMP9SXqqBdwGqeKbD2/2jlP48xlYP6l" +
-	"1SoJG3GgZ8dbAgMBAAGj\\ndjB0MAwGA1UdEwEB/wQCMAAwEwYDVR0lBAwwCgYIKwYBBQUHAwE" +
-	"wDwYDVR0PAQH/\\nBAUDAweAADAdBgNVHQ4EFgQULwWKBQNLL9s3cb3tTnyPVg+mpCMwHwYDVR" +
-	"0jBBgw\\nFoAUKwfrmq791mY831S6UHARHtgYnlgwDQYJKoZIhvcNAQELBQADggGBAMo5RglS\\nA" +
-	"HdPgaicWJvmvjjexjF/42b7Rz4pPfMjYw6uYO8He/f4UZWv5CZLrbEe7MywaK3y\\n0OsfH8Ah" +
-	"yN29pv2x8g9wbmq7omZIOZ0oCAGduEXs/A/qY/hFaCohdkz/IN8qi6JW\\nVXreGli3SrpcHFc" +
-	"hSwHTyJEXgkutcGAsOvdsOuVSmplOyrkLHc8uUe8SG4j8kGyg\\nEzaszFjHkR7g1dVyDVUedc" +
-	"588mjkQxYeAamJgfkgIhljWKMa2XzkVMcVfQHfNpM1\\nn+bu8SmqRt9Wma2bMijKRG/Blm756" +
-	"LoI+skY+WRZmlDnq8zj95TT0vceGP0FUWh5\\nhKyiocABmpQs9OK9HMi8vgSWISP+fYgkm/bK" +
-	"tKup2NbZBoO5/VL2vCEPInYzUhBO\\njCbLMjNjtM5KriCaR7wDARgHiG0gBEPOEW1PIjZ9UOH" +
-	"+LtIxbNZ4eEIIINLHnBHf\\nL+doVeZtS/gJc4G4Adr5HYuaS9ZxJ0W2uy0eQlOHzjyxR6Mf/r" +
-	"pnilJlcQ==\\n-----END CERTIFICATE-----"
+const testAccSslCertificateCA = "-----BEGIN CERTIFICATE-----\\n" +
+	"MIIDzjCCAragAwIBAgIUN9h/fEqxX/xxyLcAVUT67S/zN3kwDQYJKoZIhvcNAQEL\\n" +
+	"BQAweDELMAkGA1UEBhMCQ04xCzAJBgNVBAgMAlNYMQ0wCwYDVQQHDARYSUFOMQsw\\n" +
+	"CQYDVQQKDAJUQzELMAkGA1UECwwCTU0xETAPBgNVBAMMCHRlc3QtaWFjMSAwHgYJ\\n" +
+	"KoZIhvcNAQkBFhExMjU0Njg4NTU5QHFxLmNvbTAeFw0yMzA4MjkwODA0NTNaFw0z\\n" +
+	"MzA4MjYwODA0NTNaMHgxCzAJBgNVBAYTAkNOMQswCQYDVQQIDAJTWDENMAsGA1UE\\n" +
+	"BwwEWElBTjELMAkGA1UECgwCVEMxCzAJBgNVBAsMAk1NMREwDwYDVQQDDAh0ZXN0\\n" +
+	"LWlhYzEgMB4GCSqGSIb3DQEJARYRMTI1NDY4ODU1OUBxcS5jb20wggEiMA0GCSqG\\n" +
+	"SIb3DQEBAQUAA4IBDwAwggEKAoIBAQDbXEnXjfHZUtyTlF2BDZxfuxkJ3CMIBbrT\\n" +
+	"Tp2YXoj/jMKRCUgAg5AGD5/uuJ9yF9HMyUr0VCDrf7JJLxk5hWbpbUahATjotGd7\\n" +
+	"6z/5e9IM+IPTP//wa/7I3tL3fTq9nj1cmVxpmiC5wGwFSAssxkLVQDiQVKHW+6Oi\\n" +
+	"Qde3ENHQj+IwMq4bohAThKxCXqeDqtfAzJLPfMmLG6HTEwh10isbS1BDhXeYSoGN\\n" +
+	"HtkzTg07B0p76DnQEztJjKNY1/zVFwDSHpIbdsRAKsEsHEV0hzrjrBSCSwqfbuuG\\n" +
+	"LzWEVKWvmh+R6h2xhw9BBws7kYcw3cXUN+PiMB9aBSnRupFyASNpAgMBAAGjUDBO\\n" +
+	"MB0GA1UdDgQWBBR+qkSE6nATEINfR3xEZ9QKQfZZ+DAfBgNVHSMEGDAWgBR+qkSE\\n" +
+	"6nATEINfR3xEZ9QKQfZZ+DAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IB\\n" +
+	"AQAbgRo+4gcTGfXaXLv5KgkhPOyzpyaoAbfWtk8PkAlZb+8j6bxb4qEsaVwXrH8U\\n" +
+	"rRIe26YsoRCP3dYvJITaOO8KT0VZnO/KZeIdoHnguhjcynx60zt4hLQ+83N/JMJr\\n" +
+	"lLX+JIg2nhwnS97aXdZL6sdcraNKXD63Gyjhp4VMeSDi9juIYfheB6STbM/os0Aj\\n" +
+	"Xn12QxOg/nbAJah3aqYlZ38Js6lJ2vkd9AYptdPOw8SyeavrSiQg14iihLydh0mN\\n" +
+	"GfiXelsRMOgB46B/Xvk/ir3wes36rkrN+ixUiVcLYDME77O3Z4Mre2PzueOIWNzR\\n" +
+	"Axxb5inrStx6colrfzQThuwF\\n-----END CERTIFICATE-----"
 
 const testAccSslCertificateKey = "Public Key Info:\\n	Public Key Algorithm: RSA\\n	Key Security Level:" +
 	" High (3072 bits)\\nmodulus:\\n	00:d5:1c:a9:f8:32:8a:48:d1:4a:66:c3:ed:" +
