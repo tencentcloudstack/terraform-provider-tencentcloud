0783d099c9e925c49cd8e7ee13b65c842cfb9abe
SevenEarth <45937856+SevenEarth@users.noreply.github.com>
2024-04-29 20:56:39 +0800

    fix(cls): [117200212] cls index support dynamic_index params (#2606)
    * add
    
    * add
    
    * add

--- tencentcloud/services/cls/resource_tc_cls_inex_test.go
+++ tencentcloud/services/cls/resource_tc_cls_inex_test.go
@@ -8,6 +8,7 @@ import (
 	tcacctest "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/acctest"
 )
 
+// go test -i; go test -test.run TestAccTencentCloudClsIndex_basic -v
 func TestAccTencentCloudClsIndex_basic(t *testing.T) {
 	t.Parallel()
 
@@ -18,46 +19,58 @@ func TestAccTencentCloudClsIndex_basic(t *testing.T) {
 			{
 				Config: testAccClsIndex,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("tencentcloud_cls_index.index", "status", "true"),
+					resource.TestCheckResourceAttrSet("tencentcloud_cls_index.example", "topic_id"),
+					resource.TestCheckResourceAttr("tencentcloud_cls_index.example", "status", "true"),
+					resource.TestCheckResourceAttr("tencentcloud_cls_index.example", "include_internal_fields", "true"),
 				),
 			},
 			{
-				ResourceName:      "tencentcloud_cls_index.index",
+				ResourceName:      "tencentcloud_cls_index.example",
 				ImportState:       true,
 				ImportStateVerify: true,
+			},
+			{
+				Config: testAccClsIndexUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("tencentcloud_cls_index.example", "topic_id"),
+					resource.TestCheckResourceAttr("tencentcloud_cls_index.example", "status", "true"),
+					resource.TestCheckResourceAttr("tencentcloud_cls_index.example", "include_internal_fields", "true"),
+				),
 			},
 		},
 	})
 }
 
 const testAccClsIndex = `
-resource "tencentcloud_cls_logset" "logset" {
-  logset_name = "tf-topic-index"
+resource "tencentcloud_cls_logset" "example" {
+  logset_name = "tf_example"
   tags        = {
-    "test" = "test"
+    "demo" = "test"
   }
 }
 
-resource "tencentcloud_cls_topic" "topic" {
-  auto_split           = true
-  logset_id            = tencentcloud_cls_logset.logset.id
+resource "tencentcloud_cls_topic" "example" {
+  topic_name           = "tf_example"
+  logset_id            = tencentcloud_cls_logset.example.id
+  auto_split           = false
   max_split_partitions = 20
   partition_count      = 1
-  period               = 10
+  period               = 30
   storage_type         = "hot"
+  describes            = "Test Demo."
+  hot_period           = 10
   tags                 = {
-    "test" = "test"
+    "test" = "test",
   }
-  topic_name           = "tf-topic-index"
 }
 
-resource "tencentcloud_cls_index" "index" {
-  topic_id = tencentcloud_cls_topic.topic.id
+resource "tencentcloud_cls_index" "example" {
+  topic_id = tencentcloud_cls_topic.example.id
 
   rule {
     full_text {
       case_sensitive = true
-      tokenizer      = "@&?|#()='\",;:<>[]{}"
+      tokenizer      = "@&?|#()='\",;:<>[]{}/ \n\t\r\\"
       contain_z_h    = true
     }
 
@@ -68,7 +81,7 @@ resource "tencentcloud_cls_index" "index" {
         value {
           contain_z_h = true
           sql_flag    = true
-          tokenizer   = "@&?|#()='\",;:<>[]{}"
+          tokenizer   = "@&?|#()='\",;:<>[]{}/ \n\t\r\\"
           type        = "text"
         }
       }
@@ -78,7 +91,7 @@ resource "tencentcloud_cls_index" "index" {
         value {
           contain_z_h = true
           sql_flag    = true
-          tokenizer   = "@&?|#()='\",;:<>[]{}"
+          tokenizer   = "@&?|#()='\",;:<>[]{}/ \n\t\r\\"
           type        = "text"
         }
       }
@@ -91,15 +104,97 @@ resource "tencentcloud_cls_index" "index" {
         value {
           contain_z_h = true
           sql_flag    = true
-          tokenizer   = "@&?|#()='\",;:<>[]{}"
+          tokenizer   = "@&?|#()='\",;:<>[]{}/ \n\t\r\\"
           type        = "text"
         }
       }
+    }
+
+    dynamic_index {
+      status = true
     }
   }
   status                  = true
   include_internal_fields = true
   metadata_flag           = 1
 }
+`
 
+const testAccClsIndexUpdate = `
+resource "tencentcloud_cls_logset" "example" {
+  logset_name = "tf_example"
+  tags        = {
+    "demo" = "test"
+  }
+}
+
+resource "tencentcloud_cls_topic" "example" {
+  topic_name           = "tf_example"
+  logset_id            = tencentcloud_cls_logset.example.id
+  auto_split           = false
+  max_split_partitions = 20
+  partition_count      = 1
+  period               = 30
+  storage_type         = "hot"
+  describes            = "Test Demo."
+  hot_period           = 10
+  tags                 = {
+    "test" = "test",
+  }
+}
+
+resource "tencentcloud_cls_index" "example" {
+  topic_id = tencentcloud_cls_topic.example.id
+
+  rule {
+    full_text {
+      case_sensitive = true
+      tokenizer      = "@&?|#()='\",;:<>[]{}/ \n\t\r\\"
+      contain_z_h    = true
+    }
+
+    key_value {
+      case_sensitive = true
+      key_values {
+        key = "hello"
+        value {
+          contain_z_h = true
+          sql_flag    = true
+          tokenizer   = "@&?|#()='\",;:<>[]{}/ \n\t\r\\"
+          type        = "text"
+        }
+      }
+
+      key_values {
+        key = "world"
+        value {
+          contain_z_h = true
+          sql_flag    = true
+          tokenizer   = "@&?|#()='\",;:<>[]{}/ \n\t\r\\"
+          type        = "text"
+        }
+      }
+    }
+
+    tag {
+      case_sensitive = true
+      key_values {
+        key = "terraform"
+        value {
+          contain_z_h = true
+          sql_flag    = true
+          tokenizer   = "@&?|#()='\",;:<>[]{}/ \n\t\r\\"
+          type        = "text"
+        }
+      }
+    }
+
+    dynamic_index {
+      status = false
+    }
+  }
+  status                  = true
+  include_internal_fields = true
+  metadata_flag           = 1
+}
 `
