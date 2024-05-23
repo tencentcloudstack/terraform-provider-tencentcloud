cb5c8cc3c4671176477fa28d90342d1777d92d28
赵普磊 <33088480+rudolfzhao@users.noreply.github.com>
2024-05-23 16:55:40 +0800

    fix(gaap): [117510345] gaap test fix (#2649)
    * 修改资源ID
    
    * 修改测试用例资源
    
    * 修改测试用例资源
    
    * []修改测试用例资源信息
    
    * 修改DefaultHttpsDomainCertificateId
    
    * docs: update gaap docs
    
    * 更新资源ID
    
    ---------
    
    Co-authored-by: rudolfzhao <rudolfzhao@tencent.com>
    Co-authored-by: andrewjiang <andrewjiang@tencent.com>

--- tencentcloud/services/gaap/data_source_tc_gaap_proxy_groups_test.go
+++ tencentcloud/services/gaap/data_source_tc_gaap_proxy_groups_test.go
@@ -18,7 +18,7 @@ func TestAccTencentCloudGaapProxyGroupsDataSource_basic(t *testing.T) {
 				Config: testAccGaapProxyGroupsDataSource,
 				Check: resource.ComposeTestCheckFunc(
 					tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_gaap_proxy_groups.proxy_groups"),
-					resource.TestCheckResourceAttr("data.tencentcloud_gaap_proxy_groups.proxy_groups", "proxy_group_list.0.group_id", "lg-8h33uhwd"),
+					resource.TestCheckResourceAttr("data.tencentcloud_gaap_proxy_groups.proxy_groups", "proxy_group_list.0.group_id", "lg-2foz7wjh"),
 				),
 			},
 		},
@@ -30,7 +30,7 @@ data "tencentcloud_gaap_proxy_groups" "proxy_groups" {
 	project_id = 0
 	filters {
 		  name = "GroupId"
-		  values = ["lg-8h33uhwd"]
+		  values = ["lg-2foz7wjh"]
 	}
 }
 `
