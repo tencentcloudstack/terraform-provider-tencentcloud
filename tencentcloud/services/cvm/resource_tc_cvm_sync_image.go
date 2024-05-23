30bf90e2753099c9d76e771478b660ccb964f63b
SevenEarth <391613297@qq.com>
2024-05-23 17:26:32 +0800

    add

--- tencentcloud/services/cvm/resource_tc_cvm_sync_image.go
+++ tencentcloud/services/cvm/resource_tc_cvm_sync_image.go
@@ -1,15 +1,14 @@
 package cvm
 
 import (
+	"context"
 	"log"
-	"time"
-
-	tccommon "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/common"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	cvm "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/cvm/v20170312"
 
+	tccommon "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/common"
 	"github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/internal/helper"
 )
 
@@ -20,41 +19,41 @@ func ResourceTencentCloudCvmSyncImage() *schema.Resource {
 		Delete: resourceTencentCloudCvmSyncImageDelete,
 
 		Schema: map[string]*schema.Schema{
-			"image_id": {
+			"destination_regions": {
+				Type:        schema.TypeSet,
 				Required:    true,
 				ForceNew:    true,
-				Type:        schema.TypeString,
-				Description: "Image ID. The specified image must meet the following requirement: the images must be in the `NORMAL` state.",
-			},
-
-			"destination_regions": {
-				Required: true,
-				ForceNew: true,
-				Type:     schema.TypeSet,
+				Description: "List of destination regions for synchronization. Limits: It must be a valid region. For a custom image, the destination region cannot be the source region. For a shared image, the destination region must be the source region, which indicates to create a copy of the image as a custom image in the same region.",
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
-				Description: "List of destination regions for synchronization. Limits: It must be a valid region. For a custom image, the destination region cannot be the source region. For a shared image, the destination region must be the source region, which indicates to create a copy of the image as a custom image in the same region.",
 			},
 
 			"dry_run": {
+				Type:        schema.TypeBool,
 				Optional:    true,
 				ForceNew:    true,
-				Type:        schema.TypeBool,
 				Description: "Checks whether image synchronization can be initiated.",
 			},
 
+			"image_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Image ID. The specified image must meet the following requirement: the images must be in the `NORMAL` state.",
+			},
+
 			"image_name": {
+				Type:        schema.TypeString,
 				Optional:    true,
 				ForceNew:    true,
-				Type:        schema.TypeString,
 				Description: "Destination image name.",
 			},
 
 			"image_set_required": {
+				Type:        schema.TypeBool,
 				Optional:    true,
 				ForceNew:    true,
-				Type:        schema.TypeBool,
 				Description: "Whether to return the ID of image created in the destination region.",
 			},
 		},
@@ -66,19 +65,32 @@ func resourceTencentCloudCvmSyncImageCreate(d *schema.ResourceData, meta interfa
 	defer tccommon.InconsistentCheck(d, meta)()
 
 	logId := tccommon.GetLogId(tccommon.ContextNil)
-	request := cvm.NewSyncImagesRequest()
-	imageId := d.Get("image_id").(string)
-	request.ImageIds = []*string{&imageId}
+
+	ctx := tccommon.NewResourceLifeCycleHandleFuncContext(context.Background(), logId, d, meta)
+
+	var (
+		imageId string
+	)
+	var (
+		request  = cvm.NewSyncImagesRequest()
+		response = cvm.NewSyncImagesResponse()
+	)
+
+	if v, ok := d.GetOk("image_id"); ok {
+		imageId = v.(string)
+	}
+
+	request.ImageIds = []*string{helper.String(imageId)}
 
 	if v, ok := d.GetOk("destination_regions"); ok {
 		destinationRegionsSet := v.(*schema.Set).List()
 		for i := range destinationRegionsSet {
 			destinationRegions := destinationRegionsSet[i].(string)
-			request.DestinationRegions = append(request.DestinationRegions, &destinationRegions)
+			request.DestinationRegions = append(request.DestinationRegions, helper.String(destinationRegions))
 		}
 	}
 
-	if v, _ := d.GetOk("dry_run"); v != nil {
+	if v, ok := d.GetOkExists("dry_run"); ok {
 		request.DryRun = helper.Bool(v.(bool))
 	}
 
@@ -86,33 +98,32 @@ func resourceTencentCloudCvmSyncImageCreate(d *schema.ResourceData, meta interfa
 		request.ImageName = helper.String(v.(string))
 	}
 
-	if v, _ := d.GetOk("image_set_required"); v != nil {
+	if v, ok := d.GetOkExists("image_set_required"); ok {
 		request.ImageSetRequired = helper.Bool(v.(bool))
 	}
 
 	err := resource.Retry(tccommon.WriteRetryTimeout, func() *resource.RetryError {
-		result, e := meta.(tccommon.ProviderMeta).GetAPIV3Conn().UseCvmClient().SyncImages(request)
+		result, e := meta.(tccommon.ProviderMeta).GetAPIV3Conn().UseCvmClient().SyncImagesWithContext(ctx, request)
 		if e != nil {
 			return tccommon.RetryError(e)
 		} else {
 			log.Printf("[DEBUG]%s api[%s] success, request body [%s], response body [%s]\n", logId, request.GetAction(), request.ToJsonString(), result.ToJsonString())
 		}
+		response = result
 		return nil
 	})
 	if err != nil {
-		log.Printf("[CRITAL]%s operate cvm syncImages failed, reason:%+v", logId, err)
+		log.Printf("[CRITAL]%s create cvm sync image failed, reason:%+v", logId, err)
+		return err
+	}
+
+	_ = response
+
+	if err := resourceTencentCloudCvmSyncImageCreatePostHandleResponse0(ctx, response); err != nil {
 		return err
 	}
 
 	d.SetId(imageId)
-
-	service := CvmService{client: meta.(tccommon.ProviderMeta).GetAPIV3Conn()}
-
-	conf := tccommon.BuildStateChangeConf([]string{}, []string{"NORMAL"}, 20*tccommon.ReadRetryTimeout, time.Second, service.CvmSyncImagesStateRefreshFunc(d.Id(), []string{}))
-
-	if _, e := conf.WaitForState(); e != nil {
-		return e
-	}
 
 	return resourceTencentCloudCvmSyncImageRead(d, meta)
 }
