ecbcc2b4333dc09f33e54240734dba499ab7e380
SevenEarth <391613297@qq.com>
2024-05-30 16:54:57 +0800

    add

--- tencentcloud/services/trocket/resource_tc_tdmq_rocketmq_vip_instance.go
+++ tencentcloud/services/trocket/resource_tc_tdmq_rocketmq_vip_instance.go
@@ -293,6 +293,10 @@ func resourceTencentCloudTdmqRocketmqVipInstanceRead(d *schema.ResourceData, met
 		_ = d.Set("storage_size", storageSize)
 	}
 
+	if rocketmqVipInstances != nil {
+		_ = d.Set("spec", rocketmqVipInstances.SpecName)
+	}
+
 	return nil
 }
 
