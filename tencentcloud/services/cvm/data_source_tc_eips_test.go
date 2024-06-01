3e77bac4410758e9070d39c34b290f62439cd8a7
SevenEarth <45937856+SevenEarth@users.noreply.github.com>
2024-06-01 10:51:33 +0800

    fix(cvm): [117301871]  update cvm e2e test  (#2662)
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add

--- tencentcloud/services/cvm/data_source_tc_eips_test.go
+++ tencentcloud/services/cvm/data_source_tc_eips_test.go
@@ -8,7 +8,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccTencentCloudEipsDataSource(t *testing.T) {
+// go test -i; go test -test.run TestAccTencentCloudEipsDataSource_basic -v
+func TestAccTencentCloudEipsDataSource_basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { tcacctest.AccPreCheck(t) },
@@ -18,17 +19,23 @@ func TestAccTencentCloudEipsDataSource(t *testing.T) {
 			{
 				Config: testAccEipsDataSource,
 				Check: resource.ComposeTestCheckFunc(
-					tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.data_eips"),
-					resource.TestCheckResourceAttr("data.tencentcloud_eips.data_eips", "eip_list.#", "1"),
-					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.data_eips", "eip_list.0.eip_id"),
-					resource.TestCheckResourceAttr("data.tencentcloud_eips.data_eips", "eip_list.0.eip_name", "tf-test-eip"),
-					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.data_eips", "eip_list.0.eip_type"),
-					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.data_eips", "eip_list.0.status"),
-					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.data_eips", "eip_list.0.public_ip"),
-					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.data_eips", "eip_list.0.create_time"),
+					tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.example"),
+					resource.TestCheckResourceAttr("data.tencentcloud_eips.example", "eip_list.#", "0"),
 
-					tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.tags"),
-					resource.TestCheckResourceAttr("data.tencentcloud_eips.tags", "eip_list.0.tags.test", "test"),
+					tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.example_by_id"),
+					resource.TestCheckResourceAttr("data.tencentcloud_eips.example_by_id", "eip_list.#", "1"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.eip_id"),
+					resource.TestCheckResourceAttr("data.tencentcloud_eips.example_by_id", "eip_list.0.eip_name", "tf-example"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.eip_type"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.status"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.public_ip"),
+					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.create_time"),
+
+					tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.example_by_name"),
+					resource.TestCheckResourceAttr("data.tencentcloud_eips.example_by_name", "eip_list.0.eip_name", "tf-example"),
+
+					tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.example_by_tags"),
+					resource.TestCheckResourceAttr("data.tencentcloud_eips.example_by_tags", "eip_list.0.tags.test", "test"),
 				),
 			},
 		},
@@ -36,19 +43,25 @@ func TestAccTencentCloudEipsDataSource(t *testing.T) {
 }
 
 const testAccEipsDataSource = `
-resource "tencentcloud_eip" "eip" {
-  name = "tf-test-eip"
+resource "tencentcloud_eip" "example" {
+  name = "tf-example"
 
   tags = {
     "test" = "test"
   }
 }
 
-data "tencentcloud_eips" "data_eips" {
-  eip_id = tencentcloud_eip.eip.id
+data "tencentcloud_eips" "example" {}
+
+data "tencentcloud_eips" "example_by_id" {
+  eip_id = tencentcloud_eip.example.id
 }
 
-data "tencentcloud_eips" "tags" {
-  tags = tencentcloud_eip.eip.tags
+data "tencentcloud_eips" "example_by_name" {
+  eip_name = tencentcloud_eip.example.name
+}
+
+data "tencentcloud_eips" "example_by_tags" {
+  tags = tencentcloud_eip.example.tags
 }
 `
--- tencentcloud/services/cvm/resource_tc_cvm_security_group_attachment_test.go
+++ tencentcloud/services/cvm/resource_tc_cvm_security_group_attachment_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
+// go test -i; go test -test.run TestAccTencentCloudCvmSecurityGroupAttachmentResource_basic -v
 func TestAccTencentCloudCvmSecurityGroupAttachmentResource_basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
@@ -18,10 +19,14 @@ func TestAccTencentCloudCvmSecurityGroupAttachmentResource_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCvmSecurityGroupAttachment,
-				Check:  resource.ComposeTestCheckFunc(resource.TestCheckResourceAttrSet("tencentcloud_cvm_security_group_attachment.security_group_attachment", "id")),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("tencentcloud_cvm_security_group_attachment.example", "id"),
+					resource.TestCheckResourceAttrSet("tencentcloud_cvm_security_group_attachment.example", "instance_id"),
+					resource.TestCheckResourceAttrSet("tencentcloud_cvm_security_group_attachment.example", "security_group_id"),
+				),
 			},
 			{
-				ResourceName:      "tencentcloud_cvm_security_group_attachment.security_group_attachment",
+				ResourceName:      "tencentcloud_cvm_security_group_attachment.example",
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
@@ -30,8 +35,48 @@ func TestAccTencentCloudCvmSecurityGroupAttachmentResource_basic(t *testing.T) {
 }
 
 const testAccCvmSecurityGroupAttachment = `
-resource "tencentcloud_cvm_security_group_attachment" "security_group_attachment" {
-	security_group_id = "sg-a0212ii1"
-	instance_id = "ins-cr2rfq78"
+# create vpc
+resource "tencentcloud_vpc" "vpc" {
+  name       = "vpc"
+  cidr_block = "10.0.0.0/16"
+}
+
+# create vpc subnet
+resource "tencentcloud_subnet" "subnet" {
+  name              = "subnet"
+  vpc_id            = tencentcloud_vpc.vpc.id
+  availability_zone = "ap-guangzhou-6"
+  cidr_block        = "10.0.20.0/28"
+  is_multicast      = false
+}
+
+# create cvm
+resource "tencentcloud_instance" "example" {
+  instance_name     = "tf_example"
+  availability_zone = "ap-guangzhou-6"
+  image_id          = "img-9qrfy1xt"
+  instance_type     = "SA3.MEDIUM4"
+  system_disk_type  = "CLOUD_HSSD"
+  system_disk_size  = 100
+  hostname          = "example"
+  project_id        = 0
+  vpc_id            = tencentcloud_vpc.vpc.id
+  subnet_id         = tencentcloud_subnet.subnet.id
+
+  data_disks {
+    data_disk_type = "CLOUD_HSSD"
+    data_disk_size = 50
+    encrypt        = false
+  }
+
+  tags = {
+    tagKey = "tagValue"
+  }
+}
+
+# attachment security group
+resource "tencentcloud_cvm_security_group_attachment" "example" {
+  instance_id       = tencentcloud_instance.example.id
+  security_group_id = "sg-5275dorp"
 }
 `
