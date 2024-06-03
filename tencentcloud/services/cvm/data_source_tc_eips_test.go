44e8c5ca9c73d85620d0995685cc17cda576a5a7
mikatong <mikatong@tencent.com>
2024-06-03 21:53:57 +0800

    add cvm datasource test

--- tencentcloud/services/cvm/data_source_tc_eips_test.go
+++ tencentcloud/services/cvm/data_source_tc_eips_test.go
@@ -3,65 +3,123 @@ package cvm_test
 import (
 	"testing"
 
-	tcacctest "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/acctest"
+	acctest "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/acctest"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-// go test -i; go test -test.run TestAccTencentCloudEipsDataSource_basic -v
-func TestAccTencentCloudEipsDataSource_basic(t *testing.T) {
+func TestAccTencentCloudCvmEipsDataSource_Basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { tcacctest.AccPreCheck(t) },
-		Providers:    tcacctest.AccProviders,
-		CheckDestroy: testAccCheckEipDestroy,
+		PreCheck: func() {
+			acctest.AccPreCheck(t)
+		},
+		Providers: acctest.AccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccEipsDataSource,
-				Check: resource.ComposeTestCheckFunc(
-					tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.example"),
-					resource.TestCheckResourceAttr("data.tencentcloud_eips.example", "eip_list.#", "0"),
-
-					tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.example_by_id"),
-					resource.TestCheckResourceAttr("data.tencentcloud_eips.example_by_id", "eip_list.#", "1"),
-					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.eip_id"),
-					resource.TestCheckResourceAttr("data.tencentcloud_eips.example_by_id", "eip_list.0.eip_name", "tf-example"),
-					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.eip_type"),
-					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.status"),
-					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.public_ip"),
-					resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.create_time"),
-
-					tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.example_by_name"),
-					resource.TestCheckResourceAttr("data.tencentcloud_eips.example_by_name", "eip_list.0.eip_name", "tf-example"),
-
-					tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.example_by_tags"),
-					resource.TestCheckResourceAttr("data.tencentcloud_eips.example_by_tags", "eip_list.0.tags.test", "test"),
-				),
+				Config: testAccCvmEipsDataSource_BasicCreate,
+				Check:  resource.ComposeTestCheckFunc(acctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.example")),
 			},
 		},
 	})
 }
 
-const testAccEipsDataSource = `
-resource "tencentcloud_eip" "example" {
-  name = "tf-example"
+const testAccCvmEipsDataSource_BasicCreate = `
 
-  tags = {
-    "test" = "test"
-  }
+data "tencentcloud_eips" "example" {
 }
 
-data "tencentcloud_eips" "example" {}
+`
+
+func TestAccTencentCloudCvmEipsDataSource_ById(t *testing.T) {
+	t.Parallel()
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.AccPreCheck(t)
+		},
+		Providers: acctest.AccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCvmEipsDataSource_ByIdCreate,
+				Check:  resource.ComposeTestCheckFunc(acctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.example_by_id"), resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.public_ip"), resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.create_time"), resource.TestCheckResourceAttr("data.tencentcloud_eips.example_by_id", "eip_list.#", "1"), resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.eip_id"), resource.TestCheckResourceAttr("data.tencentcloud_eips.example_by_id", "eip_list.0.eip_name", "tf-example"), resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.eip_type"), resource.TestCheckResourceAttrSet("data.tencentcloud_eips.example_by_id", "eip_list.0.status")),
+			},
+		},
+	})
+}
+
+const testAccCvmEipsDataSource_ByIdCreate = `
 
 data "tencentcloud_eips" "example_by_id" {
-  eip_id = tencentcloud_eip.example.id
+    eip_id = tencentcloud_eip.example.id
 }
+resource "tencentcloud_eip" "example" {
+    
+    tags = {
+        test = "test"
+    }
+    name = "tf-example"
+}
+
+`
+
+func TestAccTencentCloudCvmEipsDataSource_ByName(t *testing.T) {
+	t.Parallel()
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.AccPreCheck(t)
+		},
+		Providers: acctest.AccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCvmEipsDataSource_ByNameCreate,
+				Check:  resource.ComposeTestCheckFunc(acctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.example_by_name"), resource.TestCheckResourceAttr("data.tencentcloud_eips.example_by_name", "eip_list.0.eip_name", "tf-example")),
+			},
+		},
+	})
+}
+
+const testAccCvmEipsDataSource_ByNameCreate = `
 
 data "tencentcloud_eips" "example_by_name" {
-  eip_name = tencentcloud_eip.example.name
+    eip_name = tencentcloud_eip.example.name
+}
+resource "tencentcloud_eip" "example" {
+    name = "tf-example"
+    
+    tags = {
+        test = "test"
+    }
 }
 
-data "tencentcloud_eips" "example_by_tags" {
-  tags = tencentcloud_eip.example.tags
+`
+
+func TestAccTencentCloudCvmEipsDataSource_ByTags(t *testing.T) {
+	t.Parallel()
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.AccPreCheck(t)
+		},
+		Providers: acctest.AccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCvmEipsDataSource_ByTagsCreate,
+				Check:  resource.ComposeTestCheckFunc(acctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_eips.example_by_tags"), resource.TestCheckResourceAttr("data.tencentcloud_eips.example_by_tags", "eip_list.0.tags.test", "test")),
+			},
+		},
+	})
 }
+
+const testAccCvmEipsDataSource_ByTagsCreate = `
+
+data "tencentcloud_eips" "example_by_tags" {
+    tags = tencentcloud_eip.example.tags
+}
+resource "tencentcloud_eip" "example" {
+    name = "tf-example"
+    
+    tags = {
+        test = "test"
+    }
+}
+
 `
