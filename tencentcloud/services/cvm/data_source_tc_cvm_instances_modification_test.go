3f155ba66fa65c7cb4fc95655edaf5af25f79c9b
Yanghao LUO <yhlooo.x@outlook.com>
2024-06-11 17:01:42 +0800

    test: modify tencentcloud_cvm_instances_modification test

--- tencentcloud/services/cvm/data_source_tc_cvm_instances_modification_test.go
+++ tencentcloud/services/cvm/data_source_tc_cvm_instances_modification_test.go
@@ -27,8 +27,34 @@ func TestAccTencentCloudCvmInstancesModificationDataSource_basic(t *testing.T) {
 	})
 }
 
-const testAccCvmInstancesModificationDataSource = tcacctest.DefaultCvmModificationVariable + `
+const testAccCvmInstancesModificationDataSource = `
+data "tencentcloud_images" "default" {
+    image_type = ["PUBLIC_IMAGE"]
+    image_name_regex = "Final"
+}
+data "tencentcloud_instance_types" "default" {
+    memory_size = 2
+    exclude_sold_out = true
+    filter {
+        name = "instance-family"
+        values = ["SA2","SA3","SA4","SA5","S2","S3"]
+    }
+    filter {
+        name = "zone"
+        values = ["ap-guangzhou-7"]
+    }
+    cpu_core_count = 2
+}
+
+resource "tencentcloud_instance" "test_cvm" {
+	image_id = data.tencentcloud_images.default.images.0.image_id
+	availability_zone = "ap-guangzhou-7"
+	instance_type = data.tencentcloud_instance_types.default.instance_types.0.instance_type
+    orderly_security_groups = ["sg-5275dorp"]
+	instance_charge_type = "POSTPAID_BY_HOUR"
+}
+
 data "tencentcloud_cvm_instances_modification" "foo" {
-	instance_ids = [var.cvm_id]
+	instance_ids = [tencentcloud_instance.test_cvm.id]
 }
 `
