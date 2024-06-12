273d9f1f029af66a67c3beba68a251c857338c9a
Yanghao LUO <yhlooo.x@outlook.com>
2024-06-12 14:01:55 +0800

    test: modify tencentcloud_cvm_launch_template_version test

--- tencentcloud/services/cvm/resource_tc_cvm_launch_template_version_test.go
+++ tencentcloud/services/cvm/resource_tc_cvm_launch_template_version_test.go
@@ -18,28 +18,95 @@ func TestAccTencentCloudCvmLaunchTemplateVersionResource_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCvmLaunchTemplateVersion,
-				Check:  resource.ComposeTestCheckFunc(resource.TestCheckResourceAttrSet("tencentcloud_cvm_launch_template_version.launch_template_version", "id")),
-			},
-			{
-				ResourceName:      "tencentcloud_cvm_launch_template_version.launch_template_version",
-				ImportState:       true,
-				ImportStateVerify: true,
+				Check:  resource.ComposeTestCheckFunc(resource.TestCheckResourceAttrSet("tencentcloud_cvm_launch_template_version.test_launch_tpl_v2", "id")),
 			},
 		},
 	})
 }
 
 const testAccCvmLaunchTemplateVersion = `
-resource "tencentcloud_cvm_launch_template_version" "launch_template_version" {
-	placement {
-		  zone = "ap-guangzhou-6"
-		  project_id = 0
-  
-	}
-	launch_template_id = "lt-9e1znnsa"
-	launch_template_version_description = "version description"
-	disable_api_termination = false
-	instance_type = "S5.MEDIUM4"
-	image_id = "img-9qrfy1xt"
+data "tencentcloud_images" "default" {
+  image_type = ["PUBLIC_IMAGE"]
+  image_name_regex = "Final"
+}
+data "tencentcloud_vpc_instances" "vpc" {
+  is_default = true
+}
+data "tencentcloud_vpc_subnets" "subnets" {
+  availability_zone = "ap-guangzhou-7"
+  vpc_id = data.tencentcloud_vpc_instances.vpc.instance_list.0.vpc_id
+}
+
+resource "tencentcloud_cvm_launch_template" "test_launch_tpl" {
+  launch_template_name = "test"
+  image_id             = data.tencentcloud_images.default.images.0.image_id
+  placement {
+    zone = "ap-guangzhou-7"
+  }
+  instance_name = "v1"
+}
+
+resource "tencentcloud_cvm_launch_template_version" "test_launch_tpl_v2" {
+  launch_template_id = tencentcloud_cvm_launch_template.test_launch_tpl.id
+  placement {
+    zone = "ap-guangzhou-7"
+    project_id = 0
+  }
+  launch_template_version_description = "test"
+  instance_type = "S5.MEDIUM2"
+  image_id             = data.tencentcloud_images.default.images.0.image_id
+#   system_disk {
+#     disk_type = "CLOUD_PREMIUM"
+#     disk_size = 20
+#   }
+#   data_disks {
+#     disk_type = "CLOUD_PREMIUM"
+#     disk_size = 200
+#   }
+  virtual_private_cloud {
+    vpc_id    = data.tencentcloud_vpc_subnets.subnets.instance_list.0.vpc_id
+    subnet_id = data.tencentcloud_vpc_subnets.subnets.instance_list.0.subnet_id
+  }
+#   internet_accessible {
+#     internet_charge_type = "TRAFFIC_POSTPAID_BY_HOUR"
+#     internet_max_bandwidth_out = 20
+#   }
+  instance_count = 2
+  instance_name = "v2"
+  security_group_ids = ["sg-5275dorp","sg-cm7fbbf3"]
+  enhanced_service {
+    security_service {
+      enabled = true
+    }
+    monitor_service {
+      enabled = true
+    }
+    automation_service {
+      enabled = false
+    }
+  }
+  client_token = "123"
+  host_name = "test"
+  disaster_recover_group_ids = []
+  tag_specification {
+    resource_type = "instance"
+    tags {
+      key = "key"
+      value = "value2"
+    }
+    tags {
+      key = "key2"
+      value = "value1"
+    }
+  }
+  user_data = "aGhoCg=="
+  cam_role_name = ""
+  hpc_cluster_id = ""
+  instance_charge_type = "PREPAID"
+  instance_charge_prepaid {
+    period = 3
+    renew_flag = "DISABLE_NOTIFY_AND_MANUAL_RENEW"
+  }
+  disable_api_termination = true
 }
 `
