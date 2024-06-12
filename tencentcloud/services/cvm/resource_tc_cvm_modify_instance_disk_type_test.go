c4543be892e4e0cd56b07474ded8c0ed603d9b69
Yanghao LUO <yhlooo.x@outlook.com>
2024-06-12 15:56:23 +0800

    test: modify tencentcloud_cvm_modify_instance_disk_type test

--- tencentcloud/services/cvm/resource_tc_cvm_modify_instance_disk_type_test.go
+++ tencentcloud/services/cvm/resource_tc_cvm_modify_instance_disk_type_test.go
@@ -18,39 +18,55 @@ func TestAccTencentCloudNeedFixCvmModifyInstanceDiskTypeResource_basic(t *testin
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCvmModifyInstanceDiskType,
-				Check:  resource.ComposeTestCheckFunc(resource.TestCheckResourceAttrSet("tencentcloud_cvm_modify_instance_disk_type.modify_instance_disk_type", "id")),
-			},
-			{
-				ResourceName:      "tencentcloud_cvm_modify_instance_disk_type.modify_instance_disk_type",
-				ImportState:       true,
-				ImportStateVerify: true,
+				Check:  resource.ComposeTestCheckFunc(resource.TestCheckResourceAttrSet("tencentcloud_cvm_modify_instance_disk_type.modify_disk_type", "id")),
 			},
 		},
 	})
 }
 
 const testAccCvmModifyInstanceDiskType = `
-
-resource "tencentcloud_cvm_modify_instance_disk_type" "modify_instance_disk_type" {
-  instance_id = "ins-r8hr2upy"
-  data_disks {
-		disk_size = 50
-		disk_type = "CLOUD_BASIC"
-		disk_id = "disk-hrsd0u81"
-		delete_with_instance = true
-		snapshot_id = "snap-r9unnd89"
-		encrypt = false
-		kms_key_id = "kms-abcd1234"
-		throughput_performance = 2
-		cdc_id = "cdc-b9pbd3px"
-
+data "tencentcloud_images" "default" {
+  image_type = ["PUBLIC_IMAGE"]
+  image_name_regex = "Final"
+}
+data "tencentcloud_instance_types" "default" {
+  memory_size = 2
+  exclude_sold_out = true
+  filter {
+    name = "instance-family"
+    values = ["SA2","SA3","SA4","SA5","S2","S3"]
   }
-  system_disk {
-		disk_type = "CLOUD_PREMIUM"
-		disk_id = "disk-1drr53sd"
-		disk_size = 50
-		cdc_id = "cdc-b9pbd3px"
+  filter {
+    name = "zone"
+    values = ["ap-guangzhou-7"]
+  }
+  cpu_core_count = 2
+}
 
+resource "tencentcloud_instance" "test_cvm" {
+  image_id = data.tencentcloud_images.default.images.0.image_id
+  availability_zone = "ap-guangzhou-7"
+  instance_type = "SA2.MEDIUM2"
+  orderly_security_groups = ["sg-5275dorp"]
+  instance_charge_type = "POSTPAID_BY_HOUR"
+  data_disks {
+    data_disk_size = 50
+    data_disk_type = "CLOUD_PREMIUM"
+  }
+  running_flag = false
+}
+
+resource "tencentcloud_cvm_modify_instance_disk_type" "modify_disk_type" {
+  instance_id = tencentcloud_instance.test_cvm.id
+  system_disk {
+    disk_type = "CLOUD_SSD"
+    disk_size = 50
+  }
+  data_disks {
+    disk_type = "CLOUD_SSD"
+    disk_size = 50
+    delete_with_instance = true
+    throughput_performance = 100
   }
 }
 
