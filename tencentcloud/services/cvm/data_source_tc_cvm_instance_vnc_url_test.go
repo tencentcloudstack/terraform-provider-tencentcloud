9f04e72d13e14d4edaf420abc25a9fc36bbb9154
Yanghao LUO <yhlooo.x@outlook.com>
2024-06-18 08:59:18 +0800

    test(cvm): [117971511] add some cvm acctest (#2679)
    * test: modify tencentcloud_cvm_instances_modification test
    
    * test: modify tencentcloud_cvm_instance_vnc_url test
    
    * test: modify tencentcloud_eip_address_quota test
    
    * test: modify tencentcloud_eip_network_account_type test
    
    * test: modify tencentcloud_cvm_launch_template_version test
    
    * test: modify tencentcloud_cvm_modify_instance_disk_type test
    
    * test: modify tencentcloud_cvm_reboot_instance test

--- tencentcloud/services/cvm/data_source_tc_cvm_instance_vnc_url_test.go
+++ tencentcloud/services/cvm/data_source_tc_cvm_instance_vnc_url_test.go
@@ -1,6 +1,7 @@
 package cvm_test
 
 import (
+	"fmt"
 	"testing"
 
 	tcacctest "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/acctest"
@@ -18,15 +19,48 @@ func TestAccTencentCloudCvmInstanceVncUrlDataSource_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCvmInstanceVncUrlDataSource,
-				Check:  resource.ComposeTestCheckFunc(tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_cvm_instance_vnc_url.instance_vnc_url")),
+				Check: resource.ComposeTestCheckFunc(
+					tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_cvm_instance_vnc_url.instance_vnc_url"),
+					resource.TestCheckResourceAttrWith("data.tencentcloud_cvm_instance_vnc_url.instance_vnc_url", "instance_vnc_url", func(value string) error {
+						if value == "" {
+							return fmt.Errorf("vnc url is empty")
+						}
+						return nil
+					}),
+				),
 			},
 		},
 	})
 }
 
-const testAccCvmInstanceVncUrlDataSource = tcacctest.DefaultCvmModificationVariable + `
+const testAccCvmInstanceVncUrlDataSource = `
+data "tencentcloud_images" "default" {
+    image_type = ["PUBLIC_IMAGE"]
+    image_name_regex = "Final"
+}
+data "tencentcloud_instance_types" "default" {
+    memory_size = 2
+    exclude_sold_out = true
+    filter {
+        name = "instance-family"
+        values = ["SA2","SA3","SA4","SA5","S2","S3"]
+    }
+    filter {
+        name = "zone"
+        values = ["ap-guangzhou-7"]
+    }
+    cpu_core_count = 2
+}
+
+resource "tencentcloud_instance" "test_cvm" {
+	image_id = data.tencentcloud_images.default.images.0.image_id
+	availability_zone = "ap-guangzhou-7"
+	instance_type = data.tencentcloud_instance_types.default.instance_types.0.instance_type
+    orderly_security_groups = ["sg-5275dorp"]
+	instance_charge_type = "POSTPAID_BY_HOUR"
+}
 
 data "tencentcloud_cvm_instance_vnc_url" "instance_vnc_url" {
-  instance_id = var.cvm_id
+  instance_id = tencentcloud_instance.test_cvm.id
 }
 `
