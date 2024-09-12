0e28667aaf0e9d4d498a902875c170c5f8353b4d
Yanghao LUO <yhlooo.x@outlook.com>
2024-09-12 17:33:40 +0800

    fix: resource_tc_kubernetes_native_node_pool_extension.go

--- tencentcloud/services/tke/resource_tc_kubernetes_native_node_pool_extension.go
+++ tencentcloud/services/tke/resource_tc_kubernetes_native_node_pool_extension.go
@@ -4,33 +4,33 @@ import (
 	"context"
 	"encoding/base64"
 
+	v20220501 "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/tke/v20220501"
+
 	tccommon "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/common"
 )
 
 func resourceTencentCloudKubernetesNativeNodePoolReadPostHandleResponse0(ctx context.Context, resp *v20220501.NodePool) error {
 	d := tccommon.ResourceDataFromContext(ctx)
-	meta := tccommon.ProviderMetaFromContext(ctx)
-
-	service := TkeService{client: meta.(tccommon.ProviderMeta).GetAPIV3Conn()}
 	respData := resp
 
-	lifecycleMap := map[string]interface{}{}
-	nativeMap = d.Get("native").([]interface{}).(map[string]interface{})
+	if respData.Native != nil {
+		nativeMap := d.Get("native").([]interface{})
+		lifecycleMap := map[string]interface{}{}
+		if respData.Native.Lifecycle != nil {
+			if respData.Native.Lifecycle.PreInit != nil {
+				lifecycleMap["pre_init"] = base64.StdEncoding.EncodeToString([]byte(*respData.Native.Lifecycle.PreInit))
+				//lifecycleMap["pre_init"] = respData.Native.Lifecycle.PreInit
+			}
 
-	if respData.Native.Lifecycle != nil {
-		if respData.Native.Lifecycle.PreInit != nil {
-			lifecycleMap["pre_init"] = base64.StdEncoding.EncodeToString([]byte(*respData.Native.Lifecycle.PreInit))
-			//lifecycleMap["pre_init"] = respData.Native.Lifecycle.PreInit
+			if respData.Native.Lifecycle.PostInit != nil {
+				lifecycleMap["post_init"] = base64.StdEncoding.EncodeToString([]byte(*respData.Native.Lifecycle.PostInit))
+				//lifecycleMap["post_init"] = respData.Native.Lifecycle.PostInit
+			}
+
+			nativeMap[0].(map[string]interface{})["lifecycle"] = []interface{}{lifecycleMap}
+			_ = d.Set("native", []interface{}{nativeMap})
 		}
-
-		if respData.Native.Lifecycle.PostInit != nil {
-			lifecycleMap["post_init"] = base64.StdEncoding.EncodeToString([]byte(*respData.Native.Lifecycle.PostInit))
-			//lifecycleMap["post_init"] = respData.Native.Lifecycle.PostInit
-		}
-
-		nativeMap["lifecycle"] = []interface{}{lifecycleMap}
 	}
-	_ = d.Set("native", []interface{}{nativeMap})
 
 	annotationsList := make([]map[string]interface{}, 0, len(respData.Annotations))
 	if respData.Annotations != nil {
@@ -54,4 +54,6 @@ func resourceTencentCloudKubernetesNativeNodePoolReadPostHandleResponse0(ctx con
 
 		_ = d.Set("annotations", annotationsList)
 	}
+
+	return nil
 }
