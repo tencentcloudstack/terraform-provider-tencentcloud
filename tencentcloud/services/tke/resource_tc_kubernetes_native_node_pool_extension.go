9ce46e0053423a510739f7fbf146a9e123d8d699
andrewjiang <andrewjiang@tencent.com>
2024-09-12 17:05:35 +0800

    refactor(tke): regen tke provider

--- tencentcloud/services/tke/resource_tc_kubernetes_native_node_pool_extension.go
+++ tencentcloud/services/tke/resource_tc_kubernetes_native_node_pool_extension.go
@@ -2,6 +2,7 @@ package tke
 
 import (
 	"context"
+	"encoding/base64"
 
 	tccommon "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/common"
 )
@@ -12,6 +13,25 @@ func resourceTencentCloudKubernetesNativeNodePoolReadPostHandleResponse0(ctx con
 
 	service := TkeService{client: meta.(tccommon.ProviderMeta).GetAPIV3Conn()}
 	respData := resp
+
+	lifecycleMap := map[string]interface{}{}
+	nativeMap = d.Get("native").([]interface{}).(map[string]interface{})
+
+	if respData.Native.Lifecycle != nil {
+		if respData.Native.Lifecycle.PreInit != nil {
+			lifecycleMap["pre_init"] = base64.StdEncoding.EncodeToString([]byte(*respData.Native.Lifecycle.PreInit))
+			//lifecycleMap["pre_init"] = respData.Native.Lifecycle.PreInit
+		}
+
+		if respData.Native.Lifecycle.PostInit != nil {
+			lifecycleMap["post_init"] = base64.StdEncoding.EncodeToString([]byte(*respData.Native.Lifecycle.PostInit))
+			//lifecycleMap["post_init"] = respData.Native.Lifecycle.PostInit
+		}
+
+		nativeMap["lifecycle"] = []interface{}{lifecycleMap}
+	}
+	_ = d.Set("native", []interface{}{nativeMap})
+
 	annotationsList := make([]map[string]interface{}, 0, len(respData.Annotations))
 	if respData.Annotations != nil {
 		for _, annotations := range respData.Annotations {
