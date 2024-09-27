33af45542099b642d99d01d44464eb29e8d927fe
tongyiming <1290863722@qq.com>
2024-09-27 20:05:57 +0800

    feat(tco): [119841102] tco node and member support tag (#2852)
    * tco node and member support tag
    
    * add changelog
    
    ---------
    
    Co-authored-by: mikatong <mikatong@tencent.com>

--- tencentcloud/services/tco/resource_tc_organization_org_member_test.go
+++ tencentcloud/services/tco/resource_tc_organization_org_member_test.go
@@ -12,7 +12,7 @@ func TestAccTencentCloudOrganizationOrgMemberResource_basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
-			tcacctest.AccPreCheckCommon(t, tcacctest.ACCOUNT_TYPE_ORGANIZATION)
+			tcacctest.AccPreCheck(t)
 		},
 		Providers: tcacctest.AccProviders,
 		Steps: []resource.TestStep{
@@ -20,20 +20,23 @@ func TestAccTencentCloudOrganizationOrgMemberResource_basic(t *testing.T) {
 				Config: testAccOrganizationOrgMember,
 				Check: resource.ComposeTestCheckFunc(resource.TestCheckResourceAttrSet("tencentcloud_organization_org_member.org_member", "id"),
 					resource.TestCheckResourceAttr("tencentcloud_organization_org_member.org_member", "name", "tf_example"),
-					resource.TestCheckResourceAttr("tencentcloud_organization_org_member.org_member", "node_id", "59849"),
+					resource.TestCheckResourceAttr("tencentcloud_organization_org_member.org_member", "node_id", "2023047"),
 					resource.TestCheckResourceAttrSet("tencentcloud_organization_org_member.org_member", "permission_ids.#"),
 					resource.TestCheckResourceAttr("tencentcloud_organization_org_member.org_member", "policy_type", "Financial"),
 					resource.TestCheckResourceAttr("tencentcloud_organization_org_member.org_member", "remark", "for terraform test"),
+					resource.TestCheckResourceAttr("tencentcloud_organization_org_member.org_member", "tags.tf-test1", "org_member1"),
+					resource.TestCheckResourceAttr("tencentcloud_organization_org_member.org_member", "tags.tf-test2", "org_member2"),
 				),
 			},
 			{
 				Config: testAccOrganizationOrgMemberUpdate,
 				Check: resource.ComposeTestCheckFunc(resource.TestCheckResourceAttrSet("tencentcloud_organization_org_member.org_member", "id"),
 					resource.TestCheckResourceAttr("tencentcloud_organization_org_member.org_member", "name", "tf_example_1"),
-					resource.TestCheckResourceAttr("tencentcloud_organization_org_member.org_member", "node_id", "59849"),
+					resource.TestCheckResourceAttr("tencentcloud_organization_org_member.org_member", "node_id", "2023047"),
 					resource.TestCheckResourceAttrSet("tencentcloud_organization_org_member.org_member", "permission_ids.#"),
 					resource.TestCheckResourceAttr("tencentcloud_organization_org_member.org_member", "policy_type", "Financial"),
 					resource.TestCheckResourceAttr("tencentcloud_organization_org_member.org_member", "remark", "for terraform test"),
+					resource.TestCheckResourceAttr("tencentcloud_organization_org_member.org_member", "tags.tf-test1", "org_member_update"),
 				),
 			},
 			{
@@ -49,29 +52,36 @@ const testAccOrganizationOrgMember = `
 
 resource "tencentcloud_organization_org_member" "org_member" {
   name            = "tf_example"
-  node_id         = 59849
+  node_id         = 2023047
   permission_ids  = [
     1,
     2,
-    3,
     4,
+	5,
   ]
   policy_type     = "Financial"
   remark          = "for terraform test"
+  tags = {
+    "tf-test1" = "org_member1"
+	"tf-test2" = "org_member2"
+  }
 }
 `
 const testAccOrganizationOrgMemberUpdate = `
 
 resource "tencentcloud_organization_org_member" "org_member" {
   name            = "tf_example_1"
-  node_id         = 59849
+  node_id         = 2023047
   permission_ids  = [
     1,
     2,
-    3,
+    4,
   ]
   policy_type     = "Financial"
   remark          = "for terraform test"
+  tags = {
+    "tf-test1" = "org_member_update"
+  }
 }
 
 `
