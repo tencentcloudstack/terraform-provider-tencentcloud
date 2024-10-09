0fa5f913c5b89d86340aca0ad019365aeb40f565
SevenEarth <391613297@qq.com>
2024-10-09 19:51:22 +0800

    add

--- tencentcloud/services/ssl/resource_tc_ssl_check_certificate_domain_verification_operation_test.go
+++ tencentcloud/services/ssl/resource_tc_ssl_check_certificate_domain_verification_operation_test.go
@@ -1,14 +1,13 @@
-package ssl_test
+package ssl
 
 import (
 	"testing"
 
-	tcacctest "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/acctest"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	tcacctest "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/acctest"
 )
 
-// go test -i; go test -test.run TestAccTencentCloudSslCheckCertificateDomainVerificationOperationResource_basic -v
 func TestAccTencentCloudSslCheckCertificateDomainVerificationOperationResource_basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
@@ -16,20 +15,19 @@ func TestAccTencentCloudSslCheckCertificateDomainVerificationOperationResource_b
 			tcacctest.AccPreCheck(t)
 		},
 		Providers: tcacctest.AccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccSslCheckCertificateDomainVerification,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("tencentcloud_ssl_check_certificate_domain_verification_operation.example", "id"),
-					resource.TestCheckResourceAttrSet("tencentcloud_ssl_check_certificate_domain_verification_operation.example", "certificate_id"),
-				),
-			},
-		},
+		Steps: []resource.TestStep{{
+			Config: testAccSslCheckCertificateDomainVerificationOperation,
+			Check:  resource.ComposeTestCheckFunc(resource.TestCheckResourceAttrSet("tencentcloud_ssl_check_certificate_domain_verification_operation.ssl_check_certificate_domain_verification_operation", "id")),
+		}, {
+			ResourceName:      "tencentcloud_ssl_check_certificate_domain_verification_operation.ssl_check_certificate_domain_verification_operation",
+			ImportState:       true,
+			ImportStateVerify: true,
+		}},
 	})
 }
 
-const testAccSslCheckCertificateDomainVerification = `
-resource "tencentcloud_ssl_check_certificate_domain_verification_operation" "example" {
-  certificate_id = "6BE701Jx"
+const testAccSslCheckCertificateDomainVerificationOperation = `
+
+resource "tencentcloud_ssl_check_certificate_domain_verification_operation" "ssl_check_certificate_domain_verification_operation" {
 }
 `
