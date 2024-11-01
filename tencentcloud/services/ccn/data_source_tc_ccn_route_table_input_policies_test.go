ce0f87b97dd4491848732d891370ddab7fac15b9
SevenEarth <45937856+SevenEarth@users.noreply.github.com>
2024-11-01 15:20:13 +0800

    fix(ccn): [120224824] Update `tencentcloud_ccn_route_table_input_policies` unit test (#2915)
    * add
    
    * add
    
    * add

--- tencentcloud/services/ccn/data_source_tc_ccn_route_table_input_policies_test.go
+++ tencentcloud/services/ccn/data_source_tc_ccn_route_table_input_policies_test.go
@@ -7,6 +7,7 @@ import (
 	tcacctest "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/acctest"
 )
 
+// go test -i; go test -test.run TestAccTencentCloudCcnRouteTableInputPoliciesDataSource_basic -v
 func TestAccTencentCloudCcnRouteTableInputPoliciesDataSource_basic(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
@@ -20,14 +21,47 @@ func TestAccTencentCloudCcnRouteTableInputPoliciesDataSource_basic(t *testing.T)
 				tcacctest.AccCheckTencentCloudDataSourceID("data.tencentcloud_ccn_route_table_input_policies.example"),
 				resource.TestCheckResourceAttrSet("data.tencentcloud_ccn_route_table_input_policies.example", "ccn_id"),
 				resource.TestCheckResourceAttrSet("data.tencentcloud_ccn_route_table_input_policies.example", "route_table_id"),
+				resource.TestCheckResourceAttr("data.tencentcloud_ccn_route_table_input_policies.example", "policy_set.#", "1"),
 			),
 		}},
 	})
 }
 
 const testAccCcnRouteTableInputPoliciesDataSource = `
+resource "tencentcloud_ccn" "example" {
+  name                 = "tf-example"
+  description          = "description."
+  qos                  = "AG"
+  charge_type          = "PREPAID"
+  bandwidth_limit_type = "INTER_REGION_LIMIT"
+  tags = {
+    createBy = "terraform"
+  }
+}
+
+resource "tencentcloud_ccn_route_table" "example" {
+  ccn_id      = tencentcloud_ccn.example.id
+  name        = "tf-example"
+  description = "desc."
+}
+
+resource "tencentcloud_ccn_route_table_input_policies" "example" {
+  ccn_id         = tencentcloud_ccn.example.id
+  route_table_id = tencentcloud_ccn_route_table.example.id
+  policies {
+    action      = "accept"
+    description = "desc."
+    route_conditions {
+      name          = "instance-region"
+      values        = ["ap-guangzhou"]
+      match_pattern = 1
+    }
+  }
+}
+
 data "tencentcloud_ccn_route_table_input_policies" "example" {
-  ccn_id         = "ccn-06jek8tf"
-  route_table_id = "ccnrtb-4jv5ltb9"
+  depends_on = [ tencentcloud_ccn_route_table_input_policies.example ]
+  ccn_id         = tencentcloud_ccn.example.id
+  route_table_id = tencentcloud_ccn_route_table.example.id
 }
 `
