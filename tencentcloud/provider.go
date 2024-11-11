2ec6a24405390922e34d77b46ca6ffd1b4097687
SevenEarth <45937856+SevenEarth@users.noreply.github.com>
2024-11-11 17:38:21 +0800

    fix(provider): [120516339] Adapt to data type for `tccli` (#2944)
    * add
    
    * add
    
    * add

--- tencentcloud/provider.go
+++ tencentcloud/provider.go
@@ -2224,11 +2224,13 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 		region = v.(string)
 	}
 
-	if secretId == "" && secretKey == "" && securityToken == "" && region == "" {
+	if secretId == "" && secretKey == "" && securityToken == "" {
 		secretId = getProviderConfig("secretId")
 		secretKey = getProviderConfig("secretKey")
 		securityToken = getProviderConfig("token")
-		region = getProviderConfig("region")
+		if region == "" {
+			region = getProviderConfig("region")
+		}
 	}
 
 	if region == "" {
@@ -2564,7 +2566,9 @@ func getConfigFromProfile(d *schema.ResourceData, ProfileKey string) (interface{
 			}
 
 			for k, v := range config {
-				providerConfig[k] = strings.TrimSpace(v.(string))
+				if strValue, ok := v.(string); ok {
+					providerConfig[k] = strings.TrimSpace(strValue)
+				}
 			}
 		}
 
