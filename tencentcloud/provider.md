b464607e4ded474b7b7f871ce2f1b9c117d7655e
mikatong <mikatong@tencent.com>
2024-11-15 15:09:08 +0800

    support tencentcloud_identity_center_role_configuration_permission_custom_policies_attachment

--- tencentcloud/provider.md
+++ tencentcloud/provider.md
@@ -1715,6 +1715,7 @@ Tencent Cloud Organization (TCO)
     tencentcloud_identity_center_role_configuration
     tencentcloud_identity_center_role_configuration_permission_policy_attachment
     tencentcloud_identity_center_role_configuration_permission_custom_policy_attachment
+    tencentcloud_identity_center_role_configuration_permission_custom_policies_attachment
     tencentcloud_identity_center_user_sync_provisioning
     tencentcloud_identity_center_role_assignment
     tencentcloud_invite_organization_member_operation
---
+++ tencentcloud/services/tco/resource_tc_identity_center_role_configuration_permission_custom_policies_attachment_test.go
@@ -0,0 +1,87 @@
+package tco_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	tcacctest "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/acctest"
+)
+
+func TestAccTencentCloudIdentityCenterRoleConfigurationPermissionCustomPoliciesAttachmentResource_basic(t *testing.T) {
+	t.Parallel()
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			tcacctest.AccPreCheck(t)
+		},
+		Providers: tcacctest.AccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIdentityCenterRoleConfigurationPermissionPoliciesAttachmentCustomPolicy,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("tencentcloud_identity_center_role_configuration_permission_custom_policies_attachment.identity_center_role_configuration_permission_custom_policies_attachment", "id"),
+					resource.TestCheckResourceAttrSet("tencentcloud_identity_center_role_configuration_permission_custom_policies_attachment.identity_center_role_configuration_permission_custom_policies_attachment", "zone_id"),
+					resource.TestCheckResourceAttrSet("tencentcloud_identity_center_role_configuration_permission_custom_policies_attachment.identity_center_role_configuration_permission_custom_policies_attachment", "role_configuration_id"),
+					resource.TestCheckResourceAttr("tencentcloud_identity_center_role_configuration_permission_custom_policies_attachment.identity_center_role_configuration_permission_custom_policies_attachment", "policies.#", "2"),
+				),
+			},
+			{
+				ResourceName:      "tencentcloud_identity_center_role_configuration_permission_custom_policies_attachment.identity_center_role_configuration_permission_custom_policies_attachment",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+const testAccIdentityCenterRoleConfigurationPermissionPoliciesAttachmentCustomPolicy = `
+resource "tencentcloud_identity_center_role_configuration" "identity_center_role_configuration" {
+    zone_id = "z-s64jh54hbcra"
+    role_configuration_name = "tf-test-custom-policy"
+    description = "test"
+}
+
+resource "tencentcloud_identity_center_role_configuration_permission_custom_policies_attachment" "identity_center_role_configuration_permission_custom_policies_attachment" {
+    zone_id = "z-s64jh54hbcra"
+    role_configuration_id = tencentcloud_identity_center_role_configuration.identity_center_role_configuration.role_configuration_id
+    policies {
+        role_policy_name = "CustomPolicy2"
+        role_policy_document = <<-EOF
+{
+    "version": "2.0",
+    "statement": [
+        {
+            "effect": "allow",
+            "action": [
+                "vpc:AcceptAttachCcnInstances"
+            ],
+            "resource": [
+                "*"
+            ]
+        }
+    ]
+}
+EOF
+    }
+    policies {
+        role_policy_name = "CustomPolicy1"
+        role_policy_document = <<-EOF
+{
+    "version": "2.0",
+    "statement": [
+        {
+            "effect": "allow",
+            "action": [
+                "vpc:AcceptAttachCcnInstances"
+            ],
+            "resource": [
+                "*"
+            ]
+        }
+    ]
+}
+EOF
+    }
+    
+}
+`
