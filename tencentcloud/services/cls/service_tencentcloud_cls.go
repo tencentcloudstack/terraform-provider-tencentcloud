c0ab1b6d48a72c33d9acd10d9401e4e42a592dc0
SevenEarth <391613297@qq.com>
2024-11-29 11:16:24 +0800

    add

--- tencentcloud/services/cls/service_tencentcloud_cls.go
+++ tencentcloud/services/cls/service_tencentcloud_cls.go
@@ -1399,3 +1399,36 @@ func (me *ClsService) DescribeClsCloudProductLogTaskById(ctx context.Context) (r
 	ret = response.Response
 	return
 }
+
+func (me *ClsService) DescribeClsNoticeContentById(ctx context.Context, noticeContentId string) (ret *cls.NoticeContentTemplate, errRet error) {
+	logId := tccommon.GetLogId(ctx)
+
+	request := cls.NewDescribeNoticeContentsRequest()
+	filter := &cls.Filter{
+		Key:    helper.String("noticeContentId"),
+		Values: []*string{helper.String(noticeContentId)},
+	}
+	request.Filters = append(request.Filters, filter)
+
+	defer func() {
+		if errRet != nil {
+			log.Printf("[CRITAL]%s api[%s] fail, request body [%s], reason[%s]\n", logId, request.GetAction(), request.ToJsonString(), errRet.Error())
+		}
+	}()
+
+	ratelimit.Check(request.GetAction())
+
+	response, err := me.client.UseClsV20201016Client().DescribeNoticeContents(request)
+	if err != nil {
+		errRet = err
+		return
+	}
+	log.Printf("[DEBUG]%s api[%s] success, request body [%s], response body [%s]\n", logId, request.GetAction(), request.ToJsonString(), response.ToJsonString())
+
+	if len(response.Response.NoticeContents) < 1 {
+		return
+	}
+
+	ret = response.Response.NoticeContents[0]
+	return
+}
