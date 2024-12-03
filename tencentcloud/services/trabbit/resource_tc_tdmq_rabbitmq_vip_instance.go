f4d1357958ff686a64b2b64d3160b1bb7fc294fb
SevenEarth <45937856+SevenEarth@users.noreply.github.com>
2024-12-03 19:59:24 +0800

    fix(tdmq): [121036630] `tencentcloud_tdmq_rabbitmq_vip_instance` update doc  (#2995)
    * add
    
    * add
    
    * add

--- tencentcloud/services/trabbit/resource_tc_tdmq_rabbitmq_vip_instance.go
+++ tencentcloud/services/trabbit/resource_tc_tdmq_rabbitmq_vip_instance.go
@@ -49,7 +49,7 @@ func ResourceTencentCloudTdmqRabbitmqVipInstance() *schema.Resource {
 			"node_spec": {
 				Optional:    true,
 				Type:        schema.TypeString,
-				Description: "Node specifications, basic type rabbit-vip-basic-1, standard type rabbit-vip-basic-2, high-level type 1 rabbit-vip-basic-3, high-level type 2 rabbit-vip-basic-4. If not passed, the default is the basic type.",
+				Description: "Node specifications. Valid values: rabbit-vip-basic-1 (for 4C8G), rabbit-vip-basic-2 (for 8C16G), rabbit-vip-basic-3 (for 12C24G), rabbit-vip-basic-4 (for 16C32G), rabbit-vip-basic-5 (for 2C4G), rabbit-vip-basic-6 (for 4C12G), rabbit-vip-basic-7 (for 8C24G). The default is rabbit-vip-basic-1. NOTE: The above specifications may be sold out or removed from the shelves.",
 			},
 			"node_num": {
 				Optional:    true,
