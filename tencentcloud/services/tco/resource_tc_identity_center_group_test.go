9749ae5281e1457861427844c6b6770dfd83243a
mikatong <mikatong@tencent.com>
2024-12-03 15:44:01 +0800

    update

--- tencentcloud/services/tco/resource_tc_identity_center_group_test.go
+++ tencentcloud/services/tco/resource_tc_identity_center_group_test.go
@@ -48,18 +48,18 @@ func TestAccTencentCloudIdentityCenterGroupResource_basic(t *testing.T) {
 
 const testAccIdentityCenterGroup = `
 resource "tencentcloud_identity_center_group" "identity_center_group" {
-	zone_id = "z-s64jh54hbcra"
+    zone_id = "z-s64jh54hbcra"
     group_name = "tf-test-group"
     description = "test"
-	group_type = "Manual"
+    group_type = "Manual"
 }
 `
 
 const testAccIdentityCenterGroupUpdate = `
 resource "tencentcloud_identity_center_group" "identity_center_group" {
-	zone_id = "z-s64jh54hbcra"
+    zone_id = "z-s64jh54hbcra"
     group_name = "tf-test-group"
     description = "test_update"
-	group_type = "Manual"
+    group_type = "Manual"
 }
 `
