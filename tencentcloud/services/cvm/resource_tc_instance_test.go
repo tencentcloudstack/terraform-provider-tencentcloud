fff359ae955da0c1e9da2df793f7dd5c559d5a4f
SevenEarth <391613297@qq.com>
2024-12-16 15:15:21 +0800

    add

--- tencentcloud/services/cvm/resource_tc_instance_test.go
+++ tencentcloud/services/cvm/resource_tc_instance_test.go
@@ -539,6 +539,7 @@ resource "tencentcloud_instance" "foo" {
         delete_with_instance = true
     }
     disable_security_service = true
+    disable_automation_service = true
     
     lifecycle {
         ignore_changes = [instance_type]
@@ -591,6 +592,7 @@ resource "tencentcloud_instance" "foo" {
         data_disk_type = "CLOUD_PREMIUM"
     }
     disable_security_service = true
+    disable_automation_service = true
     
     lifecycle {
         ignore_changes = [instance_type]
@@ -2140,6 +2142,7 @@ resource "tencentcloud_instance" "foo" {
 
   disable_security_service = true
   disable_monitor_service  = true
+  disable_automation_service = true
   lifecycle {
 	ignore_changes = [instance_type]
   }
@@ -2240,6 +2243,7 @@ resource "tencentcloud_instance" "foo" {
 
   disable_security_service = true
   disable_monitor_service  = true
+  disable_automation_service = true
   lifecycle {
 	ignore_changes = [instance_type]
   }
