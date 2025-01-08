11851ad7177bbea9d7d9c85e666420025c833e57
SevenEarth <45937856+SevenEarth@users.noreply.github.com>
2025-01-08 14:41:09 +0800

    Update data_source_tc_as_advices.go

--- tencentcloud/services/as/data_source_tc_as_advices.go
+++ tencentcloud/services/as/data_source_tc_as_advices.go
@@ -22,13 +22,13 @@ func DataSourceTencentCloudAsAdvices() *schema.Resource {
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
-				Description: "List of scaling groups to be queried. Upper limit: 100.",
+				Description: "123List of scaling groups to be queried. Upper limit: 100.",
 			},
 
 			"auto_scaling_advice_set": {
 				Computed:    true,
 				Type:        schema.TypeList,
-				Description: "A collection of suggestions for scaling group configurations.",
+				Description: "123A collection of suggestions for scaling group configurations.",
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"auto_scaling_group_id": {
