5aed6322e99314f60364730f539d0e18fece0075
SevenEarth <45937856+SevenEarth@users.noreply.github.com>
2025-07-01 20:11:03 +0800

    fix(trocket): [125261403] `tencentcloud_trocket_rocketmq_topic` optmize read function code logic (#3425)
    * add
    
    * add

--- tencentcloud/services/trocket/resource_tc_trocket_rocketmq_topic.go
+++ tencentcloud/services/trocket/resource_tc_trocket_rocketmq_topic.go
@@ -26,35 +26,35 @@ func ResourceTencentCloudTrocketRocketmqTopic() *schema.Resource {
 		},
 		Schema: map[string]*schema.Schema{
 			"instance_id": {
-				Required:    true,
 				Type:        schema.TypeString,
+				Required:    true,
 				ForceNew:    true,
 				Description: "Instance Id.",
 			},
 
 			"topic": {
-				Required:    true,
 				Type:        schema.TypeString,
+				Required:    true,
 				ForceNew:    true,
 				Description: "topic.",
 			},
 
 			"topic_type": {
+				Type:        schema.TypeString,
 				Required:    true,
 				ForceNew:    true,
-				Type:        schema.TypeString,
 				Description: "Topic type. `UNSPECIFIED`: not specified, `NORMAL`: normal message, `FIFO`: sequential message, `DELAY`: delayed message.",
 			},
 
 			"queue_num": {
-				Required:    true,
 				Type:        schema.TypeInt,
+				Required:    true,
 				Description: "Number of queue. Must be greater than or equal to 3.",
 			},
 
 			"remark": {
-				Optional:    true,
 				Type:        schema.TypeString,
+				Optional:    true,
 				Description: "remark.",
 			},
 		},
@@ -65,14 +65,14 @@ func resourceTencentCloudTrocketRocketmqTopicCreate(d *schema.ResourceData, meta
 	defer tccommon.LogElapsed("resource.tencentcloud_trocket_rocketmq_topic.create")()
 	defer tccommon.InconsistentCheck(d, meta)()
 
-	logId := tccommon.GetLogId(tccommon.ContextNil)
-
 	var (
+		logId      = tccommon.GetLogId(tccommon.ContextNil)
 		request    = trocket.NewCreateTopicRequest()
 		response   = trocket.NewCreateTopicResponse()
 		instanceId string
 		topic      string
 	)
+
 	if v, ok := d.GetOk("instance_id"); ok {
 		request.InstanceId = helper.String(v.(string))
 	}
@@ -100,18 +100,27 @@ func resourceTencentCloudTrocketRocketmqTopicCreate(d *schema.ResourceData, meta
 		} else {
 			log.Printf("[DEBUG]%s api[%s] success, request body [%s], response body [%s]\n", logId, request.GetAction(), request.ToJsonString(), result.ToJsonString())
 		}
+
+		if result == nil || result.Response == nil {
+			return resource.NonRetryableError(fmt.Errorf("Create trocket rocketmqTopic failed, Response is nil."))
+		}
+
 		response = result
 		return nil
 	})
+
 	if err != nil {
 		log.Printf("[CRITAL]%s create trocket rocketmqTopic failed, reason:%+v", logId, err)
 		return err
 	}
 
+	if response.Response.InstanceId == nil || response.Response.Topic == nil {
+		return fmt.Errorf("InstanceId or Topic is nil.")
+	}
+
 	instanceId = *response.Response.InstanceId
 	topic = *response.Response.Topic
-	d.SetId(instanceId + tccommon.FILED_SP + topic)
-
+	d.SetId(strings.Join([]string{instanceId, topic}, tccommon.FILED_SP))
 	return resourceTencentCloudTrocketRocketmqTopicRead(d, meta)
 }
 
@@ -119,16 +128,17 @@ func resourceTencentCloudTrocketRocketmqTopicRead(d *schema.ResourceData, meta i
 	defer tccommon.LogElapsed("resource.tencentcloud_trocket_rocketmq_topic.read")()
 	defer tccommon.InconsistentCheck(d, meta)()
 
-	logId := tccommon.GetLogId(tccommon.ContextNil)
-
-	ctx := context.WithValue(context.TODO(), tccommon.LogIdKey, logId)
-
-	service := TrocketService{client: meta.(tccommon.ProviderMeta).GetAPIV3Conn()}
+	var (
+		logId   = tccommon.GetLogId(tccommon.ContextNil)
+		ctx     = context.WithValue(context.TODO(), tccommon.LogIdKey, logId)
+		service = TrocketService{client: meta.(tccommon.ProviderMeta).GetAPIV3Conn()}
+	)
 
 	idSplit := strings.Split(d.Id(), tccommon.FILED_SP)
 	if len(idSplit) != 2 {
 		return fmt.Errorf("id is broken,%s", d.Id())
 	}
+
 	instanceId := idSplit[0]
 	topic := idSplit[1]
 
@@ -138,8 +148,8 @@ func resourceTencentCloudTrocketRocketmqTopicRead(d *schema.ResourceData, meta i
 	}
 
 	if rocketmqTopic == nil {
-		d.SetId("")
 		log.Printf("[WARN]%s resource `TrocketRocketmqTopic` [%s] not found, please check if it has been deleted.\n", logId, d.Id())
+		d.SetId("")
 		return nil
 	}
 
@@ -170,44 +180,46 @@ func resourceTencentCloudTrocketRocketmqTopicUpdate(d *schema.ResourceData, meta
 	defer tccommon.LogElapsed("resource.tencentcloud_trocket_rocketmq_topic.update")()
 	defer tccommon.InconsistentCheck(d, meta)()
 
-	logId := tccommon.GetLogId(tccommon.ContextNil)
-
-	request := trocket.NewModifyTopicRequest()
+	var (
+		logId   = tccommon.GetLogId(tccommon.ContextNil)
+		request = trocket.NewModifyTopicRequest()
+	)
 
 	idSplit := strings.Split(d.Id(), tccommon.FILED_SP)
 	if len(idSplit) != 2 {
 		return fmt.Errorf("id is broken,%s", d.Id())
 	}
+
 	instanceId := idSplit[0]
 	topic := idSplit[1]
 
-	request.InstanceId = &instanceId
-	request.Topic = &topic
+	if d.HasChange("queue_num") || d.HasChange("remark") {
+		request.InstanceId = &instanceId
+		request.Topic = &topic
 
-	if d.HasChange("queue_num") {
 		if v, ok := d.GetOkExists("queue_num"); ok {
 			request.QueueNum = helper.IntInt64(v.(int))
 		}
-	}
 
-	if d.HasChange("remark") {
 		if v, ok := d.GetOk("remark"); ok {
 			request.Remark = helper.String(v.(string))
 		}
-	}
 
-	err := resource.Retry(tccommon.WriteRetryTimeout, func() *resource.RetryError {
-		result, e := meta.(tccommon.ProviderMeta).GetAPIV3Conn().UseTrocketClient().ModifyTopic(request)
-		if e != nil {
-			return tccommon.RetryError(e)
-		} else {
-			log.Printf("[DEBUG]%s api[%s] success, request body [%s], response body [%s]\n", logId, request.GetAction(), request.ToJsonString(), result.ToJsonString())
+		err := resource.Retry(tccommon.WriteRetryTimeout, func() *resource.RetryError {
+			result, e := meta.(tccommon.ProviderMeta).GetAPIV3Conn().UseTrocketClient().ModifyTopic(request)
+			if e != nil {
+				return tccommon.RetryError(e)
+			} else {
+				log.Printf("[DEBUG]%s api[%s] success, request body [%s], response body [%s]\n", logId, request.GetAction(), request.ToJsonString(), result.ToJsonString())
+			}
+
+			return nil
+		})
+
+		if err != nil {
+			log.Printf("[CRITAL]%s update trocket rocketmqTopic failed, reason:%+v", logId, err)
+			return err
 		}
-		return nil
-	})
-	if err != nil {
-		log.Printf("[CRITAL]%s update trocket rocketmqTopic failed, reason:%+v", logId, err)
-		return err
 	}
 
 	return resourceTencentCloudTrocketRocketmqTopicRead(d, meta)
@@ -217,14 +229,17 @@ func resourceTencentCloudTrocketRocketmqTopicDelete(d *schema.ResourceData, meta
 	defer tccommon.LogElapsed("resource.tencentcloud_trocket_rocketmq_topic.delete")()
 	defer tccommon.InconsistentCheck(d, meta)()
 
-	logId := tccommon.GetLogId(tccommon.ContextNil)
-	ctx := context.WithValue(context.TODO(), tccommon.LogIdKey, logId)
+	var (
+		logId   = tccommon.GetLogId(tccommon.ContextNil)
+		ctx     = context.WithValue(context.TODO(), tccommon.LogIdKey, logId)
+		service = TrocketService{client: meta.(tccommon.ProviderMeta).GetAPIV3Conn()}
+	)
 
-	service := TrocketService{client: meta.(tccommon.ProviderMeta).GetAPIV3Conn()}
 	idSplit := strings.Split(d.Id(), tccommon.FILED_SP)
 	if len(idSplit) != 2 {
 		return fmt.Errorf("id is broken,%s", d.Id())
 	}
+
 	instanceId := idSplit[0]
 	topic := idSplit[1]
 
