dbb6e213c0bef27b288aa51a9fc69a605adf1666
SevenEarth <391613297@qq.com>
2025-08-14 10:30:33 +0800

    add

--- vendor/github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/http/request.go
+++ vendor/github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/http/request.go
@@ -265,11 +265,7 @@ func CompleteCommonParams(request Request, region string, requestClient string) 
 	params["Action"] = request.GetAction()
 	params["Timestamp"] = strconv.FormatInt(time.Now().Unix(), 10)
 	params["Nonce"] = strconv.Itoa(rand.Int())
-<<<<<<< HEAD
 	params["RequestClient"] = "SDK_GO_1.1.8"
-=======
-	params["RequestClient"] = "SDK_GO_1.0.1211"
->>>>>>> 61f41828f (add)
 	if requestClient != "" {
 		params["RequestClient"] += ": " + requestClient
 	}
