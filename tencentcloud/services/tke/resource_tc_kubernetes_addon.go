6a401b29973ee2b09f0da24beb0c8e4158c43f8d
SevenEarth <391613297@qq.com>
2025-10-17 16:48:25 +0800

    add

--- tencentcloud/services/tke/resource_tc_kubernetes_addon.go
+++ tencentcloud/services/tke/resource_tc_kubernetes_addon.go
@@ -27,9 +27,9 @@ func ResourceTencentCloudKubernetesAddon() *schema.Resource {
 			State: schema.ImportStatePassthrough,
 		},
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(10 * time.Minute),
-			Update: schema.DefaultTimeout(10 * time.Minute),
-			Delete: schema.DefaultTimeout(10 * time.Minute),
+			Create: schema.DefaultTimeout(3 * time.Minute),
+			Update: schema.DefaultTimeout(3 * time.Minute),
+			Delete: schema.DefaultTimeout(3 * time.Minute),
 		},
 		Schema: map[string]*schema.Schema{
 			"cluster_id": {
