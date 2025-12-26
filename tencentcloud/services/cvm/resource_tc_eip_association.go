35983e577ca83624fa14656b9949025e8f490fe1
SevenEarth <391613297@qq.com>
2025-12-26 14:50:32 +0800

    add

--- tencentcloud/services/cvm/resource_tc_eip_association.go
+++ tencentcloud/services/cvm/resource_tc_eip_association.go
@@ -42,7 +42,7 @@ func ResourceTencentCloudEipAssociation() *schema.Resource {
 					"private_ip",
 				},
 				ValidateFunc: tccommon.ValidateStringLengthInRange(1, 25),
-				Description:  "The CVM, SaaS WAF or CLB instance id going to bind with the EIP. This field is conflict with `network_interface_id` and `private_ip fields`.",
+				Description:  "The ID of the target resource to associate with the Elastic IP (EIP). Supported targets include a CVM instance, SaaS WAF instance, CLB instance, or a VPC endpoint.\nLimitation (GWLB VPC endpoint): Only an EIP in the bound state can be associated with a GWLB-type VPC endpoint through this field, enabling more advanced networking scenarios.\nMutual exclusivity: This field conflicts with `network_interface_id` and `private_ip`. Only one association target can be specified per request.",
 			},
 			"network_interface_id": {
 				Type:         schema.TypeString,
