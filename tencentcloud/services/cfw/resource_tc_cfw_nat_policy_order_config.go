303518af654cc8d65238a5fcf5745fc2fc6a071c
SevenEarth <391613297@qq.com>
2025-12-29 19:53:26 +0800

    add

---
+++ tencentcloud/services/cfw/resource_tc_cfw_nat_policy_order_config.go
@@ -0,0 +1,189 @@
+package cfw
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	cfwv20190904 "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/cfw/v20190904"
+
+	tccommon "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/common"
+	"github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/internal/helper"
+)
+
+func ResourceTencentCloudCfwNatPolicyOrderConfig() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceTencentCloudCfwNatPolicyOrderConfigCreate,
+		Read:   resourceTencentCloudCfwNatPolicyOrderConfigRead,
+		Update: resourceTencentCloudCfwNatPolicyOrderConfigUpdate,
+		Delete: resourceTencentCloudCfwNatPolicyOrderConfigDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"uuid": {
+				Type:        schema.TypeInt,
+				Required:    true,
+				Description: "The unique ID of the rule, which is not required when you create a rule.",
+			},
+			"order_index": {
+				Type:        schema.TypeInt,
+				Required:    true,
+				Description: "Rule sequence number.",
+			},
+		},
+	}
+}
+
+func resourceTencentCloudCfwNatPolicyOrderConfigCreate(d *schema.ResourceData, meta interface{}) error {
+	defer tccommon.LogElapsed("resource.tencentcloud_cfw_nat_policy_order_config.create")()
+	defer tccommon.InconsistentCheck(d, meta)()
+
+	var (
+		uuid string
+	)
+
+	if v, ok := d.GetOkExists("uuid"); ok {
+		uuid = helper.IntToStr(v.(int))
+	}
+
+	d.SetId(uuid)
+	return resourceTencentCloudCfwNatPolicyOrderConfigUpdate(d, meta)
+}
+
+func resourceTencentCloudCfwNatPolicyOrderConfigRead(d *schema.ResourceData, meta interface{}) error {
+	defer tccommon.LogElapsed("resource.tencentcloud_cfw_nat_policy_order_config.read")()
+	defer tccommon.InconsistentCheck(d, meta)()
+
+	var (
+		logId   = tccommon.GetLogId(tccommon.ContextNil)
+		ctx     = tccommon.NewResourceLifeCycleHandleFuncContext(context.Background(), logId, d, meta)
+		service = CfwService{client: meta.(tccommon.ProviderMeta).GetAPIV3Conn()}
+		uuid    = d.Id()
+	)
+
+	respData, err := service.DescribeCfwNatPolicyOrderConfigById(ctx, uuid)
+	if err != nil {
+		return err
+	}
+
+	if respData == nil {
+		log.Printf("[WARN]%s resource `tencentcloud_cfw_nat_policy_order_config` [%s] not found, please check if it has been deleted.\n", logId, d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	if respData.Uuid != nil {
+		_ = d.Set("uuid", respData.Uuid)
+	}
+
+	if respData.OrderIndex != nil {
+		_ = d.Set("order_index", respData.OrderIndex)
+	}
+
+	return nil
+}
+
+func resourceTencentCloudCfwNatPolicyOrderConfigUpdate(d *schema.ResourceData, meta interface{}) error {
+	defer tccommon.LogElapsed("resource.tencentcloud_cfw_nat_policy_order_config.update")()
+	defer tccommon.InconsistentCheck(d, meta)()
+
+	var (
+		logId   = tccommon.GetLogId(tccommon.ContextNil)
+		ctx     = tccommon.NewResourceLifeCycleHandleFuncContext(context.Background(), logId, d, meta)
+		service = CfwService{client: meta.(tccommon.ProviderMeta).GetAPIV3Conn()}
+		request = cfwv20190904.NewModifyNatAcRuleRequest()
+		uuid    = d.Id()
+	)
+
+	natPolicy, err := service.DescribeCfwNatPolicyById(ctx, uuid)
+	if err != nil {
+		return err
+	}
+
+	if natPolicy == nil {
+		return fmt.Errorf("uuid does not exist.")
+	}
+
+	modifyRuleItem := cfwv20190904.CreateNatRuleItem{}
+	if natPolicy.SourceContent != nil {
+		modifyRuleItem.SourceContent = natPolicy.SourceContent
+	}
+
+	if natPolicy.SourceType != nil {
+		modifyRuleItem.SourceType = natPolicy.SourceType
+	}
+
+	if natPolicy.TargetContent != nil {
+		modifyRuleItem.TargetContent = natPolicy.TargetContent
+	}
+
+	if natPolicy.TargetType != nil {
+		modifyRuleItem.TargetType = natPolicy.TargetType
+	}
+
+	if natPolicy.Protocol != nil {
+		modifyRuleItem.Protocol = natPolicy.Protocol
+	}
+
+	if natPolicy.RuleAction != nil {
+		modifyRuleItem.RuleAction = natPolicy.RuleAction
+	}
+
+	if natPolicy.Port != nil {
+		modifyRuleItem.Port = natPolicy.Port
+	}
+
+	if natPolicy.Direction != nil {
+		modifyRuleItem.Direction = natPolicy.Direction
+	}
+
+	if natPolicy.Enable != nil {
+		modifyRuleItem.Enable = natPolicy.Enable
+	}
+
+	if natPolicy.Description != nil {
+		modifyRuleItem.Description = natPolicy.Description
+	}
+
+	if natPolicy.ParamTemplateId != nil {
+		modifyRuleItem.ParamTemplateId = natPolicy.ParamTemplateId
+	}
+
+	if natPolicy.Scope != nil {
+		modifyRuleItem.Scope = natPolicy.Scope
+	}
+
+	if v, ok := d.GetOkExists("order_index"); ok {
+		modifyRuleItem.OrderIndex = helper.IntInt64(v.(int))
+	}
+
+	modifyRuleItem.Uuid = helper.StrToInt64Point(uuid)
+	request.Rules = append(request.Rules, &modifyRuleItem)
+	reqErr := resource.Retry(tccommon.WriteRetryTimeout, func() *resource.RetryError {
+		result, e := meta.(tccommon.ProviderMeta).GetAPIV3Conn().UseCfwV20190904Client().ModifyNatAcRuleWithContext(ctx, request)
+		if e != nil {
+			return tccommon.RetryError(e)
+		} else {
+			log.Printf("[DEBUG]%s api[%s] success, request body [%s], response body [%s]\n", logId, request.GetAction(), request.ToJsonString(), result.ToJsonString())
+		}
+
+		return nil
+	})
+
+	if reqErr != nil {
+		log.Printf("[CRITAL]%s update cfw nat policy order config failed, reason:%+v", logId, reqErr)
+		return reqErr
+	}
+
+	return resourceTencentCloudCfwNatPolicyOrderConfigRead(d, meta)
+}
+
+func resourceTencentCloudCfwNatPolicyOrderConfigDelete(d *schema.ResourceData, meta interface{}) error {
+	defer tccommon.LogElapsed("resource.tencentcloud_cfw_nat_policy_order_config.delete")()
+	defer tccommon.InconsistentCheck(d, meta)()
+
+	return nil
+}
