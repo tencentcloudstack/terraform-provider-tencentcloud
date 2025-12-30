aa0599dd5f17c157c7b9f6026c520a5d89e6182a
SevenEarth <391613297@qq.com>
2025-12-30 19:18:46 +0800

    add

--- tencentcloud/services/tcr/resource_tc_tcr_replication.go
+++ tencentcloud/services/tcr/resource_tc_tcr_replication.go
@@ -167,7 +167,7 @@ func resourceTencentCloudTcrReplicationCreate(d *schema.ResourceData, meta inter
 			ruleName = v
 		}
 
-		if v, ok := ruleMap["dest_namespace"].(string); ok && v != "" {
+		if v, ok := ruleMap["dest_namespace"].(string); ok {
 			replicationRule.DestNamespace = helper.String(v)
 		}
 
@@ -183,7 +183,7 @@ func resourceTencentCloudTcrReplicationCreate(d *schema.ResourceData, meta inter
 					replicationFilter.Type = helper.String(v)
 				}
 
-				if v, ok := filtersMap["value"].(string); ok && v != "" {
+				if v, ok := filtersMap["value"].(string); ok {
 					replicationFilter.Value = helper.String(v)
 				}
 
