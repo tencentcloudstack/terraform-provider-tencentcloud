41352ca14a7dded6e265f6a9a506af9aaf99c5d3
SevenEarth <391613297@qq.com>
2026-01-13 19:36:55 +0800

    add

--- tencentcloud/services/tke/resource_tc_kubernetes_control_plane_log_test.go
+++ tencentcloud/services/tke/resource_tc_kubernetes_control_plane_log_test.go
@@ -37,7 +37,6 @@ resource "tencentcloud_kubernetes_control_plane_log" "example" {
   cluster_type = "tke"
   components {
     name         = "cluster-autoscaler"
-    log_level    = "2"
     log_set_id   = "40eed846-0f43-44b1-b216-c786a8970b1f"
     topic_id     = "21918a54-9ab4-40bc-90cd-c600cff00695"
     topic_region = "ap-guangzhou"
