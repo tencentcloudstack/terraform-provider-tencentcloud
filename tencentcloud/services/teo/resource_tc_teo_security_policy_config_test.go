bd0f7e1f8e8cdcb173b8680a1e8671f58bcd405e
iac-agent <tencentcloud_iac@tencent.com>
2026-06-04 20:40:21 +0800

    feat(teo): add security_policy parameter to tencentcloud_teo_security_policy_config resource

--- tencentcloud/services/teo/resource_tc_teo_security_policy_config_test.go
+++ tencentcloud/services/teo/resource_tc_teo_security_policy_config_test.go
@@ -13,6 +13,7 @@ import (
 	tcacctest "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/acctest"
 	tccommon "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/common"
 	"github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/connectivity"
+	"github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/internal/helper"
 	"github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/services/teo"
 )
 
@@ -626,6 +627,455 @@ func TestBotManagementLite_UpdateExpand(t *testing.T) {
 	assert.NotNil(t, capturedRequest.SecurityPolicy.BotManagementLite.AICrawlerDetection.Action.DenyActionParameters)
 	assert.Equal(t, "on", *capturedRequest.SecurityPolicy.BotManagementLite.AICrawlerDetection.Action.DenyActionParameters.BlockIp)
 	assert.Equal(t, "120s", *capturedRequest.SecurityPolicy.BotManagementLite.AICrawlerDetection.Action.DenyActionParameters.BlockIpDuration)
+}
+
+// go test ./tencentcloud/services/teo/ -run "TestSecurityPolicy_ReadWithCustomRules" -v -count=1 -gcflags="all=-l"
+// TestSecurityPolicy_ReadWithCustomRules tests Read flattens CustomRules from API response
+func TestSecurityPolicy_ReadWithCustomRules(t *testing.T) {
+	patches := gomonkey.NewPatches()
+	defer patches.Reset()
+
+	teoClient := &teov20220901.Client{}
+	patches.ApplyMethodReturn(newMockMetaSecurityPolicy().client, "UseTeoV20220901Client", teoClient)
+
+	patches.ApplyMethodFunc(teoClient, "DescribeSecurityPolicy", func(request *teov20220901.DescribeSecurityPolicyRequest) (*teov20220901.DescribeSecurityPolicyResponse, error) {
+		resp := teov20220901.NewDescribeSecurityPolicyResponse()
+		resp.Response = &teov20220901.DescribeSecurityPolicyResponseParams{
+			SecurityPolicy: &teov20220901.SecurityPolicy{
+				CustomRules: &teov20220901.CustomRules{
+					Rules: []*teov20220901.CustomRule{
+						{
+							Name:      ptrStringSecurityPolicy("precise-rule-1"),
+							Condition: ptrStringSecurityPolicy("${http.request.host} contain ['test']"),
+							Action: &teov20220901.SecurityAction{
+								Name: ptrStringSecurityPolicy("BlockIP"),
+								BlockIPActionParameters: &teov20220901.BlockIPActionParameters{
+									Duration: ptrStringSecurityPolicy("120s"),
+								},
+							},
+							Enabled:  ptrStringSecurityPolicy("on"),
+							Id:       ptrStringSecurityPolicy("12345"),
+							RuleType: ptrStringSecurityPolicy("PreciseMatchRule"),
+							Priority: helper.Int64(50),
+						},
+						{
+							Name:      ptrStringSecurityPolicy("basic-rule-1"),
+							Condition: ptrStringSecurityPolicy("${http.request.ip} in ['1.2.3.4']"),
+							Action: &teov20220901.SecurityAction{
+								Name: ptrStringSecurityPolicy("Deny"),
+							},
+							Enabled:  ptrStringSecurityPolicy("off"),
+							Id:       ptrStringSecurityPolicy("67890"),
+							RuleType: ptrStringSecurityPolicy("BasicAccessRule"),
+							Priority: helper.Int64(30),
+						},
+					},
+				},
+			},
+			RequestId: ptrStringSecurityPolicy("fake-request-id"),
+		}
+		return resp, nil
+	})
+
+	meta := newMockMetaSecurityPolicy()
+	res := teo.ResourceTencentCloudTeoSecurityPolicyConfig()
+	d := schema.TestResourceDataRaw(t, res.Schema, map[string]interface{}{
+		"zone_id": "zone-12345678",
+		"entity":  "ZoneDefaultPolicy",
+	})
+	d.SetId("zone-12345678#ZoneDefaultPolicy")
+
+	err := res.Read(d, meta)
+	assert.NoError(t, err)
+
+	securityPolicy := d.Get("security_policy").([]interface{})
+	assert.Len(t, securityPolicy, 1)
+	spMap := securityPolicy[0].(map[string]interface{})
+
+	customRules := spMap["custom_rules"].([]interface{})
+	assert.Len(t, customRules, 1)
+	crMap := customRules[0].(map[string]interface{})
+
+	preciseMatchRules := crMap["precise_match_rules"].([]interface{})
+	assert.Len(t, preciseMatchRules, 1)
+	pmrMap := preciseMatchRules[0].(map[string]interface{})
+	assert.Equal(t, "precise-rule-1", pmrMap["name"])
+	assert.Equal(t, "${http.request.host} contain ['test']", pmrMap["condition"])
+	assert.Equal(t, "on", pmrMap["enabled"])
+	assert.Equal(t, "12345", pmrMap["id"])
+	assert.Equal(t, "PreciseMatchRule", pmrMap["rule_type"])
+
+	pmrAction := pmrMap["action"].([]interface{})
+	assert.Len(t, pmrAction, 1)
+	pmrActionMap := pmrAction[0].(map[string]interface{})
+	assert.Equal(t, "BlockIP", pmrActionMap["name"])
+	blockIPParams := pmrActionMap["block_ip_action_parameters"].([]interface{})
+	assert.Len(t, blockIPParams, 1)
+	blockIPMap := blockIPParams[0].(map[string]interface{})
+	assert.Equal(t, "120s", blockIPMap["duration"])
+
+	basicAccessRules := crMap["basic_access_rules"].([]interface{})
+	assert.Len(t, basicAccessRules, 1)
+	barMap := basicAccessRules[0].(map[string]interface{})
+	assert.Equal(t, "basic-rule-1", barMap["name"])
+	assert.Equal(t, "${http.request.ip} in ['1.2.3.4']", barMap["condition"])
+	assert.Equal(t, "off", barMap["enabled"])
+	assert.Equal(t, "67890", barMap["id"])
+	assert.Equal(t, "BasicAccessRule", barMap["rule_type"])
+
+	barAction := barMap["action"].([]interface{})
+	assert.Len(t, barAction, 1)
+	barActionMap := barAction[0].(map[string]interface{})
+	assert.Equal(t, "Deny", barActionMap["name"])
+}
+
+// go test ./tencentcloud/services/teo/ -run "TestSecurityPolicy_ReadWithManagedRules" -v -count=1 -gcflags="all=-l"
+// TestSecurityPolicy_ReadWithManagedRules tests Read flattens ManagedRules from API response
+func TestSecurityPolicy_ReadWithManagedRules(t *testing.T) {
+	patches := gomonkey.NewPatches()
+	defer patches.Reset()
+
+	teoClient := &teov20220901.Client{}
+	patches.ApplyMethodReturn(newMockMetaSecurityPolicy().client, "UseTeoV20220901Client", teoClient)
+
+	patches.ApplyMethodFunc(teoClient, "DescribeSecurityPolicy", func(request *teov20220901.DescribeSecurityPolicyRequest) (*teov20220901.DescribeSecurityPolicyResponse, error) {
+		resp := teov20220901.NewDescribeSecurityPolicyResponse()
+		resp.Response = &teov20220901.DescribeSecurityPolicyResponseParams{
+			SecurityPolicy: &teov20220901.SecurityPolicy{
+				ManagedRules: &teov20220901.ManagedRules{
+					Enabled:          ptrStringSecurityPolicy("on"),
+					DetectionOnly:    ptrStringSecurityPolicy("off"),
+					SemanticAnalysis: ptrStringSecurityPolicy("off"),
+					AutoUpdate: &teov20220901.ManagedRuleAutoUpdate{
+						AutoUpdateToLatestVersion: ptrStringSecurityPolicy("on"),
+						RulesetVersion:            ptrStringSecurityPolicy("v2024.01"),
+					},
+					ManagedRuleGroups: []*teov20220901.ManagedRuleGroup{
+						{
+							GroupId:          ptrStringSecurityPolicy("wafgroup-sql-injections"),
+							SensitivityLevel: ptrStringSecurityPolicy("strict"),
+							Action: &teov20220901.SecurityAction{
+								Name: ptrStringSecurityPolicy("Deny"),
+							},
+						},
+						{
+							GroupId:          ptrStringSecurityPolicy("wafgroup-xss-attacks"),
+							SensitivityLevel: ptrStringSecurityPolicy("normal"),
+							Action: &teov20220901.SecurityAction{
+								Name: ptrStringSecurityPolicy("Monitor"),
+							},
+						},
+					},
+				},
+			},
+			RequestId: ptrStringSecurityPolicy("fake-request-id"),
+		}
+		return resp, nil
+	})
+
+	meta := newMockMetaSecurityPolicy()
+	res := teo.ResourceTencentCloudTeoSecurityPolicyConfig()
+	d := schema.TestResourceDataRaw(t, res.Schema, map[string]interface{}{
+		"zone_id": "zone-12345678",
+		"entity":  "ZoneDefaultPolicy",
+	})
+	d.SetId("zone-12345678#ZoneDefaultPolicy")
+
+	err := res.Read(d, meta)
+	assert.NoError(t, err)
+
+	securityPolicy := d.Get("security_policy").([]interface{})
+	assert.Len(t, securityPolicy, 1)
+	spMap := securityPolicy[0].(map[string]interface{})
+
+	managedRules := spMap["managed_rules"].([]interface{})
+	assert.Len(t, managedRules, 1)
+	mrMap := managedRules[0].(map[string]interface{})
+	assert.Equal(t, "on", mrMap["enabled"])
+	assert.Equal(t, "off", mrMap["detection_only"])
+	assert.Equal(t, "off", mrMap["semantic_analysis"])
+
+	autoUpdate := mrMap["auto_update"].([]interface{})
+	assert.Len(t, autoUpdate, 1)
+	auMap := autoUpdate[0].(map[string]interface{})
+	assert.Equal(t, "on", auMap["auto_update_to_latest_version"])
+	assert.Equal(t, "v2024.01", auMap["ruleset_version"])
+
+	managedRuleGroups := mrMap["managed_rule_groups"].(*schema.Set).List()
+	assert.Len(t, managedRuleGroups, 2)
+}
+
+// go test ./tencentcloud/services/teo/ -run "TestSecurityPolicy_UpdateWithCustomRules" -v -count=1 -gcflags="all=-l"
+// TestSecurityPolicy_UpdateWithCustomRules tests Update expands custom_rules into ModifySecurityPolicy request
+func TestSecurityPolicy_UpdateWithCustomRules(t *testing.T) {
+	patches := gomonkey.NewPatches()
+	defer patches.Reset()
+
+	teoClient := &teov20220901.Client{}
+	patches.ApplyMethodReturn(newMockMetaSecurityPolicy().client, "UseTeoV20220901Client", teoClient)
+
+	var capturedRequest *teov20220901.ModifySecurityPolicyRequest
+	patches.ApplyMethodFunc(teoClient, "ModifySecurityPolicyWithContext", func(_ context.Context, request *teov20220901.ModifySecurityPolicyRequest) (*teov20220901.ModifySecurityPolicyResponse, error) {
+		capturedRequest = request
+		resp := teov20220901.NewModifySecurityPolicyResponse()
+		resp.Response = &teov20220901.ModifySecurityPolicyResponseParams{
+			RequestId: ptrStringSecurityPolicy("fake-request-id"),
+		}
+		return resp, nil
+	})
+
+	patches.ApplyMethodFunc(teoClient, "DescribeSecurityPolicy", func(request *teov20220901.DescribeSecurityPolicyRequest) (*teov20220901.DescribeSecurityPolicyResponse, error) {
+		resp := teov20220901.NewDescribeSecurityPolicyResponse()
+		resp.Response = &teov20220901.DescribeSecurityPolicyResponseParams{
+			SecurityPolicy: &teov20220901.SecurityPolicy{
+				CustomRules: &teov20220901.CustomRules{
+					Rules: []*teov20220901.CustomRule{
+						{
+							Name:      ptrStringSecurityPolicy("precise-rule-1"),
+							Condition: ptrStringSecurityPolicy("${http.request.host} contain ['test']"),
+							Action: &teov20220901.SecurityAction{
+								Name: ptrStringSecurityPolicy("BlockIP"),
+								BlockIPActionParameters: &teov20220901.BlockIPActionParameters{
+									Duration: ptrStringSecurityPolicy("120s"),
+								},
+							},
+							Enabled:  ptrStringSecurityPolicy("on"),
+							RuleType: ptrStringSecurityPolicy("PreciseMatchRule"),
+							Priority: helper.Int64(50),
+						},
+					},
+				},
+			},
+			RequestId: ptrStringSecurityPolicy("fake-request-id"),
+		}
+		return resp, nil
+	})
+
+	meta := newMockMetaSecurityPolicy()
+	res := teo.ResourceTencentCloudTeoSecurityPolicyConfig()
+	d := schema.TestResourceDataRaw(t, res.Schema, map[string]interface{}{
+		"zone_id": "zone-12345678",
+		"entity":  "ZoneDefaultPolicy",
+		"security_policy": []interface{}{
+			map[string]interface{}{
+				"custom_rules": []interface{}{
+					map[string]interface{}{
+						"precise_match_rules": []interface{}{
+							map[string]interface{}{
+								"name":      "precise-rule-1",
+								"condition": "${http.request.host} contain ['test']",
+								"enabled":   "on",
+								"priority":  50,
+								"action": []interface{}{
+									map[string]interface{}{
+										"name": "BlockIP",
+										"block_ip_action_parameters": []interface{}{
+											map[string]interface{}{
+												"duration": "120s",
+											},
+										},
+									},
+								},
+							},
+						},
+						"basic_access_rules": []interface{}{
+							map[string]interface{}{
+								"name":      "basic-rule-1",
+								"condition": "${http.request.ip} in ['1.2.3.4']",
+								"enabled":   "off",
+								"priority":  30,
+								"action": []interface{}{
+									map[string]interface{}{
+										"name": "Deny",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	})
+	d.SetId("zone-12345678#ZoneDefaultPolicy")
+
+	err := res.Update(d, meta)
+	assert.NoError(t, err)
+	assert.NotNil(t, capturedRequest)
+	assert.NotNil(t, capturedRequest.SecurityPolicy)
+	assert.NotNil(t, capturedRequest.SecurityPolicy.CustomRules)
+	assert.Len(t, capturedRequest.SecurityPolicy.CustomRules.Rules, 2)
+
+	// Verify precise match rule
+	preciseRule := capturedRequest.SecurityPolicy.CustomRules.Rules[0]
+	assert.Equal(t, "precise-rule-1", *preciseRule.Name)
+	assert.Equal(t, "${http.request.host} contain ['test']", *preciseRule.Condition)
+	assert.Equal(t, "on", *preciseRule.Enabled)
+	assert.Equal(t, "PreciseMatchRule", *preciseRule.RuleType)
+	assert.Equal(t, int64(50), *preciseRule.Priority)
+	assert.NotNil(t, preciseRule.Action)
+	assert.Equal(t, "BlockIP", *preciseRule.Action.Name)
+	assert.NotNil(t, preciseRule.Action.BlockIPActionParameters)
+	assert.Equal(t, "120s", *preciseRule.Action.BlockIPActionParameters.Duration)
+
+	// Verify basic access rule
+	basicRule := capturedRequest.SecurityPolicy.CustomRules.Rules[1]
+	assert.Equal(t, "basic-rule-1", *basicRule.Name)
+	assert.Equal(t, "${http.request.ip} in ['1.2.3.4']", *basicRule.Condition)
+	assert.Equal(t, "off", *basicRule.Enabled)
+	assert.Equal(t, "BasicAccessRule", *basicRule.RuleType)
+	assert.Equal(t, int64(30), *basicRule.Priority)
+	assert.NotNil(t, basicRule.Action)
+	assert.Equal(t, "Deny", *basicRule.Action.Name)
+}
+
+// go test ./tencentcloud/services/teo/ -run "TestSecurityPolicy_UpdateWithManagedRules" -v -count=1 -gcflags="all=-l"
+// TestSecurityPolicy_UpdateWithManagedRules tests Update expands managed_rules into ModifySecurityPolicy request
+func TestSecurityPolicy_UpdateWithManagedRules(t *testing.T) {
+	patches := gomonkey.NewPatches()
+	defer patches.Reset()
+
+	teoClient := &teov20220901.Client{}
+	patches.ApplyMethodReturn(newMockMetaSecurityPolicy().client, "UseTeoV20220901Client", teoClient)
+
+	var capturedRequest *teov20220901.ModifySecurityPolicyRequest
+	patches.ApplyMethodFunc(teoClient, "ModifySecurityPolicyWithContext", func(_ context.Context, request *teov20220901.ModifySecurityPolicyRequest) (*teov20220901.ModifySecurityPolicyResponse, error) {
+		capturedRequest = request
+		resp := teov20220901.NewModifySecurityPolicyResponse()
+		resp.Response = &teov20220901.ModifySecurityPolicyResponseParams{
+			RequestId: ptrStringSecurityPolicy("fake-request-id"),
+		}
+		return resp, nil
+	})
+
+	patches.ApplyMethodFunc(teoClient, "DescribeSecurityPolicy", func(request *teov20220901.DescribeSecurityPolicyRequest) (*teov20220901.DescribeSecurityPolicyResponse, error) {
+		resp := teov20220901.NewDescribeSecurityPolicyResponse()
+		resp.Response = &teov20220901.DescribeSecurityPolicyResponseParams{
+			SecurityPolicy: &teov20220901.SecurityPolicy{
+				ManagedRules: &teov20220901.ManagedRules{
+					Enabled:          ptrStringSecurityPolicy("on"),
+					DetectionOnly:    ptrStringSecurityPolicy("off"),
+					SemanticAnalysis: ptrStringSecurityPolicy("off"),
+					AutoUpdate: &teov20220901.ManagedRuleAutoUpdate{
+						AutoUpdateToLatestVersion: ptrStringSecurityPolicy("off"),
+					},
+					ManagedRuleGroups: []*teov20220901.ManagedRuleGroup{
+						{
+							GroupId:          ptrStringSecurityPolicy("wafgroup-sql-injections"),
+							SensitivityLevel: ptrStringSecurityPolicy("strict"),
+							Action: &teov20220901.SecurityAction{
+								Name: ptrStringSecurityPolicy("Deny"),
+							},
+						},
+					},
+				},
+			},
+			RequestId: ptrStringSecurityPolicy("fake-request-id"),
+		}
+		return resp, nil
+	})
+
+	meta := newMockMetaSecurityPolicy()
+	res := teo.ResourceTencentCloudTeoSecurityPolicyConfig()
+	d := schema.TestResourceDataRaw(t, res.Schema, map[string]interface{}{
+		"zone_id": "zone-12345678",
+		"entity":  "ZoneDefaultPolicy",
+		"security_policy": []interface{}{
+			map[string]interface{}{
+				"managed_rules": []interface{}{
+					map[string]interface{}{
+						"enabled":           "on",
+						"detection_only":    "off",
+						"semantic_analysis": "off",
+						"auto_update": []interface{}{
+							map[string]interface{}{
+								"auto_update_to_latest_version": "off",
+							},
+						},
+					},
+				},
+			},
+		},
+	})
+	d.SetId("zone-12345678#ZoneDefaultPolicy")
+
+	err := res.Update(d, meta)
+	assert.NoError(t, err)
+	assert.NotNil(t, capturedRequest)
+	assert.NotNil(t, capturedRequest.SecurityPolicy)
+	assert.NotNil(t, capturedRequest.SecurityPolicy.ManagedRules)
+	assert.Equal(t, "on", *capturedRequest.SecurityPolicy.ManagedRules.Enabled)
+	assert.Equal(t, "off", *capturedRequest.SecurityPolicy.ManagedRules.DetectionOnly)
+	assert.Equal(t, "off", *capturedRequest.SecurityPolicy.ManagedRules.SemanticAnalysis)
+	assert.NotNil(t, capturedRequest.SecurityPolicy.ManagedRules.AutoUpdate)
+	assert.Equal(t, "off", *capturedRequest.SecurityPolicy.ManagedRules.AutoUpdate.AutoUpdateToLatestVersion)
+}
+
+// go test ./tencentcloud/services/teo/ -run "TestSecurityPolicy_ReadWithNilSecurityPolicy" -v -count=1 -gcflags="all=-l"
+// TestSecurityPolicy_ReadWithNilSecurityPolicy tests Read when SecurityPolicy is nil (resource deleted)
+func TestSecurityPolicy_ReadWithNilSecurityPolicy(t *testing.T) {
+	patches := gomonkey.NewPatches()
+	defer patches.Reset()
+
+	teoClient := &teov20220901.Client{}
+	patches.ApplyMethodReturn(newMockMetaSecurityPolicy().client, "UseTeoV20220901Client", teoClient)
+
+	patches.ApplyMethodFunc(teoClient, "DescribeSecurityPolicy", func(request *teov20220901.DescribeSecurityPolicyRequest) (*teov20220901.DescribeSecurityPolicyResponse, error) {
+		resp := teov20220901.NewDescribeSecurityPolicyResponse()
+		resp.Response = &teov20220901.DescribeSecurityPolicyResponseParams{
+			SecurityPolicy: nil,
+			RequestId:      ptrStringSecurityPolicy("fake-request-id"),
+		}
+		return resp, nil
+	})
+
+	meta := newMockMetaSecurityPolicy()
+	res := teo.ResourceTencentCloudTeoSecurityPolicyConfig()
+	d := schema.TestResourceDataRaw(t, res.Schema, map[string]interface{}{
+		"zone_id": "zone-12345678",
+		"entity":  "ZoneDefaultPolicy",
+	})
+	d.SetId("zone-12345678#ZoneDefaultPolicy")
+
+	err := res.Read(d, meta)
+	assert.NoError(t, err)
+	assert.Equal(t, "", d.Id())
+}
+
+// go test ./tencentcloud/services/teo/ -run "TestSecurityPolicy_ReadWithHostEntity" -v -count=1 -gcflags="all=-l"
+// TestSecurityPolicy_ReadWithHostEntity tests Read correctly parses Host entity ID
+func TestSecurityPolicy_ReadWithHostEntity(t *testing.T) {
+	patches := gomonkey.NewPatches()
+	defer patches.Reset()
+
+	teoClient := &teov20220901.Client{}
+	patches.ApplyMethodReturn(newMockMetaSecurityPolicy().client, "UseTeoV20220901Client", teoClient)
+
+	var capturedRequest *teov20220901.DescribeSecurityPolicyRequest
+	patches.ApplyMethodFunc(teoClient, "DescribeSecurityPolicy", func(request *teov20220901.DescribeSecurityPolicyRequest) (*teov20220901.DescribeSecurityPolicyResponse, error) {
+		capturedRequest = request
+		resp := teov20220901.NewDescribeSecurityPolicyResponse()
+		resp.Response = &teov20220901.DescribeSecurityPolicyResponseParams{
+			SecurityPolicy: &teov20220901.SecurityPolicy{},
+			RequestId:      ptrStringSecurityPolicy("fake-request-id"),
+		}
+		return resp, nil
+	})
+
+	meta := newMockMetaSecurityPolicy()
+	res := teo.ResourceTencentCloudTeoSecurityPolicyConfig()
+	d := schema.TestResourceDataRaw(t, res.Schema, map[string]interface{}{
+		"zone_id": "zone-12345678",
+		"entity":  "Host",
+		"host":    "www.example.com",
+	})
+	d.SetId("zone-12345678#Host#www.example.com")
+
+	err := res.Read(d, meta)
+	assert.NoError(t, err)
+	assert.NotNil(t, capturedRequest)
+	assert.Equal(t, "zone-12345678", *capturedRequest.ZoneId)
+	assert.Equal(t, "Host", *capturedRequest.Entity)
+	assert.Equal(t, "www.example.com", *capturedRequest.Host)
 }
 
 // TestBotManagementLite_Schema tests the bot_management_lite schema definition
